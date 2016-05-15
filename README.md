9d8d9d535d98a9fc3f6d9177501ef536ce52f638
mouradmourafiq <mouradmourafiq@gmail.com>
2016-05-15 22:39:14 +0200

    add lstm code and update wiki

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tensorflow-lstm-regression
-Sequence prediction using recurrent neural networks(LSTM) with TensorFlow
+[Sequence prediction using recurrent neural networks(LSTM) with TensorFlow](http://mourafiq.com/2016/05/15/predicting-sequences-using-rnn-in-tensorflow.html)
---
+++ lstm.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.python.framework import dtypes
+import tensorflow.contrib.learn as skflow
+
+
+def x_sin(x):
+    return x * np.sin(x)
+
+
+def sin_cos(x):
+    return pd.DataFrame(dict(a=np.sin(x), b=np.cos(x)), index=x)
+
+
+def rnn_data(data, time_steps, labels=False):
+    """
+    creates new data frame based on previous observation
+      * example:
+        l = [1, 2, 3, 4, 5]
+        time_steps = 2
+        -> labels == False [[1, 2], [2, 3], [3, 4]]
+        -> labels == True [2, 3, 4, 5]
+    """
+    rnn_df = []
+    for i in range(len(data) - time_steps):
+        if labels:
+            try:
+                rnn_df.append(data.iloc[i + time_steps].as_matrix())
+            except AttributeError:
+                rnn_df.append(data.iloc[i + time_steps])
+        else:
+            data_ = data.iloc[i: i + time_steps].as_matrix()
+            rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
+    return np.array(rnn_df)
+
+
+def split_data(data, val_size=0.1, test_size=0.1):
+    """
+    splits data to training, validation and testing parts
+    """
+    ntest = int(round(len(data) * (1 - test_size)))
+    nval = int(round(len(data.iloc[:ntest]) * (1 - val_size)))
+
+    df_train, df_val, df_test = data.iloc[:nval], data.iloc[nval:ntest], data.iloc[ntest:]
+
+    return df_train, df_val, df_test
+
+
+def prepare_data(data, time_steps, labels=False, val_size=0.1, test_size=0.1):
+    """
+    Given the number of `time_steps` and some data.
+    prepares training, validation and test data for an lstm cell.
+    """
+    df_train, df_val, df_test = split_data(data, val_size, test_size)
+    return (rnn_data(df_train, time_steps, labels=labels),
+            rnn_data(df_val, time_steps, labels=labels),
+            rnn_data(df_test, time_steps, labels=labels))
+
+
+def generate_data(fct, x, time_steps, seperate=False):
+    """generate data with based on a function fct"""
+    data = fct(x)
+    if not isinstance(data, pd.DataFrame):
+        data = pd.DataFrame(data)
+    train_x, val_x, test_x = prepare_data(data['a'] if seperate else data, time_steps)
+    train_y, val_y, test_y = prepare_data(data['b'] if seperate else data, time_steps, labels=True)
+    return dict(train=train_x, val=val_x, test=test_x), dict(train=train_y, val=val_y, test=test_y)
+
+
+def lstm_model(time_steps, rnn_layers, dense_layers=None):
+    def lstm_cells(layers):
+        if isinstance(layers[0], dict):
+            return [tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.BasicLSTMCell(layer['steps']), layer['keep_prob'])
+                    if layer.get('keep_prob') else tf.nn.rnn_cell.BasicLSTMCell(layer['steps'])
+                    for layer in layers]
+        return [tf.nn.rnn_cell.BasicLSTMCell(steps) for steps in layers]
+
+    def dnn_layers(input_layers, layers):
+        if layers and isinstance(layers, dict):
+            return skflow.ops.dnn(input_layers,
+                                  layers['layers'],
+                                  activation=layers.get('activation'),
+                                  dropout=layers.get('dropout'))
+        elif layers:
+            return skflow.ops.dnn(input_layers, layers)
+        else:
+            return input_layers
+
+    def _lstm_model(X, y):
+        stacked_lstm = tf.nn.rnn_cell.MultiRNNCell(lstm_cells(rnn_layers))
+        x_ = skflow.ops.split_squeeze(1, time_steps, X)
+        output, layers = tf.nn.rnn(stacked_lstm, x_, dtype=dtypes.float32)
+        output = dnn_layers(output[-1], dense_layers)
+        return skflow.models.linear_regression(output, y)
+
+    return _lstm_model
