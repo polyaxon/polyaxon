795085dbc8525b4507084f0e02e81041f9ab37af
mouradmourafiq <mouradmourafiq@gmail.com>
2016-06-05 19:51:22 +0200

    fix some warning issues

--- lstm.py
+++ lstm.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 from tensorflow.python.framework import dtypes
 from tensorflow.contrib import learn
 
+import logging
+logging.basicConfig(level=logging.INFO)
 
 def x_sin(x):
     return x * np.sin(x)
@@ -83,11 +85,13 @@ def lstm_model(time_steps, rnn_layers, dense_layers=None):
 
     def lstm_cells(layers):
         if isinstance(layers[0], dict):
-            return [tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.BasicLSTMCell(layer['steps']),
+            return [tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.BasicLSTMCell(layer['steps'],
+                                                                               state_is_tuple=True),
                                                   layer['keep_prob'])
-                    if layer.get('keep_prob') else tf.nn.rnn_cell.BasicLSTMCell(layer['steps'])
+                    if layer.get('keep_prob') else tf.nn.rnn_cell.BasicLSTMCell(layer['steps'],
+                                                                                state_is_tuple=True)
                     for layer in layers]
-        return [tf.nn.rnn_cell.BasicLSTMCell(steps) for steps in layers]
+        return [tf.nn.rnn_cell.BasicLSTMCell(steps, state_is_tuple=True) for steps in layers]
 
     def dnn_layers(input_layers, layers):
         if layers and isinstance(layers, dict):
@@ -101,7 +105,7 @@ def lstm_model(time_steps, rnn_layers, dense_layers=None):
             return input_layers
 
     def _lstm_model(X, y):
-        stacked_lstm = tf.nn.rnn_cell.MultiRNNCell(lstm_cells(rnn_layers))
+        stacked_lstm = tf.nn.rnn_cell.MultiRNNCell(lstm_cells(rnn_layers), state_is_tuple=True)
         x_ = learn.ops.split_squeeze(1, time_steps, X)
         output, layers = tf.nn.rnn(stacked_lstm, x_, dtype=dtypes.float32)
         output = dnn_layers(output[-1], dense_layers)
