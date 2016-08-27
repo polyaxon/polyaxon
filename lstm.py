f9e4a64fafc4cb94c545cb47a11209310e2bfcc2
mouradmourafiq <mouradmourafiq@gmail.com>
2016-08-27 12:08:48 +0200

    update code to be compatible with tensorflow 0.10.0rc  * add truncated backpropagation (num_units vs time_steps)

--- lstm.py
+++ lstm.py
@@ -4,8 +4,6 @@ import tensorflow as tf
 from tensorflow.python.framework import dtypes
 from tensorflow.contrib import learn
 
-import logging
-logging.basicConfig(level=logging.INFO)
 
 def x_sin(x):
     return x * np.sin(x)
@@ -70,12 +68,12 @@ def generate_data(fct, x, time_steps, seperate=False):
     return dict(train=train_x, val=val_x, test=test_x), dict(train=train_y, val=val_y, test=test_y)
 
 
-def lstm_model(time_steps, rnn_layers, dense_layers=None):
+def lstm_model(num_units, rnn_layers, dense_layers=None, learning_rate=0.1, optimizer='Adagrad'):
     """
     Creates a deep model based on:
         * stacked lstm cells
         * an optional dense layers
-    :param time_steps: the number of time steps the model will be looking at.
+    :param num_units: the size of the cells.
     :param rnn_layers: list of int or dict
                          * list of int: the steps used to instantiate the `BasicLSTMCell` cell
                          * list of dict: [{steps: int, keep_prob: int}, ...]
@@ -85,10 +83,10 @@ def lstm_model(time_steps, rnn_layers, dense_layers=None):
 
     def lstm_cells(layers):
         if isinstance(layers[0], dict):
-            return [tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.BasicLSTMCell(layer['steps'],
+            return [tf.nn.rnn_cell.DropoutWrapper(tf.nn.rnn_cell.BasicLSTMCell(layer['num_units'],
                                                                                state_is_tuple=True),
                                                   layer['keep_prob'])
-                    if layer.get('keep_prob') else tf.nn.rnn_cell.BasicLSTMCell(layer['steps'],
+                    if layer.get('keep_prob') else tf.nn.rnn_cell.BasicLSTMCell(layer['num_units'],
                                                                                 state_is_tuple=True)
                     for layer in layers]
         return [tf.nn.rnn_cell.BasicLSTMCell(steps, state_is_tuple=True) for steps in layers]
@@ -106,9 +104,13 @@ def lstm_model(time_steps, rnn_layers, dense_layers=None):
 
     def _lstm_model(X, y):
         stacked_lstm = tf.nn.rnn_cell.MultiRNNCell(lstm_cells(rnn_layers), state_is_tuple=True)
-        x_ = learn.ops.split_squeeze(1, time_steps, X)
+        x_ = tf.unpack(X, axis=1, num=num_units)
         output, layers = tf.nn.rnn(stacked_lstm, x_, dtype=dtypes.float32)
         output = dnn_layers(output[-1], dense_layers)
-        return learn.models.linear_regression(output, y)
+        prediction, loss = learn.models.linear_regression(output, y)
+        train_op = tf.contrib.layers.optimize_loss(
+            loss, tf.contrib.framework.get_global_step(), optimizer=optimizer,
+            learning_rate=learning_rate)
+        return prediction, loss, train_op
 
     return _lstm_model
