cc3aa14c236b41d4b6ab54b899c7280488864623
mouradmourafiq <mouradmourafiq@gmail.com>
2017-06-16 10:48:37 +0200

    Bump version:  * compatibility with tensorflow >= 1.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon',
-      version='0.0.25',
+      version='0.0.26',
       description='Deep Learning library for TensorFlow for '
                   'building end to end models and experiments.',
       maintainer='Mourad Mourafiq',
