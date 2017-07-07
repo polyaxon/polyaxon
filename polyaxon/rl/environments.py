Fix some issues with NAF model, and action calculation in continuous mode

polyaxon/rl/environments.py
@@ -78,10 +78,11 @@ class GymEnvironment(Environment):
         return state
 
     def step(self, action, state, return_spec=True):
-        if isinstance(self._env.action_space, Box) and not isinstance(action, list):
+        if isinstance(action, (list, np.ndarray)):
+            if isinstance(self._env.action_space, Discrete) or isinstance(action, (list, np.ndarray)):
+                action = action[0]
+        if isinstance(self._env.action_space, Box) and not isinstance(action, (list, np.ndarray)):
             action = list(action)
-        elif isinstance(self._env.action_space, Discrete) and isinstance(action, (list, np.ndarray)):
-            action = action[0]
         next_state, reward, done, _ = self._env.step(action)
         if return_spec:
             return EnvSpec(