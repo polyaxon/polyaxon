5af3c40f460b21aadffaf0628d6bdc61c7ae696a
mouradmourafiq <mouradmourafiq@gmail.com>
2017-07-07 12:21:40 +0200

    Add rl examples

--- examples/reinforcement_learning/dueling_ddqn_cartpole.py
+++ examples/reinforcement_learning/dueling_ddqn_cartpole.py
@@ -13,19 +13,20 @@ def main(*args):
     env = plx.envs.GymEnvironment('CartPole-v0')
 
     def graph_fn(mode, inputs):
-        return plx.layers.FullyConnected(mode, num_units=32)(inputs['state'])
+        return plx.layers.FullyConnected(mode, num_units=512)(inputs['state'])
 
     def model_fn(features, labels, mode):
-        model = plx.models.DQNModel(
-            mode, graph_fn=graph_fn, loss_config=plx.configs.LossConfig(module='mean_squared_error'),
+        model = plx.models.DDQNModel(
+            mode, graph_fn=graph_fn, loss_config=plx.configs.LossConfig(module='huber_loss'),
             num_states=env.num_states, num_actions=env.num_actions,
             optimizer_config=plx.configs.OptimizerConfig(module='sgd', learning_rate=0.01),
             exploration_config=plx.configs.ExplorationConfig(module='decay'),
-            summaries='all')
+            target_update_frequency=10, summaries='all')
         return model(features, labels)
 
     memory = Memory(num_states=env.num_states, num_actions=env.num_actions, continuous=env.is_continuous)
-    estimator = plx.estimators.Agent(model_fn=model_fn, memory=memory, model_dir="/tmp/polyaxon_logs/dqn")
+    estimator = plx.estimators.Agent(
+        model_fn=model_fn, memory=memory, model_dir="/tmp/polyaxon_logs/ddqn_cartpole")
 
     # Fit
     estimator.train(env)
