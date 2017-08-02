8f071381bac269f698fac3e51b0a6fc8f303b723
mouradmourafiq <mouradmourafiq@gmail.com>
2017-08-03 00:14:48 +0200

    Move routes to components

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from "react-router-dom";
 
 import Header from "./Header";
 import Footer from "./Footer";
-import Routes from "../routes";
+import Routes from "./routes";
 
 import "./App.less";
 
