2c8cd4f6a3f7db77ce75b2068696b747a627915d
mouradmourafiq <mouradmourafiq@gmail.com>
2017-10-03 21:43:31 +0200

    Update polyaxonfile manager and local runner

--- polyaxon/polyaxonfile/local_runner.py
+++ polyaxon/polyaxonfile/local_runner.py
@@ -2,18 +2,29 @@
 from __future__ import absolute_import, division, print_function
 
 import atexit
+import os
+import signal
+import subprocess
+import sys
+
+from six.moves import xrange
 from multiprocessing import Process
 
+from tensorflow.python.platform import tf_logging as logging
 
-# List that stores all the children processes of this module
-from polyaxon.polyaxonfile.manager import prepare_experiments
+from polyaxon_schemas.polyaxonfile.polyaxonfile import PolyaxonFile
+
+from polyaxon.estimators.run_config import TaskType
+from polyaxon.polyaxonfile.manager import (
+    prepare_all_experiments,
+    prepare_experiment,
+)
 
 jobs = []
+processes = []
 
 
 def cleanup():
-    """ Kills all the children processes.
-    """
     for job in jobs:
         job.terminate()
         job.join()
@@ -23,19 +34,87 @@ def cleanup():
 atexit.register(cleanup)
 
 
-def run_experiment(experiment, schedule):
+def signal_handler(signal, frame):
+    for p in processes:
+        p.terminate()
+    sys.exit(0)
+
+
+def get_pybin():
+    try:
+        pybin = os.path.join(os.environ['VIRTUAL_ENV'], 'bin/python')
+    except:
+        pybin = sys.executable
+    return pybin
+
+
+def run_experiment(polyaxonfile, task_type, task_id, schedule):
+    plx_file = PolyaxonFile.read(polyaxonfile)
+    experiment = prepare_experiment(plx_file, task_type, task_id)
     task = getattr(experiment, schedule)
     return task()
 
 
-def run(polyaxonfil):
-    xps = prepare_experiments(polyaxonfil)
+def run_cmd(pybin, cmd, cwd):
+    env_cmd = '{} {}'.format(pybin, cmd)
+    signal.signal(signal.SIGINT, signal_handler)
+    logging.info(env_cmd)
+    p = subprocess.Popen(env_cmd, cwd=cwd, shell=True)
+    processes.append(p)
+    (output, error) = p.communicate()
+    if error:
+        logging.info('{} - ERROR: '.format(error))
+
+
+def create_process(env):
+    cmd = ("-c \"from polyaxon.polyaxonfile.local_runner import run_experiment; "
+           "run_experiment('{polyaxonfile}', '{task_type}', {task_id}, '{schedule}')\"".format(**env))
+    p = Process(target=run_cmd, args=(get_pybin(), cmd, os.getcwd(),))
+    p.daemon = True
+    p.start()
+    jobs.append(p)
+
+
+def run(polyaxonfile):
+    plx_file = PolyaxonFile.read(polyaxonfile)
+    cluster, is_distributed = plx_file.cluster_def
+    if not is_distributed:
+        run_experiment(plx_file, TaskType.MASTER, 0, 'continuous_train_and_eval')
+        return
+
+    env = {
+        'polyaxonfile': polyaxonfile,
+        'task_type': TaskType.MASTER,
+        'task_id': 0,
+        'schedule': 'train_and_evaluate'
+    }
+
+    create_process(env)
+
+    for i in xrange(cluster.get(TaskType.WORKER, 0)):
+        env['task_id'] = i
+        env['task_type'] = TaskType.WORKER
+        env['schedule'] = 'train'
+        create_process(env)
+
+    for i in xrange(cluster.get(TaskType.PS, 0)):
+        env['task_id'] = i
+        env['task_type'] = TaskType.PS
+        env['schedule'] = 'run_std_server'
+        create_process(env)
+
+    for job in jobs:
+        job.join()
+
+
+def run_all(polyaxonfile):
+    xps = prepare_all_experiments(polyaxonfile)
     for i, xp in enumerate(xps):
         if i == 0:
-            schedule = 'continuous_train_and_eval'
+            schedule = 'train_and_evaluate'
         else:
             schedule = 'train'
-        p = Process(target=run_experiment, args=(xp, schedule))
+        p = Process(target=getattr(xp, schedule))
         p.start()
         jobs.append(p)
 
--- polyaxon/polyaxonfile/manager.py
+++ polyaxon/polyaxonfile/manager.py
@@ -56,7 +56,7 @@ def _get_local_cluster(num_workers, num_ps):
         return '{}:{}'.format(host, port)
 
     cluster_config = {
-        'master': [get_address(master_port)]
+        TaskType.MASTER: [get_address(master_port)]
     }
 
     workers = []
@@ -64,19 +64,23 @@ def _get_local_cluster(num_workers, num_ps):
         workers.append(get_address(worker_port))
         worker_port += 1
 
-    cluster_config['worker'] = workers
+    cluster_config[TaskType.WORKER] = workers
 
     ps = []
     for i in range(num_ps):
         ps.append(get_address(ps_port))
         ps_port += 1
 
-    cluster_config['ps'] = ps
+    cluster_config[TaskType.PS] = ps
 
     return ClusterConfig.from_dict(cluster_config)
 
 
-def _get_run_configs(environment):
+def _get_run_configs(polyaxonfile):
+    plx_file = PolyaxonFile.read(polyaxonfile)
+    environment = plx_file.settings.environment
+    cluster_def, is_distributed = plx_file.cluster_def
+
     def get_master_config(config, task_type=None, task_id=None):
         config = RunConfig.from_config(config)
         if task_type is None and task_id is None:
@@ -85,22 +89,18 @@ def _get_run_configs(environment):
 
     config = environment.run_config or RunConfig.CONFIG()
 
-    num_workers = environment.n_workers or 0
-    num_ps = environment.n_ps or 0
+    if not is_distributed:
+        return {TaskType.MASTER: get_master_config(config)}, False
 
-    if not num_workers and not num_ps:
-        return {'master': get_master_config(config)}, False
+    config.cluster = plx_file.get_cluster()
 
-    cluster_config = _get_local_cluster(num_workers, num_ps)
-    config.cluster = cluster_config
+    configs = {TaskType.MASTER: [get_master_config(config, TaskType.MASTER, 0)]}
 
-    configs = {'master': [get_master_config(config, TaskType.MASTER, 0)]}
+    if cluster_def.get(TaskType.WORKER, 0) > 0:
+        configs[TaskType.WORKER] = []
 
-    if num_workers > 0:
-        configs['workers'] = []
-
-    if num_ps > 0:
-        configs['ps'] = []
+    if cluster_def.get(TaskType.PS, 0) > 0:
+        configs[TaskType.PS] = []
 
     worker_session_configs = {}
     for session_config in environment.worker_configs or []:
@@ -111,37 +111,76 @@ def _get_run_configs(environment):
         ps_session_configs[session_config.index] = session_config
 
     default_worker_config = environment.default_worker_config
-    for i in range(num_workers):
+    for i in range(cluster_def.get(TaskType.WORKER, 0)):
         w_config = get_master_config(config, task_type=TaskType.WORKER, task_id=i)
         session_config = worker_session_configs.get(i, default_worker_config)
         if session_config:
             session_config = RunConfig.get_session_config(session_config)
             w_config = w_config.replace(session_config=session_config)
 
-        configs['workers'].append(w_config)
+        configs[TaskType.WORKER].append(w_config)
 
     default_ps_config = environment.default_ps_config
-    for i in range(num_ps):
-        ps_config = get_master_config(config)
+    for i in range(cluster_def.get(TaskType.PS, 0)):
+        ps_config = get_master_config(config, task_type=TaskType.PS, task_id=i)
         session_config = ps_session_configs.get(i, default_ps_config)
         if session_config:
             session_config = RunConfig.get_session_config(session_config)
-            ps_config = ps_config.replace(session_config=session_config,
-                                          task_type=TaskType.PS,
-                                          task_id=i)
+            ps_config = ps_config.replace(session_config=session_config)
 
-        configs['ps'].append(ps_config)
+        configs[TaskType.PS].append(ps_config)
 
     return configs, True
 
 
-def prepare_experiments(polyaxonfil):
-    plx_file = PolyaxonFile(polyaxonfil)
+def prepare_experiment(polyaxonfile, task_type=TaskType.MASTER, task_id=0):
+    plx_file = PolyaxonFile.read(polyaxonfile)
+    cluster, is_distributed = plx_file.cluster_def
+
+    if (task_type not in cluster or
+            not isinstance(cluster[task_type], int) or
+            task_id >= cluster[task_type]):
+        raise ValueError('task_type, task_id `{}, {}` is not supported by '
+                         'the specification file passed.'.format(task_type, task_id))
+
+    if not plx_file.settings.environment:
+        tf.logging.set_verbosity(tf.logging.INFO)
+        configs = {TaskType.MASTER: [RunConfig()]}
+        delay_workers_by_global_step = False
+    else:
+        tf.logging.set_verbosity(LOGGING_LEVEL[plx_file.settings.logging.level])
+        configs, is_distributed = _get_run_configs(plx_file)
+        delay_workers_by_global_step = plx_file.settings.environment.delay_workers_by_global_step
+
+    train_input_fn, train_steps, train_hooks = _get_train(plx_file.train)
+    (eval_input_fn, eval_steps, eval_hooks, eval_delay_secs,
+     continuous_eval_throttle_secs) = _get_eval(plx_file.eval)
+
+    estimator = getters.get_estimator(plx_file.model,
+                                      configs[task_type][task_id],
+                                      output_dir=plx_file.project_path)
+
+    return Experiment(
+        estimator=estimator,
+        train_input_fn=train_input_fn,
+        eval_input_fn=eval_input_fn,
+        train_steps=train_steps,
+        eval_steps=eval_steps,
+        train_hooks=train_hooks,
+        eval_hooks=eval_hooks,
+        eval_delay_secs=eval_delay_secs,
+        continuous_eval_throttle_secs=continuous_eval_throttle_secs,
+        delay_workers_by_global_step=delay_workers_by_global_step,
+        export_strategies=plx_file.settings.export_strategies)
+
+
+def prepare_all_experiments(polyaxonfile):
+    plx_file = PolyaxonFile.read(polyaxonfile)
     is_distributed = False
 
     if not plx_file.settings.environment:
         tf.logging.set_verbosity(tf.logging.INFO)
-        configs = {'master': [RunConfig()]}
+        configs = {TaskType.MASTER: [RunConfig()]}
         delay_workers_by_global_step = False
     else:
         tf.logging.set_verbosity(LOGGING_LEVEL[plx_file.settings.logging.level])
@@ -170,14 +209,14 @@ def prepare_experiments(polyaxonfil):
             delay_workers_by_global_step=delay_workers_by_global_step,
             export_strategies=plx_file.settings.export_strategies)
 
-    xps = [get_experiment(configs['master'][0])]
+    xps = [get_experiment(configs[TaskType.MASTER][0])]
     if not is_distributed:
         return xps
 
-    for i_config in configs.get('workers', []):
+    for i_config in configs.get(TaskType.WORKER, []):
         xps.append(get_experiment(i_config))
 
-    for i_config in configs.get('ps', []):
+    for i_config in configs.get(TaskType.PS, []):
         xps.append(get_experiment(i_config))
 
     return xps
