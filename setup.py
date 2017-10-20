5d99b93e8cf4d06c2e14d7b001362e4d5650be52
mouradmourafiq <mouradmourafiq@gmail.com>
2017-10-20 11:55:18 +0200

    Remove k8s from setup_requires

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(name='polyaxon-schemas',
       ],
       install_requires=[
           'Jinja2==2.9.6',
-          'kubernetes==3.0.0',
           'marshmallow==2.13.5',
           'numpy==1.13.1',
           'pytz==2017.2',
