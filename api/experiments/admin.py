dfb3daffd3a1739c9a13bcdcd56ab9f360a0975d
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-07 14:45:44 +0100

    Update experiments models

--- api/experiments/admin.py
+++ api/experiments/admin.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, division, print_function
 
 from django.contrib import admin
 
-from experiments.models import Experiment
+from experiments.models import Experiment, ExperimentStatus, ExperimentJob, ExperimentJobStatus
 
 admin.site.register(Experiment)
+admin.site.register(ExperimentStatus)
+admin.site.register(ExperimentJob)
+admin.site.register(ExperimentJobStatus)
--- api/experiments/models.py
+++ api/experiments/models.py
@@ -3,21 +3,143 @@ from __future__ import absolute_import, division, print_function
 
 import uuid
 
+from django.conf import settings
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+from polyaxon_k8s.constants import JobLifeCycle
+
+from clusters.constants import ExperimentLifeCycle
+from clusters.models import Cluster
 from libs.models import DiffModel
-from projects.models import PolyaxonFile
+from projects.models import Polyaxonfile, Project
 
 
 class Experiment(DiffModel):
-    """A model that represents experiments created through a polyaxonfile."""
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, null=False)
-    name = models.CharField(max_length=256, blank=True, null=True,
-                            help_text='Name of the experiment')
-    description = models.TextField(blank=True, null=True,
-                                   help_text='Description of the experiment.')
-    polyaxonfile = models.ForeignKey(PolyaxonFile,
-                                     help_text='The polyaxonfile that generate this experiment.')
-    compiled_polyaxonfile = JSONField(help_text='The compiled polyaxon with specific values '
-                                                'for this experiment.')
+    """A model that represents experiments."""
+
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+        null=False)
+    cluster = models.ForeignKey(
+        Cluster,
+        related_name='experiments')
+    project = models.ForeignKey(
+        Project,
+        related_name='experiments')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='experiments')
+    name = models.CharField(
+        max_length=256,
+        blank=True,
+        null=True,
+        help_text='Name of the experiment')
+    description = models.TextField(
+        blank=True,
+        null=True,
+        help_text='Description of the experiment.')
+    polyaxonfile = models.ForeignKey(
+        Polyaxonfile,
+        blank=True,
+        null=True,
+        related_name='experiments',
+        help_text='The polyaxonfile that generate this experiment.')
+    config = JSONField(
+        # TODO: should be validated by the polyaxonfile validator
+        help_text='The compiled polyaxon with specific values for this experiment.')
+    original_experiment = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name='clones',
+        help_text='The original experiment that was cloned from.')
+
+    @property
+    def last_status(self):
+        return self.status.last()
+
+    @property
+    def finished_at(self):
+        status = self.status.filter(status__in=ExperimentLifeCycle.DONE_STATUS).first()
+        if status:
+            return status.created_at
+        return None
+
+    @property
+    def started_at(self):
+        status = self.status.filter(status=ExperimentLifeCycle.STARTING).first()
+        if status:
+            return status.created_at
+        return None
+
+    @property
+    def is_clone(self):
+        return self.original_experiment is not None
+
+
+class ExperimentStatus(models.Model):
+    """A model that represents experiment status at certain time."""
+    experiment = models.ForeignKey(Experiment, related_name='status')
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    status = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+        default=ExperimentLifeCycle.CREATED,
+        choices=ExperimentLifeCycle.CHOICES)
+
+    class Meta:
+        ordering = ['created_at']
+
+
+class ExperimentJob(DiffModel):
+    """A model that represents job related to an experiment"""
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+        null=False)
+    experiment = models.ForeignKey(Experiment, related_name='jobs')
+    definition = JSONField(help_text='The specific values for this job.')
+
+    @property
+    def last_status(self):
+        return self.status.last()
+
+    @property
+    def started_at(self):
+        status = self.status.filter(status=JobLifeCycle.STARTING).first()
+        if status:
+            return status.created_at
+        return None
+
+    @property
+    def finished_at(self):
+        status = self.status.filter(status__in=JobLifeCycle.DONE_STATUS).last()
+        if status:
+            return status.created_at
+        return None
+
+
+class ExperimentJobStatus(models.Model):
+    """A model that represents job status at certain time."""
+    job = models.ForeignKey(ExperimentJob, related_name='status')
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    status = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+        default=JobLifeCycle.CREATED,
+        choices=JobLifeCycle.CHOICES)
+
+    message = models.ForeignKey('ExperimentJobMessage', null=True, blank=True)
+
+
+class ExperimentJobMessage(models.Model):
+    """A model to represent the extend the job status with information about the job."""
+    reason = models.CharField(max_length=256, null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+    exit_code = models.IntegerField(null=True, blank=True)
