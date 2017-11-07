8d5aae690ac601c432b871332e39280ff0db0620
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-07 14:48:08 +0100

    Update views

--- api/projects/urls.py
+++ api/projects/urls.py
@@ -4,11 +4,18 @@ from __future__ import absolute_import, division, print_function
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from libs.urls import UUID_PATTERN, PROJECT_UUID_PATTERN
 from projects import views
 
 urlpatterns = [
-    url(r'^projects/?$', views.ProjectListView.as_view()),
-    url(r'^projects/(?P<uuid>[0-9]+)/?$', views.ProjectDetailView.as_view()),
+    url(r'^projects/?$',
+        views.ProjectListView.as_view()),
+    url(r'^projects/{}/?$'.format(UUID_PATTERN),
+        views.ProjectDetailView.as_view()),
+    url(r'^projects/{}/polyaxonfiles?$'.format(PROJECT_UUID_PATTERN),
+        views.ProjectPolyaxonfileListView.as_view()),
+    url(r'^projects/{}/polyaxonfiles/{}?$'.format(PROJECT_UUID_PATTERN, UUID_PATTERN),
+        views.ProjectPolyaxonfileDetailView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/projects/views.py
+++ api/projects/views.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-from rest_framework.generics import RetrieveAPIView, ListCreateAPIView
+from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, get_object_or_404
 
-from projects.models import Project
-from projects.serialiazers import (
+from projects.models import Project, Polyaxonfile
+from projects.serializers import (
     ProjectSerializer,
     ProjectDetailSerializer,
-)
+    PolyaxonfileSerializer)
 
 
 class ProjectListView(ListCreateAPIView):
@@ -18,4 +18,24 @@ class ProjectListView(ListCreateAPIView):
 class ProjectDetailView(RetrieveAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectDetailSerializer
+    lookup_field = 'uuid'
+
+
+class ProjectPolyaxonfileViewMixin(object):
+    def get_project(self):
+        project_uuid = self.kwargs['project_uuid']
+        return get_object_or_404(Project, uuid=project_uuid)
+
+    def filter_queryset(self, queryset):
+        return queryset.filter(movie=self.get_project())
+
+
+class ProjectPolyaxonfileListView(ListCreateAPIView, ProjectPolyaxonfileViewMixin):
+    queryset = Polyaxonfile.objects.all()
+    serializer_class = PolyaxonfileSerializer
+
+
+class ProjectPolyaxonfileDetailView(RetrieveAPIView, ProjectPolyaxonfileViewMixin):
+    queryset = Polyaxonfile.objects.all()
+    serializer_class = PolyaxonfileSerializer
     lookup_field = 'uuid'
