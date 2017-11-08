f2d28b842ef72502d04055c8a0b29072263b255f
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-08 15:52:20 +0100

    Add urls patterns utils

--- api/libs/urls.py
+++ api/libs/urls.py
@@ -1,2 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
 UUID_PATTERN = r'(?P<uuid>\w+)'
 PROJECT_UUID_PATTERN = r'(?P<project_uuid>\w+)'
+CLUSTER_UUID_PATTERN = r'(?P<cluster_uuid>\w+)'
+CLUSTER_NODE_UUID_PATTERN = r'(?P<cluster_node_uuid>\w+)'
