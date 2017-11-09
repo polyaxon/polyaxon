e7aced49066aaf268f00e54f987aaab92a9cc5fd
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-09 09:55:26 +0100

    Add experiement job status views tests

--- tests/test_experiments/test_views.py
+++ tests/test_experiments/test_views.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 
 import datetime
 
+from polyaxon_k8s.constants import JobLifeCycle
 from rest_framework import status
 
 from api.urls import API_V1
@@ -12,7 +13,6 @@ from experiments.models import (
     ExperimentStatus,
     ExperimentJob,
     ExperimentJobStatus,
-    ExperimentJobMessage,
 )
 from experiments.serializers import (
     ExperimentSerializer,
@@ -26,7 +26,6 @@ from tests.factories.factory_experiments import (
     ExperimentStatusFactory,
     ExperimentJobFactory,
     ExperimentJobStatusFactory,
-    ExperimentJobMessageFactory,
 )
 from tests.utils import BaseTest
 
@@ -349,3 +348,116 @@ class TestExperimentJobDetailViewV1(BaseTest):
         resp = self.auth_client.delete(self.url)
         assert resp.status_code == status.HTTP_200_OK
         assert self.model_class.objects.count() == 0
+
+
+class TestExperimentJobStatusListViewV1(BaseTest):
+    serializer_class = ExperimentJobStatusSerializer
+    model_class = ExperimentJobStatus
+    factory_class = ExperimentJobStatusFactory
+    num_objects = 3
+    HAS_AUTH = False
+
+    def setUp(self):
+        super().setUp()
+        self.experiment_job = ExperimentJobFactory()
+        self.url = '/{}/experiments/{}/jobs/{}/status/'.format(
+            API_V1,
+            self.experiment_job.experiment.uuid.hex,
+            self.experiment_job.uuid.hex)
+        self.objects = [self.factory_class(job=self.experiment_job,
+                                           status=JobLifeCycle.CHOICES[i][0])
+                        for i in range(self.num_objects)]
+        self.queryset = self.model_class.objects.all()
+
+    def test_get(self):
+        resp = self.auth_client.get(self.url)
+        assert resp.status_code == status.HTTP_200_OK
+
+        assert resp.data['next'] is None
+        assert resp.data['count'] == len(self.objects)
+
+        data = resp.data['results']
+        assert len(data) == self.queryset.count()
+        assert data == self.serializer_class(self.queryset, many=True).data
+
+    def test_pagination(self):
+        limit = self.num_objects - 1
+        resp = self.auth_client.get("{}?limit={}".format(self.url, limit))
+        assert resp.status_code == status.HTTP_200_OK
+
+        next = resp.data.get('next')
+        assert next is not None
+        assert resp.data['count'] == self.queryset.count()
+
+        data = resp.data['results']
+        assert len(data) == limit
+        assert data == self.serializer_class(self.queryset[:limit], many=True).data
+
+        resp = self.auth_client.get(next)
+        assert resp.status_code == status.HTTP_200_OK
+
+        assert resp.data['next'] is None
+
+        data = resp.data['results']
+        assert len(data) == 1
+        assert data == self.serializer_class(self.queryset[limit:], many=True).data
+
+    def test_create(self):
+        data = {}
+        resp = self.auth_client.post(self.url, data)
+        assert resp.status_code == status.HTTP_201_CREATED
+        assert self.model_class.objects.count() == self.num_objects + 1
+        last_object = self.model_class.objects.last()
+        assert last_object.status == JobLifeCycle.CREATED
+
+        data = {'status': JobLifeCycle.FINISHED}
+        resp = self.auth_client.post(self.url, data)
+        assert resp.status_code == status.HTTP_201_CREATED
+        assert self.model_class.objects.count() == self.num_objects + 2
+        last_object = self.model_class.objects.last()
+        assert last_object.job == self.experiment_job
+        assert last_object.status == data['status']
+
+
+class TestExperimentJobStatusDetailViewV1(BaseTest):
+    serializer_class = ExperimentJobStatusSerializer
+    model_class = ExperimentJobStatus
+    factory_class = ExperimentJobStatusFactory
+    HAS_AUTH = False
+
+    def setUp(self):
+        super().setUp()
+        self.experiment_job = ExperimentJobFactory()
+        self.object = self.factory_class(job=self.experiment_job)
+        self.url = '/{}/experiments/{}/jobs/{}/status/{}'.format(
+            API_V1,
+            self.experiment_job.experiment.uuid.hex,
+            self.experiment_job.uuid.hex,
+            self.object.uuid.hex)
+        self.queryset = self.model_class.objects.all()
+
+    def test_get(self):
+        resp = self.auth_client.get(self.url)
+        assert resp.status_code == status.HTTP_200_OK
+        assert resp.data == self.serializer_class(self.object).data
+
+    def test_patch(self):
+        data = {'message': {'reason': 'some reason'}}
+        resp = self.auth_client.patch(self.url, data=data)
+        assert resp.status_code == status.HTTP_200_OK
+        assert self.object.message is None
+        new_object = self.model_class.objects.get(id=self.object.id)
+        assert new_object.message is not None
+        assert new_object.message.reason == 'some reason'
+
+        data = {'message': {'reason': 'new reason'}}
+        resp = self.auth_client.patch(self.url, data=data)
+        assert resp.status_code == status.HTTP_200_OK
+        new_object = self.model_class.objects.get(id=self.object.id)
+        assert new_object.message.reason == 'new reason'
+
+    def test_delete(self):
+        assert self.model_class.objects.count() == 1
+        resp = self.auth_client.delete(self.url)
+        assert resp.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+        assert self.model_class.objects.count() == 1
