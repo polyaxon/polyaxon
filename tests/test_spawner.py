a8f0fb9557e271279fb9cbe504f3d17dbfd218d6
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-14 10:04:18 +0100

    Fix tests

--- tests/test_spawner.py
+++ tests/test_spawner.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, division, print_function
 import os
 from unittest import TestCase
 
-from polyaxon_spawner.spawner import K8SSpawner
+from polyaxon_spawner.spawner import K8SPolyaxonFileSpawner
 from tests import base
 
 
@@ -14,8 +14,8 @@ class TestPolyaxonSpawner(TestCase):
         cls.config = base.get_e2e_configuration()
 
     def test_spawner(self):
-        k8s_spawner = K8SSpawner(os.path.abspath('tests/fixtures/experiment_file.yml'),
-                                 self.config)
-        assert k8s_spawner.project_name == k8s_spawner.polyaxonfile.project.name
-        k8s_spawner.create_experiment()
+        k8s_spawner = K8SPolyaxonFileSpawner(os.path.abspath('tests/fixtures/experiment_file.yml'),
+                                             self.config)
+        assert k8s_spawner.project_name == k8s_spawner.spec.project.name
+        k8s_spawner.start_experiment()
         k8s_spawner.delete_experiment()
