1b1c6a1a96bcddd07d7d265fa828f5763e6ea79f
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-14 19:29:43 +0100

    Add tasks to start / schedule experiments

--- api/experiments/tasks.py
+++ api/experiments/tasks.py
@@ -3,30 +3,23 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 
+from django.conf import settings
+from polyaxon_schemas.utils import TaskType
+from polyaxon_spawner.spawner import K8SExperimentSpawner
+
 from api.settings import CeleryTasks
 from api.celery_api import app
-from experiments.task_status import ExperimentStatus
+from experiments.models import ExperimentJob
+from experiments.task_status import (
+    RedisExperimentStatus,
+)
 
 logger = logging.getLogger('polyaxon.api.experiments')
 
 
-def get_experiment_run_status(experiment):
-    job_id, status = ExperimentStatus.get_status(experiment.id)
-    return {'job_id': job_id, 'status': status}
-
-
-def start_experiment(experiment):
-    job_id, status = ExperimentStatus.get_status(experiment.id)
-    if not status or ExperimentStatus.is_final_status(status):
-        job = execute.delay(experiment.id)
-        ExperimentStatus.set_status(experiment.id, job.id, 'PENDING')
-        return {'status': 'PENDING'}
-    return {'status': status}
-
-
 @app.task(name=CeleryTasks.START_EXPERIMENT)
 def start_experiment(experiment_id):
-    from experiments.models import Experiment
+    from experiments.models import Experiment, ExperimentStatus, ExperimentLifeCycle
 
     try:
         experiment = Experiment.objects.get(id=experiment_id)
@@ -35,4 +28,24 @@ def start_experiment(experiment_id):
         return
 
     # Update experiment status to show that its started
+    ExperimentStatus.objects.create(experiment=experiment, status=ExperimentLifeCycle.SCHEDULED)
+    RedisExperimentStatus.set_status(experiment_id, ExperimentLifeCycle.SCHEDULED)
+
     # Use spawner to start the experiment
+    spawner = K8SExperimentSpawner(project_id=experiment.project.id,
+                                   spec_id=experiment.spec.id,
+                                   experiment_id=experiment.id,
+                                   specification=experiment.spec.content,
+                                   k8s_config=settings.K8S_CONFIG)
+    resp = spawner.start_experiment()
+
+    # Get the number of jobs this experiment started
+    ExperimentJob.objects.create(experiment=experiment, definition=resp[TaskType.MASTER])
+    for worker in resp[TaskType.WORKER]:
+        ExperimentJob.objects.create(experiment=experiment, definition=worker)
+        for ps in resp[TaskType.PS]:
+            ExperimentJob.objects.create(experiment=experiment, definition=ps)
+
+    # Add the experiment to the list of experiments to monitor
+    RedisExperimentStatus.monitor(experiment_id)
+
--- api/projects/tasks.py
+++ api/projects/tasks.py
@@ -3,12 +3,32 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 
+from django.conf import settings
+
 from api.settings import CeleryTasks
 from api.celery_api import app
+from experiments.tasks import start_experiment
 
 logger = logging.getLogger('polyaxon.api.experiments')
 
 
-@app.task(name=CeleryTasks.START_EXPERIMENTS)
-def start_group_experiments(spec_id):
-    pass
+@app.task(name=CeleryTasks.START_EXPERIMENTS, bind=True)
+def start_group_experiments(task, spec_id):
+    from projects.models import PolyaxonSpec
+
+    try:
+        polyaxon_spec = PolyaxonSpec.objects.get(id=spec_id)
+    except PolyaxonSpec.DoesNotExist:
+        # TODO : log
+        return
+
+    pending_experiments = list(polyaxon_spec.pending_experiments.values_list('id', flat=True))
+    experiment_to_start = polyaxon_spec.n_experiments_to_start
+    while experiment_to_start > 0 and pending_experiments:
+        experiment_id = pending_experiments.pop()
+        start_experiment.delay(experiment_id=experiment_id)
+
+    if pending_experiments:
+        # Schedule another task
+        task.apply_async(spec_id, countdown=settings.EXPERIMENTS_SCHEDULER_INTERVAL_SEC)
+
