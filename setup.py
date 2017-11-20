85446513208d228cc1b99ebffbbb0bc7107487f2
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-20 18:32:28 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class PyTest(TestCommand):
         sys.exit(errcode)
 
 setup(name='polyaxon-schemas',
-      version='0.0.15',
+      version='0.0.16',
       description='Schema definitions and validation for Polyaxon.',
       maintainer='Mourad Mourafiq',
       maintainer_email='mouradmourafiq@gmail.com',
