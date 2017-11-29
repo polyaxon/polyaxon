bdbf57171f6e18a1338b4c552042ba17b13fec81
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-29 23:55:43 +0100

    Add redis job containers

--- api/api/config_settings/redis_settings.py
+++ api/api/config_settings/redis_settings.py
@@ -5,6 +5,8 @@ from api.utils import config
 
 class RedisPools(object):
     EXPERIMENTS_STATUS = redis.ConnectionPool.from_url(
-        config.get_string("POLYAXON_REDIS_EXPERIMENTS_STATUS_URL"))
+        config.get_string('POLYAXON_REDIS_EXPERIMENTS_STATUS_URL'))
     JOBS_STATUS = redis.ConnectionPool.from_url(
-        config.get_string("POLYAXON_REDIS_JOBS_STATUS_URL"))
+        config.get_string('POLYAXON_REDIS_JOBS_STATUS_URL'))
+    JOB_CONTAINERS = redis.ConnectionPool.from_url(
+        config.get_string('POLYAXON_REDIS_JOB_CONTAINERS_URL'))
--- api/libs/redis_db.py
+++ api/libs/redis_db.py
@@ -46,7 +46,7 @@ class RedisExperimentStatus(object):
 
 
 class RedisExperimentJobStatus(object):
-    """The `RedisExperimentJobStatus` class holds information about the experiment run status."""
+    """The `RedisExperimentJobStatus` class holds information about the job run status."""
 
     REDIS_JOBS_KEY = 'JOBS'  # Redis set: jobs to monitor the status
     REDIS_JOBS_STATUS_KEY = 'JOBS_STATUS'  # Redis Hash: maps jobs to status
@@ -88,3 +88,41 @@ class RedisExperimentJobStatus(object):
     def monitor(cls, object_id):
         red = cls._get_redis()
         red.sadd(cls.REDIS_JOBS_KEY, object_id)
+
+
+class RedisJobContainers(object):
+    """Tracks containers currently running and to be monitored."""
+
+    REDIS_CONTAINERS_KEY = 'CONTAINERS'  # Redis set: container ids
+    REDIS_CONTAINERS_TO_JOBS = 'CONTAINERS_TO_JOBS'  # Redis hash, maps container id to jobs
+    REDIS_POOL = RedisPools.JOB_CONTAINERS
+
+    @classmethod
+    def _get_redis(cls):
+        return redis.Redis(connection_pool=cls.REDIS_POOL)
+
+    @classmethod
+    def get_containers(cls):
+        red = cls._get_redis()
+        container_ids = red.smembers(cls.REDIS_CONTAINERS_KEY)
+        return [container_id.decode('utf-8') for container_id in container_ids]
+
+    @classmethod
+    def get_job(cls, object_id):
+        red = cls._get_redis()
+        job_id = None
+        if red.sismember(cls.REDIS_CONTAINERS_KEY, object_id):
+            job_id = red.hget(cls.REDIS_CONTAINERS_TO_JOBS, object_id)
+        return job_id.decode('utf-8') if job_id else None
+
+    @classmethod
+    def monitor(cls, object_id, job_id):
+        red = cls._get_redis()
+        red.sadd(cls.REDIS_CONTAINERS_KEY, object_id)
+        red.hset(cls.REDIS_CONTAINERS_TO_JOBS, object_id, job_id)
+
+    @classmethod
+    def remove_container(cls, object_id):
+        red = cls._get_redis()
+        red.srem(cls.REDIS_CONTAINERS_KEY, object_id)
+        red.hdel(cls.REDIS_CONTAINERS_TO_JOBS, object_id)
