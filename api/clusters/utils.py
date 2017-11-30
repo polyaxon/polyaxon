a91a044b27f9596b31162a78987dc4141dd02c30
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-30 10:40:19 +0100

    Fix get_cluster

--- api/clusters/utils.py
+++ api/clusters/utils.py
@@ -5,7 +5,14 @@ from clusters.models import Cluster
 
 def get_cluster():
     if settings.CLUSTER_ID:
-        return Cluster.objects.get(uuid=settings.CLUSTER_ID)
+        cluster = Cluster.objects.get(uuid=settings.CLUSTER_ID)
     else:
         # Get default cluster
-        return Cluster.objects.first()
+        cluster = Cluster.objects.first()
+
+    if cluster:
+        return cluster
+
+    cluster = Cluster.objects.create(user=settings.AUTH_USER_MODEL.objects.fist(),
+                                     version_api={})
+    return cluster
