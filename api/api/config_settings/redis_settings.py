9cf29c769e3902c44914d3e216ae9457aa7e5fef
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-01 19:51:27 +0100

    Add to stream redis db

--- api/api/config_settings/redis_settings.py
+++ api/api/config_settings/redis_settings.py
@@ -10,3 +10,5 @@ class RedisPools(object):
         config.get_string('POLYAXON_REDIS_JOBS_STATUS_URL'))
     JOB_CONTAINERS = redis.ConnectionPool.from_url(
         config.get_string('POLYAXON_REDIS_JOB_CONTAINERS_URL'))
+    TO_STREAM = redis.ConnectionPool.from_url(
+        config.get_string('POLYAXON_REDIS_TO_STREAM_URL'))
