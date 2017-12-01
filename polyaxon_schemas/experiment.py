6b75637f74475785b20b54e3713fa17a96580415
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-01 20:55:00 +0100

    Add schemas for pod/job states

--- polyaxon_schemas/experiment.py
+++ polyaxon_schemas/experiment.py
@@ -30,3 +30,94 @@ class ExperimentConfig(BaseConfig):
         self.uuid = uuid
         self.project = project
         self.description = description
+
+
+class JobLabelSchema(Schema):
+    project = fields.Str()
+    experiment = fields.Str()
+    task_type = fields.Str()
+    task_idx = fields.Str()
+    task = fields.Str()
+    task_id = fields.Str()
+    role = fields.Str()
+    type = fields.Str()
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make(self, data):
+        return JobLabelConfig(**data)
+
+
+class JobLabelConfig(BaseConfig):
+    SCHEMA = JobLabelSchema
+    IDENTIFIER = 'JobLabel'
+
+    def __init__(self, project, experiment, task_type, task_idx, task, task_id, role, type):
+        self.project = project
+        self.experiment = experiment
+        self.task_type = task_type
+        self.task_idx = task_idx
+        self.task = task
+        self.task_id = task_id
+        self.role = role
+        self.type = type
+
+
+class PodStateSchema(Schema):
+    event_type = fields.Str()
+    labels = fields.Nested(JobLabelSchema)
+    phase = fields.Str()
+    deletion_timestamp = fields.DateTime()
+    pod_conditions = fields.Dict()
+    container_statuses = fields.Dict()
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make(self, data):
+        return PodStateConfig(**data)
+
+
+class PodStateConfig(BaseConfig):
+    SCHEMA = PodStateSchema
+    IDENTIFIER = 'PodState'
+
+    def __init__(self,
+                 event_type,
+                 labels,
+                 phase,
+                 deletion_timestamp,
+                 pod_conditions,
+                 container_statuses):
+        self.event_type = event_type
+        self.labels = labels
+        self.phase = phase
+        self.deletion_timestamp = deletion_timestamp
+        self.pod_conditions = pod_conditions
+        self.container_statuses = container_statuses
+
+
+class JobStateSchema(Schema):
+    status = fields.Str()
+    message = fields.Str()
+    details = fields.Nested(PodStateSchema)
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make(self, data):
+        return JobStateConfig(**data)
+
+
+class JobStateConfig(BaseConfig):
+    SCHEMA = JobStateSchema
+    IDENTIFIER = 'JobState'
+
+    def __init__(self, status, message, details):
+        self.status = status
+        self.message = message
+        self.details = details
--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -4,11 +4,79 @@ from __future__ import absolute_import, division, print_function
 import uuid
 from unittest import TestCase
 
-from polyaxon_schemas.experiment import ExperimentConfig
+from datetime import datetime
+
+from polyaxon_schemas.experiment import (
+    ExperimentConfig,
+    JobLabelConfig,
+    PodStateConfig,
+    JobStateConfig,
+)
+from polyaxon_schemas.polyaxonfile.constants import TASK_NAME
 
 
-class TestProjectConfigs(TestCase):
-    def test_project_config(self):
+class TestExperimentConfigs(TestCase):
+    def test_experiment_config(self):
         config_dict = {'name': 'test', 'uuid': str(uuid.uuid4()), 'project': str(uuid.uuid4())}
         config = ExperimentConfig.from_dict(config_dict)
         assert config.to_dict() == config_dict
+
+    @staticmethod
+    def create_pod_labels():
+        project = 'test-id1'
+        experiment = str(uuid.uuid4())
+        task_type = 'master'
+        return {'project': project,
+                'experiment': experiment,
+                'task_type': task_type,
+                'task_idx': '0',
+                'task': TASK_NAME.format(project='test-id1',
+                                         experiment=experiment,
+                                         task_type=task_type,
+                                         task_idx='0'),
+                'task_id': str(uuid.uuid4()),
+                'role': 'polyaxon-worker',
+                'type': 'polyaxon-experiment'
+                }
+
+    @classmethod
+    def create_pod_state(cls):
+        event_type = 'ADDED'
+        phase = 'Running'
+        labels = cls.create_pod_labels()
+        deletion_timestamp = datetime.now().isoformat()
+        pod_conditions = {}
+        container_statuses = {}
+        return {
+            'event_type': event_type,
+            'phase': phase,
+            'labels': labels,
+            'deletion_timestamp': deletion_timestamp,
+            'pod_conditions': pod_conditions,
+            'container_statuses': container_statuses,
+        }
+
+    def test_pod_label_config(self):
+        config_dict = self.create_pod_labels()
+        config = JobLabelConfig.from_dict(config_dict)
+        assert config.to_dict() == config_dict
+
+    def test_pod_state_config(self):
+        config_dict = self.create_pod_state()
+        config = PodStateConfig.from_dict(config_dict)
+        config_to_dict = config.to_dict()
+        config_to_dict.pop('deletion_timestamp')
+        config_dict.pop('deletion_timestamp')
+        assert config_to_dict == config_dict
+
+    def test_jpn_state_config(self):
+        config_dict = {
+            'status': 'Running',
+            'message': 'something',
+            'details': self.create_pod_state()
+        }
+        config = JobStateConfig.from_dict(config_dict)
+        config_to_dict = config.to_dict()
+        config_to_dict['details'].pop('deletion_timestamp')
+        config_dict['details'].pop('deletion_timestamp')
+        assert config_to_dict == config_dict
