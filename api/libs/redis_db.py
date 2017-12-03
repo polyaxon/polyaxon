3ef26134a9cf4116c56c67d269aa80bf27a6cd26
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-03 23:29:17 +0100

    Update redis dbs to use uuids instead of ids

--- api/libs/redis_db.py
+++ api/libs/redis_db.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-from polyaxon_k8s.constants import JobLifeCycle, ExperimentLifeCycle
 
 from api.settings import RedisPools, redis
 from experiments.models import ExperimentStatus, ExperimentJobStatus
+from spawner.utils.constants import JobLifeCycle, ExperimentLifeCycle
 
 
 class BaseRedisDb(object):
@@ -20,34 +20,35 @@ class RedisExperimentStatus(BaseRedisDb):
 
     KEY_EXPERIMENTS = 'EXPERIMENTS'  # Redis set: experiments to monitor the status
     KEY_EXPERIMENTS_STATUS = 'EXPERIMENTS_STATUS'  # Redis Hash: maps experiments to status
+
     REDIS_POOL = RedisPools.EXPERIMENTS_STATUS  # Redis pool
 
     @classmethod
-    def get_status(cls, object_id):
+    def get_status(cls, experiment_uuid):
         """Return the status for an experiment given the id."""
         red = cls._get_redis()
-        if red.sismember(cls.KEY_EXPERIMENTS, object_id):
-            status = red.hget(cls.KEY_EXPERIMENTS_STATUS, object_id)
+        if red.sismember(cls.KEY_EXPERIMENTS, experiment_uuid):
+            status = red.hget(cls.KEY_EXPERIMENTS_STATUS, experiment_uuid)
             return status
 
     @classmethod
-    def set_status(cls, object_id, status):
+    def set_status(cls, experiment_uuid, status):
         red = cls._get_redis()
-        current_status = cls.get_status(object_id=object_id)
+        current_status = cls.get_status(experiment_uuid=experiment_uuid)
         if status != current_status:
-            red.hset(cls.KEY_EXPERIMENTS_STATUS, object_id, status)
+            red.hset(cls.KEY_EXPERIMENTS_STATUS, experiment_uuid, status)
             # Add new status to the experiment
-            ExperimentStatus.objects.create(experiment_id=object_id, status=status)
+            ExperimentStatus.objects.create(experiment__uuid=experiment_uuid, status=status)
             # Check if we need to remove this experiment from the set to monitor
             if ExperimentLifeCycle.is_done(status):
-                red.srem(cls.KEY_EXPERIMENTS, object_id)
+                red.srem(cls.KEY_EXPERIMENTS, experiment_uuid)
             return True
         return False
 
     @classmethod
-    def monitor(cls, object_id):
+    def monitor(cls, experiment_uuid):
         red = cls._get_redis()
-        red.sadd(cls.KEY_EXPERIMENTS, object_id)
+        red.sadd(cls.KEY_EXPERIMENTS, experiment_uuid)
 
 
 class RedisExperimentJobStatus(BaseRedisDb):
@@ -56,43 +57,58 @@ class RedisExperimentJobStatus(BaseRedisDb):
     KEY_JOBS = 'JOBS'  # Redis set: jobs to monitor the status
     KEY_JOBS_STATUS = 'JOBS_STATUS'  # Redis Hash: maps jobs to status
     KEY_JOBS_TO_EXPERIMENTS = 'JOBS_TO_EXPERIMENTS'  # Redis hash, maps jobs to experiments
+    KEYF_JOBS_TO_CONTAINERS = 'JOBS_TO_CONTAINERS:{}'  # Redis set, maps jobs to containers
+
     REDIS_POOL = RedisPools.JOBS_STATUS  # Redis pool
 
     @classmethod
-    def get_status(cls, object_id):
+    def get_status(cls, job_uuid):
         """Return the status for an job given the id."""
         red = cls._get_redis()
-        if red.sismember(cls.KEY_JOBS, object_id):
-            status = red.hget(cls.KEY_JOBS_STATUS, object_id)
-            return status
+        if red.sismember(cls.KEY_JOBS, job_uuid):
+            status = red.hget(cls.KEY_JOBS_STATUS, job_uuid)
+            return status.decode('utf-8')
 
     @classmethod
-    def get_experiment(cls, object_id):
+    def get_experiment(cls, job_uuid):
         red = cls._get_redis()
-        return red.hget(cls.KEY_JOBS_TO_EXPERIMENTS, object_id)
+        experiment_uuid = red.hget(cls.KEY_JOBS_TO_EXPERIMENTS, job_uuid)
+        return experiment_uuid.decode('utf-8') if experiment_uuid else None
 
     @classmethod
-    def set_status(cls, object_id, status, message=None, details=None):
+    def set_status(cls, job_uuid, status, message=None, details=None):
         red = cls._get_redis()
-        current_status = cls.get_status(object_id=object_id)
+        current_status = cls.get_status(job_uuid=job_uuid)
         if status != current_status:
-            red.hset(cls.KEY_JOBS_STATUS, object_id, status)
+            red.hset(cls.KEY_JOBS_STATUS, job_uuid, status)
             # Add new status to the job
-            ExperimentJobStatus.objects.create(job_id=object_id,
+            ExperimentJobStatus.objects.create(job__uuid=job_uuid,
                                                status=status,
                                                message=message,
                                                details=details)
             # Check if we need to remove this job from the set to monitor
             if JobLifeCycle.is_done(status):
-                red.srem(cls.KEY_JOBS, object_id)
-                check_experiment_status.delay(experiment_id)
+                red.srem(cls.KEY_JOBS, job_uuid)
+                # we need also to remove containers monitoring related to the job
+                key_jobs_to_containers = cls.KEYF_JOBS_TO_CONTAINERS.format(job_uuid)
+                containers = red.sismember(key_jobs_to_containers, job_uuid)
+                for container_id in containers:
+                    container_id = container_id.decode('utf-8')
+                    red.srem(key_jobs_to_containers, container_id)
+                    RedisJobContainers.remove_container(container_id=container_id)
             return True
         return False
 
     @classmethod
-    def monitor(cls, object_id):
+    def add_container_for_job(cls, job_uuid, container_id):
         red = cls._get_redis()
-        red.sadd(cls.KEY_JOBS, object_id)
+        red.sadd(cls.KEYF_JOBS_TO_CONTAINERS.format(job_uuid), container_id)
+
+    @classmethod
+    def monitor(cls, job_uuid, experiment_uuid):
+        red = cls._get_redis()
+        red.sadd(cls.KEY_JOBS, job_uuid)
+        red.hset(cls.KEY_JOBS_TO_EXPERIMENTS, job_uuid, experiment_uuid)
 
 
 class RedisJobContainers(BaseRedisDb):
@@ -110,24 +126,25 @@ class RedisJobContainers(BaseRedisDb):
         return [container_id.decode('utf-8') for container_id in container_ids]
 
     @classmethod
-    def get_job(cls, object_id):
+    def get_job(cls, container_id):
         red = cls._get_redis()
-        job_id = None
-        if red.sismember(cls.KEY_CONTAINERS, object_id):
-            job_id = red.hget(cls.KEY_CONTAINERS_TO_JOBS, object_id)
-        return job_id.decode('utf-8') if job_id else None
+        job_uuid = None
+        if red.sismember(cls.KEY_CONTAINERS, container_id):
+            job_uuid = red.hget(cls.KEY_CONTAINERS_TO_JOBS, container_id)
+        return job_uuid.decode('utf-8') if job_uuid else None
 
     @classmethod
-    def monitor(cls, object_id, job_id):
+    def monitor(cls, container_id, job_uuid):
         red = cls._get_redis()
-        red.sadd(cls.KEY_CONTAINERS, object_id)
-        red.hset(cls.KEY_CONTAINERS_TO_JOBS, object_id, job_id)
+        red.sadd(cls.KEY_CONTAINERS, container_id)
+        red.hset(cls.KEY_CONTAINERS_TO_JOBS, container_id, job_uuid)
+        RedisExperimentJobStatus.add_container_for_job(job_uuid=job_uuid, container_id=container_id)
 
     @classmethod
-    def remove_container(cls, object_id):
+    def remove_container(cls, container_id):
         red = cls._get_redis()
-        red.srem(cls.KEY_CONTAINERS, object_id)
-        red.hdel(cls.KEY_CONTAINERS_TO_JOBS, object_id)
+        red.srem(cls.KEY_CONTAINERS, container_id)
+        red.hdel(cls.KEY_CONTAINERS_TO_JOBS, container_id)
 
 
 class RedisToStream(BaseRedisDb):
@@ -151,20 +168,20 @@ class RedisToStream(BaseRedisDb):
         red.sadd(key, object_id)
 
     @classmethod
-    def monitor_job_resources(cls, job):
-        cls._monitor(cls.KEY_JOB_RESOURCES, job)
+    def monitor_job_resources(cls, job_uuid):
+        cls._monitor(cls.KEY_JOB_RESOURCES, job_uuid)
 
     @classmethod
-    def monitor_job_logs(cls, job):
-        cls._monitor(cls.KEY_JOB_LOGS, job)
+    def monitor_job_logs(cls, job_uuid):
+        cls._monitor(cls.KEY_JOB_LOGS, job_uuid)
 
     @classmethod
-    def monitor_experiment_resources(cls, experiment):
-        cls._monitor(cls.KEY_EXPERIMENT_RESOURCES, experiment)
+    def monitor_experiment_resources(cls, experiment_uuid):
+        cls._monitor(cls.KEY_EXPERIMENT_RESOURCES, experiment_uuid)
 
     @classmethod
-    def monitor_experiment_logs(cls, experiment):
-        cls._monitor(cls.KEY_EXPERIMENT_LOGS, experiment)
+    def monitor_experiment_logs(cls, experiment_uuid):
+        cls._monitor(cls.KEY_EXPERIMENT_LOGS, experiment_uuid)
 
     @classmethod
     def _is_monitored(cls, key, object_id):
@@ -172,20 +189,20 @@ class RedisToStream(BaseRedisDb):
         return red.sismember(key, object_id)
 
     @classmethod
-    def is_monitored_job_resources(cls, job):
-        return cls._is_monitored(cls.KEY_JOB_RESOURCES, job)
+    def is_monitored_job_resources(cls, job_uuid):
+        return cls._is_monitored(cls.KEY_JOB_RESOURCES, job_uuid)
 
     @classmethod
-    def is_monitored_job_logs(cls, job):
-        return cls._is_monitored(cls.KEY_JOB_LOGS, job)
+    def is_monitored_job_logs(cls, job_uuid):
+        return cls._is_monitored(cls.KEY_JOB_LOGS, job_uuid)
 
     @classmethod
-    def is_monitored_experiment_resources(cls, experiment):
-        return cls._is_monitored(cls.KEY_EXPERIMENT_RESOURCES, experiment)
+    def is_monitored_experiment_resources(cls, experiment_uuid):
+        return cls._is_monitored(cls.KEY_EXPERIMENT_RESOURCES, experiment_uuid)
 
     @classmethod
-    def is_monitored_experiment_logs(cls, experiment):
-        return cls._is_monitored(cls.KEY_EXPERIMENT_LOGS, experiment)
+    def is_monitored_experiment_logs(cls, experiment_uuid):
+        return cls._is_monitored(cls.KEY_EXPERIMENT_LOGS, experiment_uuid)
 
     @classmethod
     def _remove_object(cls, key, object_id):
@@ -193,20 +210,20 @@ class RedisToStream(BaseRedisDb):
         red.srem(key, object_id)
 
     @classmethod
-    def remove_job_resources(cls, job):
-        cls._remove_object(cls.KEY_JOB_RESOURCES, job)
+    def remove_job_resources(cls, job_uuid):
+        cls._remove_object(cls.KEY_JOB_RESOURCES, job_uuid)
 
     @classmethod
-    def remove_job_logs(cls, job):
-        cls._remove_object(cls.KEY_JOB_LOGS, job)
+    def remove_job_logs(cls, job_uuid):
+        cls._remove_object(cls.KEY_JOB_LOGS, job_uuid)
 
     @classmethod
-    def remove_experiment_resources(cls, experiment):
-        cls._remove_object(cls.KEY_EXPERIMENT_RESOURCES, experiment)
+    def remove_experiment_resources(cls, experiment_uuid):
+        cls._remove_object(cls.KEY_EXPERIMENT_RESOURCES, experiment_uuid)
 
     @classmethod
-    def remove_experiment_logs(cls, experiment):
-        cls._remove_object(cls.KEY_EXPERIMENT_LOGS, experiment)
+    def remove_experiment_logs(cls, experiment_uuid):
+        cls._remove_object(cls.KEY_EXPERIMENT_LOGS, experiment_uuid)
 
     @classmethod
     def get_latest_job_resources(cls, job):
