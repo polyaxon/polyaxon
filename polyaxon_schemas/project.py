5447b79701958a0a0ee81c8898539c56c5dbea72
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-08 03:43:34 +0100

    Add polyaxon spec schema

--- polyaxon_schemas/project.py
+++ polyaxon_schemas/project.py
@@ -5,7 +5,32 @@ from marshmallow import Schema, fields, post_load
 from marshmallow import validate
 
 from polyaxon_schemas.base import BaseConfig
+from polyaxon_schemas.experiment import ExperimentSchema
 from polyaxon_schemas.utils import UUID
+
+
+class PolyaxonSpecSchema(Schema):
+    uuid = UUID(allow_none=True)
+    content = fields.Str()
+    project = UUID(allow_none=True)
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make(self, data):
+        return PolyaxonSpecConfig(**data)
+
+
+class PolyaxonSpecConfig(BaseConfig):
+    SCHEMA = PolyaxonSpecSchema
+    IDENTIFIER = 'spec'
+    REDUCED_ATTRIBUTES = ['uuid', 'project']
+
+    def __init__(self, content=None, uuid=None, project=None):
+        self.content = content
+        self.uuid = uuid
+        self.project = project
 
 
 class ProjectSchema(Schema):
@@ -13,6 +38,8 @@ class ProjectSchema(Schema):
     uuid = UUID(allow_none=True)
     description = fields.Str(allow_none=True)
     is_public = fields.Boolean(allow_none=True)
+    experiments = fields.Nested(ExperimentSchema, many=True, allow_none=True)
+    specs = fields.Nested(PolyaxonSpecSchema, many=True, allow_none=True)
 
     class Meta:
         ordered = True
@@ -25,10 +52,18 @@ class ProjectSchema(Schema):
 class ProjectConfig(BaseConfig):
     SCHEMA = ProjectSchema
     IDENTIFIER = 'project'
-    REDUCED_ATTRIBUTES = ['uuid', 'description']
+    REDUCED_ATTRIBUTES = ['uuid', 'description', 'experiments', 'specs']
 
-    def __init__(self, name, uuid=None, description=None, is_public=True):
+    def __init__(self,
+                 name,
+                 uuid=None,
+                 description=None,
+                 is_public=True,
+                 experiments=None,
+                 specs=None):
         self.name = name
         self.uuid = uuid
         self.description = description
         self.is_public = is_public
+        self.experiments = experiments
+        self.specs = specs
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+import uuid
 from unittest import TestCase
 
 from marshmallow import ValidationError
 
-from polyaxon_schemas.project import ProjectConfig
+from polyaxon_schemas.project import ProjectConfig, PolyaxonSpecConfig
 
 
 class TestProjectConfigs(TestCase):
@@ -17,4 +18,11 @@ class TestProjectConfigs(TestCase):
     def test_project_config(self):
         config_dict = {'name': 'test', 'description': '', 'is_public': True}
         config = ProjectConfig.from_dict(config_dict)
+        assert config.to_dict() == config_dict
+
+    def test_spec_config(self):
+        config_dict = {'content': 'some content',
+                       'uuid': uuid.uuid4().hex,
+                       'project': uuid.uuid4().hex}
+        config = PolyaxonSpecConfig.from_dict(config_dict)
         assert config.to_dict() == config_dict
