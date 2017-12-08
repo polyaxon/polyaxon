e1cdb25dd3b45285d27e300fcebd6a3b02299cd3
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-08 15:41:53 +0100

    Add token fixture to setup commad

--- api/users/signals.py
+++ api/users/signals.py
@@ -9,5 +9,9 @@ from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if kwargs.get('raw'):
+        # Ignore signal handling for fixture loading
+        return
+
     if created:
         Token.objects.create(user=instance)
