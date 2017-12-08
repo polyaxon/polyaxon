2c1de9fe9387f4d909c7a69dc8411b34f5759ecd
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-08 02:19:41 +0100

    Fix auth client

--- polyaxon_client/auth.py
+++ polyaxon_client/auth.py
@@ -2,7 +2,6 @@
 from __future__ import absolute_import, division, print_function
 
 import requests
-
 from polyaxon_schemas.user import UserConfig
 
 from polyaxon_client.base import PolyaxonClient
@@ -10,14 +9,15 @@ from polyaxon_client.exceptions import AuthenticationError
 
 
 class AuthClient(PolyaxonClient):
-    """Auth/User specific client"""
-    ENDPOINT = "/user"
+    """Auth/User specific client
+
+    Special case client because of the token is not acquired yet,
+    so we do not use shared client functions.
+    """
+    ENDPOINT = "/users"
 
     def get_user(self):
         request_url = self._get_url()
-        # This is a special case client,
-        # because auth_token is not set yet (this is how we verify it)
-        # So do not use the shared base client for this!
         response = self.get(request_url,
                             headers={"Authorization": "{} {}".format(
                                 self.authentication_type, self.token)})
@@ -38,3 +38,24 @@ class AuthClient(PolyaxonClient):
                 response.status_code)
 
         return UserConfig.from_dict(user_dict)
+
+    def login(self, credentials):
+        request_url = self._build_url(self._get_url(), 'token')
+        response = requests.post(request_url, json=credentials.to_dict())
+        try:
+            token_dict = response.json()
+            response.raise_for_status()
+        except Exception:
+            if response.status_code == 401:
+                raise AuthenticationError(
+                    request_url,
+                    response,
+                    "Invalid credentials.",
+                    401)
+            raise AuthenticationError(
+                request_url,
+                response,
+                "Login failed.\nSee http://docs.polyaxon.com/faqs/authentication/ for help",
+                response.status_code)
+
+        return token_dict.get(self.authentication_type)
