4c315cd8fa0e33e49bb9e18eab2db3a3486efe72
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-09 16:56:52 +0100

    Add missing projects tests

--- tests/test_project.py
+++ tests/test_project.py
@@ -2,12 +2,14 @@
 from __future__ import absolute_import, division, print_function
 
 import json
+import tempfile
 import uuid
 from unittest import TestCase
 import httpretty
 from faker import Faker
+from polyaxon_schemas.experiment import ExperimentConfig
 
-from polyaxon_schemas.project import ProjectConfig
+from polyaxon_schemas.project import ProjectConfig, PolyaxonSpecConfig
 
 from polyaxon_client.project import ProjectClient
 
@@ -15,7 +17,6 @@ faker = Faker()
 
 
 class TestProjectClient(TestCase):
-
     def setUp(self):
         self.client = ProjectClient(host='http://localhost', version='v1')
         self.base_url = ProjectClient.BASE_URL.format('http://localhost', 'v1')
@@ -29,7 +30,8 @@ class TestProjectClient(TestCase):
                 self.base_url,
                 ProjectClient.ENDPOINT),
             body=json.dumps({'results': projects, 'count': 10, 'next': None}),
-            content_type='application/json', status=200)
+            content_type='application/json',
+            status=200)
 
         projects = self.client.list_projects()
         assert len(projects) == 10
@@ -44,7 +46,8 @@ class TestProjectClient(TestCase):
                 ProjectClient.ENDPOINT,
                 'uuid'),
             body=json.dumps(object),
-            content_type='application/json', status=200)
+            content_type='application/json',
+            status=200)
         result = self.client.get_project('uuid')
         assert object == result.to_dict()
 
@@ -57,7 +60,8 @@ class TestProjectClient(TestCase):
                 self.base_url,
                 ProjectClient.ENDPOINT),
             body=json.dumps(object.to_dict()),
-            content_type='application/json', status=200)
+            content_type='application/json',
+            status=200)
         result = self.client.create_project(object)
         assert result.to_dict() == object.to_dict()
 
@@ -72,7 +76,8 @@ class TestProjectClient(TestCase):
                 ProjectClient.ENDPOINT,
                 project_uuid),
             body=json.dumps(object.to_dict()),
-            content_type='application/json', status=200)
+            content_type='application/json',
+            status=200)
         result = self.client.update_project(project_uuid, {'name': 'new'})
         assert result.to_dict() == object.to_dict()
 
@@ -85,6 +90,134 @@ class TestProjectClient(TestCase):
                 self.base_url,
                 ProjectClient.ENDPOINT,
                 project_uuid),
-            content_type='application/json', status=204)
+            content_type='application/json',
+            status=204)
         result = self.client.delete_project(project_uuid)
         assert result.status_code == 204
+
+    @httpretty.activate
+    def test_upload_repo(self):
+        project_uuid = uuid.uuid4().hex
+        httpretty.register_uri(
+            httpretty.PUT,
+            ProjectClient._build_url(
+                self.base_url,
+                ProjectClient.ENDPOINT,
+                project_uuid,
+                'repo',
+                'upload'),
+            content_type='application/json',
+            status=204)
+        files = [('code', ('repo',
+                           open('./tests/fixtures_static/repo.tar.gz', 'rb'),
+                           'text/plain'))]
+        result = self.client.upload_repo(project_uuid, files=files, files_size=10)
+        assert result.status_code == 204
+
+    @httpretty.activate
+    def test_list_specs(self):
+        project_uuid = uuid.uuid4().hex
+        specs = [PolyaxonSpecConfig(content=faker.word, project=project_uuid).to_dict()
+                 for _ in range(10)]
+        httpretty.register_uri(
+            httpretty.GET,
+            ProjectClient._build_url(
+                self.base_url,
+                ProjectClient.ENDPOINT,
+                project_uuid,
+                'specs'),
+            body=json.dumps({'results': specs, 'count': 10, 'next': None}),
+            content_type='application/json',
+            status=200)
+
+        specs = self.client.list_specs(project_uuid)
+        assert len(specs) == 10
+
+    @httpretty.activate
+    def test_create_spec(self):
+        project_uuid = uuid.uuid4().hex
+        object = PolyaxonSpecConfig(content=faker.word(), project=project_uuid)
+        httpretty.register_uri(
+            httpretty.POST,
+            ProjectClient._build_url(
+                self.base_url,
+                ProjectClient.ENDPOINT,
+                project_uuid,
+                'specs'),
+            body=json.dumps(object.to_dict()),
+            content_type='application/json',
+            status=200)
+        result = self.client.create_spec(project_uuid, object)
+        assert result.to_dict() == object.to_dict()
+
+    @httpretty.activate
+    def test_update_project(self):
+        object = PolyaxonSpecConfig(faker.word(), uuid=uuid.uuid4().hex, project=uuid.uuid4().hex)
+        spec_uuid = object.uuid
+        project_uuid = object.project
+        httpretty.register_uri(
+            httpretty.PATCH,
+            ProjectClient._build_url(
+                self.base_url,
+                ProjectClient.ENDPOINT,
+                project_uuid,
+                'specs',
+                spec_uuid),
+            body=json.dumps(object.to_dict()),
+            content_type='application/json',
+            status=200)
+        result = self.client.update_spec(project_uuid, spec_uuid, {'content': 'new'})
+        assert result.to_dict() == object.to_dict()
+
+    @httpretty.activate
+    def test_delete_spec(self):
+        project_uuid = uuid.uuid4().hex
+        spec_uuid = uuid.uuid4().hex
+        httpretty.register_uri(
+            httpretty.DELETE,
+            ProjectClient._build_url(
+                self.base_url,
+                ProjectClient.ENDPOINT,
+                project_uuid,
+                'specs',
+                spec_uuid),
+            content_type='application/json',
+            status=204)
+        result = self.client.delete_spec(project_uuid, spec_uuid)
+        assert result.status_code == 204
+
+    @httpretty.activate
+    def test_list_experiments(self):
+        project_uuid = uuid.uuid4().hex
+        xp_uuid = uuid.uuid4().hex
+        xps = [ExperimentConfig(name='xp', uuid=xp_uuid, project=project_uuid).to_dict()
+               for _ in range(10)]
+        httpretty.register_uri(
+            httpretty.GET,
+            ProjectClient._build_url(
+                self.base_url,
+                ProjectClient.ENDPOINT,
+                project_uuid,
+                'experiments'),
+            body=json.dumps({'results': xps, 'count': 10, 'next': None}),
+            content_type='application/json',
+            status=200)
+
+        xps_results = self.client.list_experiments(project_uuid)
+        assert len(xps_results) == 10
+
+        # pagination
+
+        httpretty.register_uri(
+            httpretty.GET,
+            ProjectClient._build_url(
+                self.base_url,
+                ProjectClient.ENDPOINT,
+                project_uuid,
+                'experiments') + '?offset=2',
+            body=json.dumps({'results': xps, 'count': 10, 'next': None}),
+            content_type='application/json',
+            status=200)
+
+        xps_results = self.client.list_experiments(project_uuid, page=2)
+        assert len(xps_results) == 10
