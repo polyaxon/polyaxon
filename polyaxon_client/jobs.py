407390904eb93a4d9135e65e939b8519ca25b069
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-11 01:42:09 +0100

    Add job client

---
+++ polyaxon_client/jobs.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from polyaxon_schemas.experiment import (
+    ExperimentJobStatusConfig,
+    ExperimentJobConfig,
+)
+
+from polyaxon_client.base import PolyaxonClient
+from polyaxon_client.exceptions import PolyaxonException
+
+
+class JobClient(PolyaxonClient):
+    """Client to get jobs from the server"""
+    ENDPOINT = "/jobs"
+
+    def get_job(self, job_uuid):
+        request_url = self._build_url(self._get_http_url(), job_uuid)
+        try:
+            response = self.get(request_url)
+            return ExperimentJobConfig.from_dict(response.json())
+        except PolyaxonException as e:
+            self.handle_exception(e=e, log_message='Error while retrieving job')
+            return None
+
+    def get_job_status(self, job_uuid):
+        request_url = self._build_url(self._get_http_url(), job_uuid, 'status')
+
+        try:
+            response = self.get(request_url)
+            return ExperimentJobStatusConfig.from_dict(response.json())
+        except PolyaxonException as e:
+            self.handle_exception(e=e, log_message='Error while retrieving job status')
+            return []
+
+    def resources(self, job_uuid, message_handler=None):
+        """Streams job resources using websockets.
+
+        message_handler: handles the messages received from server.
+            e.g. def f(x): print(x)
+        """
+        request_url = self._build_url(self._get_ws_url(), job_uuid, 'resources')
+        self.socket(request_url, message_handler=message_handler)
+
+    def logs(self, job_uuid, message_handler=None):
+        """Streams job logs using websockets.
+
+        message_handler: handles the messages received from server.
+            e.g. def f(x): print(x)
+        """
+        request_url = self._build_url(self._get_ws_url(), job_uuid, 'logs')
+        self.socket(request_url, message_handler=message_handler)
---
+++ tests/test_jobs.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import json
+import uuid
+from unittest import TestCase
+
+import datetime
+import httpretty
+from faker import Faker
+from polyaxon_schemas.experiment import ExperimentJobStatusConfig, ExperimentJobConfig
+from polyaxon_client.jobs import JobClient
+
+faker = Faker()
+
+
+class TestJobClient(TestCase):
+    def setUp(self):
+        self.client = JobClient(host='localhost',
+                                http_port=8000,
+                                ws_port=1337,
+                                version='v1',
+                                token=faker.uuid4(),
+                                reraise=True)
+
+    @httpretty.activate
+    def test_get_job(self):
+        experiment_uuid = uuid.uuid4().hex
+        object = ExperimentJobConfig(uuid=uuid.uuid4().hex,
+                                     experiment=experiment_uuid,
+                                     created_at=datetime.datetime.now(),
+                                     definition='').to_dict()
+        httpretty.register_uri(
+            httpretty.GET,
+            JobClient._build_url(
+                self.client.base_url,
+                JobClient.ENDPOINT,
+                'uuid'),
+            body=json.dumps(object),
+            content_type='application/json',
+            status=200)
+        result = self.client.get_job('uuid')
+        assert object == result.to_dict()
+
+    @httpretty.activate
+    def test_get_experiment_job_status(self):
+        job_uuid = uuid.uuid4().hex
+        object = ExperimentJobStatusConfig(uuid=uuid.uuid4().hex,
+                                           job=job_uuid,
+                                           created_at=datetime.datetime.now(),
+                                           status='Running').to_dict()
+        httpretty.register_uri(
+            httpretty.GET,
+            JobClient._build_url(
+                self.client.base_url,
+                JobClient.ENDPOINT,
+                job_uuid,
+                'status'),
+            body=json.dumps(object),
+            content_type='application/json',
+            status=200)
+        result = self.client.get_job_status(job_uuid)
+        assert object == result.to_dict()
