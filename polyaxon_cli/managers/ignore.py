7fbe630c7345f8465cb07dc65c1c1ef234727e0e
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-14 02:24:57 +0100

    Update ignore manager

--- polyaxon_cli/managers/ignore.py
+++ polyaxon_cli/managers/ignore.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
+from pathlib import PurePath
+
 from polyaxon_cli.logger import logger
 from polyaxon_cli.managers.base import BaseConfigManager
 from polyaxon_cli.utils.constants import DEFAULT_IGNORE_LIST
-from polyaxon_cli.utils.files import matches_glob_list, unix_style_path
+from polyaxon_cli.utils.files import unix_style_path
 
 
 class IgnoreManager(BaseConfigManager):
@@ -18,15 +20,33 @@ class IgnoreManager(BaseConfigManager):
     def init_config(cls):
         cls.set_config(DEFAULT_IGNORE_LIST)
 
+    @staticmethod
+    def _is_empty_or_comment(line):
+        return not line or line.startswith('#')
+
+    @classmethod
+    def _get_whitelisted(cls, line):
+        return cls._remove_slash_prefix(line[1:]) if line.startswith('!') else None
+
+    @classmethod
+    def _unescape(cls, line):
+        """allow escaping file names that start with !, #, or \ """
+        line = line[1:] if line.startswith('\\') else line
+        return cls._remove_slash_prefix(line)
+
+    @staticmethod
+    def _remove_slash_prefix(line):
+        """For details on the glob matcher,
+        see: https://docs.python.org/3/library/pathlib.html#pathlib.PurePath.match
+        """
+        return line[1:] if line.startswith('/') else line
+
     @classmethod
     def get_config(cls):
-        # For details on the glob matcher, see:
-        # https://docs.python.org/3/library/pathlib.html#pathlib.PurePath.match
-        def trim_slash_prefix(path):
-            if path.startswith('/'):
-                return line[1:]
-            return line
+        """This is a different config manager since it does not rely on a json object.
 
+        This config return 2 lists representing the ignore_list and white_list of files.
+        """
         config_file_path = cls.get_config_file_path()
 
         if not os.path.isfile(config_file_path):
@@ -36,20 +56,15 @@ class IgnoreManager(BaseConfigManager):
         with open(config_file_path, "r") as ignore_file:
             for line in ignore_file:
                 line = line.strip()
-                if not line or line.startswith('#'):
+                if cls._is_empty_or_comment(line):
                     continue
 
-                if line.startswith('!'):
-                    line = line[1:]
-                    white_list.append(trim_slash_prefix(line))
+                white_list_line = cls._get_whitelisted(line)
+                if white_list_line:
+                    white_list.append(white_list_line)
                     continue
 
-                # To allow escaping file names that start with !, #, or \,
-                # remove the escaping \
-                if line.startswith('\\'):
-                    line = line[1:]
-
-                ignore_list.append(trim_slash_prefix(line))
+                ignore_list.append(cls._unescape(line))
 
         return ignore_list, white_list
 
@@ -63,14 +78,14 @@ class IgnoreManager(BaseConfigManager):
         for root, dirs, files in os.walk("."):
             logger.debug("Root:%s, Dirs:%s", root, dirs)
 
-            if cls.ignore_path(unix_style_path(root), ignore_list, white_list):
+            if cls._ignore_path(unix_style_path(root), ignore_list, white_list):
                 dirs[:] = []
                 logger.debug("Ignoring directory : %s", root)
                 continue
 
             for file_name in files:
                 file_path = unix_style_path(os.path.join(root, file_name))
-                if cls.ignore_path(file_path, ignore_list, white_list):
+                if cls._ignore_path(file_path, ignore_list, white_list):
                     logger.debug("Ignoring file : %s", file_name)
                     continue
 
@@ -79,13 +94,23 @@ class IgnoreManager(BaseConfigManager):
         return unignored_files
 
     @staticmethod
-    def ignore_path(path, ignore_list=None, white_list=None):
-        """Returns a boolean indicating if a path should be ignored given an
-        ignore_list and a white_list of glob patterns.
-        """
+    def _matches_patterns(path, patterns):
+        """Given a list of patterns, returns a if a path matches any pattern."""
+        for glob in patterns:
+            try:
+                if PurePath(path).match(glob):
+                    return True
+            except TypeError:
+                pass
+        return False
+
+    @classmethod
+    def _ignore_path(cls, path, ignore_list=None, white_list=None):
+        """Returns a whether a path should be ignored or not."""
         ignore_list = ignore_list or []
         white_list = white_list or []
-        return matches_glob_list(path, ignore_list) and not matches_glob_list(path, white_list)
+        return (cls._matches_patterns(path, ignore_list) and
+                not cls._matches_patterns(path, white_list))
 
     @classmethod
     def get_value(cls, key):
