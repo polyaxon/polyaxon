fb3df9d269c4398d761c6333ab4a035c3452a14e
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-20 17:20:23 +0100

    Update to_bool utils

--- api/libs/utils.py
+++ api/libs/utils.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+from distutils.util import strtobool
+
 
 def to_bool(value):
-    if value in ('false', 'False', 'no', False, 0, '0'):
+    if isinstance(value, str):
+        return strtobool(value)
+
+    if value in (False, 0):
         return False
-    if value in ('true', 'True', 'yes', True, 1, '1'):
+
+    if value in (True, 1):
         return True
+
     raise TypeError('The value `{}` cannot be interpreted as boolean'.format(value))
