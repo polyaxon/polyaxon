0c99cca9ae9a2a4e957febe1970bf6508225f292
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-21 00:02:37 +0100

    Update manager

---
+++ polyaxon/polyaxonfile/constants.py
@@ -0,0 +1,1 @@
+CLUSTER_CONFIG_MAP_KEY_NAME = 'plxcluster_{experiment_uuid}_{task_type}'
--- polyaxon/polyaxonfile/manager.py
+++ polyaxon/polyaxonfile/manager.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+import os
 import tensorflow as tf
 
 from polyaxon_schemas.eval import EvalConfig
 from polyaxon_schemas.polyaxonfile.specification import Specification
+from polyaxon_schemas.settings import ClusterConfig
 from polyaxon_schemas.train import TrainConfig
 from polyaxon_schemas.utils import TaskType
 
@@ -12,6 +14,7 @@ from polyaxon.experiments import Experiment
 from polyaxon import Modes, getters
 from polyaxon.estimators.run_config import RunConfig
 from polyaxon.processing.input_data import create_input_data_fn
+from polyaxonfile import constants
 
 LOGGING_LEVEL = {
     'INFO': tf.logging.INFO,
@@ -46,10 +49,10 @@ def _get_eval(config):
             config.continuous_eval_throttle_secs)
 
 
-def _get_run_configs(polyaxonfile, experiment_id):
-    plx_file = Specification.read(polyaxonfile)
-    environment = plx_file.get_environment_at(experiment_id)
-    cluster_def, is_distributed = plx_file.get_cluster_def_at(experiment_id)
+def _get_run_configs(spec_config, experiment_uuid):
+    spec = Specification.read(spec_config)
+    environment = spec.environment
+    cluster_def, is_distributed = spec.cluster_def
 
     def get_master_config(config, task_type=None, task_id=None):
         config = RunConfig.from_config(config)
@@ -62,7 +65,22 @@ def _get_run_configs(polyaxonfile, experiment_id):
     if not is_distributed:
         return {TaskType.MASTER: [get_master_config(config)]}, False
 
-    config.cluster = plx_file.get_cluster(experiment=experiment_id)
+    if spec.is_local:
+        config.cluster = spec.get_local_cluster()
+    else:
+        # Get value from env
+        master = os.getenv(constants.CLUSTER_CONFIG_MAP_KEY_NAME(
+            experiment_uuid=experiment_uuid, task_type=TaskType.MASTER), '')
+        worker = os.getenv(constants.CLUSTER_CONFIG_MAP_KEY_NAME.format(
+            experiment_uuid=experiment_uuid, task_type=TaskType.WORKER), '')
+        ps = os.getenv(constants.CLUSTER_CONFIG_MAP_KEY_NAME.format(
+            experiment_uuid=experiment_uuid, task_type=TaskType.PS), '')
+        cluster_dict = {
+            TaskType.MASTER: master,
+            TaskType.WORKER: worker,
+            TaskType.PS: ps
+        }
+        config.cluster = ClusterConfig.from_dict(cluster_dict)
 
     configs = {TaskType.MASTER: [get_master_config(config, TaskType.MASTER, 0)]}
 
@@ -72,29 +90,21 @@ def _get_run_configs(polyaxonfile, experiment_id):
     if cluster_def.get(TaskType.PS, 0) > 0:
         configs[TaskType.PS] = []
 
-    # TODO: Replace with plxfile.get_worker_configs_at
-    worker_session_configs = {}
-    for session_config in environment.worker_configs or []:
-        worker_session_configs[session_config.index] = session_config
+    worker_session_configs = spec.worker_configs
+    ps_session_configs = spec.ps_configs
 
-    ps_session_configs = {}
-    for session_config in environment.ps_configs or []:
-        ps_session_configs[session_config.index] = session_config
-
-    default_worker_config = environment.default_worker_config
     for i in range(cluster_def.get(TaskType.WORKER, 0)):
         w_config = get_master_config(config, task_type=TaskType.WORKER, task_id=i)
-        session_config = worker_session_configs.get(i, default_worker_config)
+        session_config = worker_session_configs.get(i)
         if session_config:
             session_config = RunConfig.get_session_config(session_config)
             w_config = w_config.replace(session_config=session_config)
 
         configs[TaskType.WORKER].append(w_config)
 
-    default_ps_config = environment.default_ps_config
     for i in range(cluster_def.get(TaskType.PS, 0)):
         ps_config = get_master_config(config, task_type=TaskType.PS, task_id=i)
-        session_config = ps_session_configs.get(i, default_ps_config)
+        session_config = ps_session_configs.get(i)
         if session_config:
             session_config = RunConfig.get_session_config(session_config)
             ps_config = ps_config.replace(session_config=session_config)
@@ -104,33 +114,33 @@ def _get_run_configs(polyaxonfile, experiment_id):
     return configs, True
 
 
-def prepare_experiment_run(polyaxonfile, experiment_id, task_type=TaskType.MASTER, task_id=0):
-    plx_file = Specification.read(polyaxonfile)
-    cluster, _ = plx_file.get_cluster_def_at(experiment_id)
+def prepare_experiment_run(spec_config, experiment_uuid, task_type=TaskType.MASTER, task_id=0):
+    spec = Specification.read(spec_config)
+    cluster, _ = spec.cluster_def
 
     if (task_type not in cluster or
             not isinstance(cluster[task_type], int) or
-            task_id >= cluster[task_type]):
+                task_id >= cluster[task_type]):
         raise ValueError('task_type, task_id `{}, {}` is not supported by '
                          'the specification file passed.'.format(task_type, task_id))
 
-    env = plx_file.get_environment_at(experiment_id)
+    env = spec.environment
     if not env:
         tf.logging.set_verbosity(tf.logging.INFO)
         configs = {TaskType.MASTER: [RunConfig()]}
         delay_workers_by_global_step = False
     else:
-        tf.logging.set_verbosity(LOGGING_LEVEL[plx_file.settings.logging.level])
-        configs, _ = _get_run_configs(plx_file, experiment_id)
+        tf.logging.set_verbosity(LOGGING_LEVEL[spec.settings.logging.level])
+        configs, _ = _get_run_configs(spec, experiment_uuid)
         delay_workers_by_global_step = env.delay_workers_by_global_step
 
-    train_input_fn, train_steps, train_hooks = _get_train(plx_file.get_train_at(experiment_id))
+    train_input_fn, train_steps, train_hooks = _get_train(spec.train)
     (eval_input_fn, eval_steps, eval_hooks, eval_delay_secs,
-     continuous_eval_throttle_secs) = _get_eval(plx_file.get_eval_at(experiment_id))
+     continuous_eval_throttle_secs) = _get_eval(spec.eval)
 
-    estimator = getters.get_estimator(plx_file.get_model_at(experiment_id),
+    estimator = getters.get_estimator(spec.model,
                                       configs[task_type][task_id],
-                                      output_dir=plx_file.get_project_path_at(experiment_id))
+                                      output_dir=spec.project_path)
 
     return Experiment(
         estimator=estimator,
@@ -143,37 +153,37 @@ def prepare_experiment_run(polyaxonfile, experiment_id, task_type=TaskType.MASTE
         eval_delay_secs=eval_delay_secs,
         continuous_eval_throttle_secs=continuous_eval_throttle_secs,
         delay_workers_by_global_step=delay_workers_by_global_step,
-        export_strategies=plx_file.settings.export_strategies)
+        export_strategies=spec.settings.export_strategies)
 
 
-def start_experiment_run(polyaxonfile, experiment_id, task_type, task_id, schedule):
-    plx_file = Specification.read(polyaxonfile)
-    experiment = prepare_experiment_run(plx_file, int(experiment_id), task_type, int(task_id))
+def start_experiment_run(spec_config, experiment_uuid, task_type, task_id, schedule):
+    spec = Specification.read(spec_config)
+    experiment = prepare_experiment_run(spec, int(experiment_uuid), task_type, int(task_id))
     task = getattr(experiment, schedule)
     return task()
 
 
-def prepare_all_experiment_runs(polyaxonfile, experiment_id):
-    plx_file = Specification.read(polyaxonfile)
+def prepare_all_experiment_jobs(spec_config, experiment_uuid):
+    spec = Specification.read(spec_config)
     is_distributed = False
 
-    if not plx_file.get_environment_at(experiment_id):
+    if not spec.environment:
         tf.logging.set_verbosity(tf.logging.INFO)
         configs = {TaskType.MASTER: [RunConfig()]}
         delay_workers_by_global_step = False
     else:
-        tf.logging.set_verbosity(LOGGING_LEVEL[plx_file.settings.logging.level])
-        configs, is_distributed = _get_run_configs(plx_file.settings.environment, experiment_id)
-        delay_workers_by_global_step = plx_file.settings.environment.delay_workers_by_global_step
+        tf.logging.set_verbosity(LOGGING_LEVEL[spec.settings.logging.level])
+        configs, is_distributed = _get_run_configs(spec, experiment_uuid)
+        delay_workers_by_global_step = spec.environment.delay_workers_by_global_step
 
-    train_input_fn, train_steps, train_hooks = _get_train(plx_file.get_train_at(experiment_id))
+    train_input_fn, train_steps, train_hooks = _get_train(spec.train)
     (eval_input_fn, eval_steps, eval_hooks, eval_delay_secs,
-     continuous_eval_throttle_secs) = _get_eval(plx_file.get_eval_at(experiment_id))
+     continuous_eval_throttle_secs) = _get_eval(spec.eval)
 
     def get_experiment(config):
-        estimator = getters.get_estimator(plx_file.model,
+        estimator = getters.get_estimator(spec.model,
                                           config,
-                                          output_dir=plx_file.project_path)
+                                          output_dir=spec.project_path)
 
         return Experiment(
             estimator=estimator,
@@ -186,7 +196,7 @@ def prepare_all_experiment_runs(polyaxonfile, experiment_id):
             eval_delay_secs=eval_delay_secs,
             continuous_eval_throttle_secs=continuous_eval_throttle_secs,
             delay_workers_by_global_step=delay_workers_by_global_step,
-            export_strategies=plx_file.settings.export_strategies)
+            export_strategies=spec.settings.export_strategies)
 
     xps = [get_experiment(configs[TaskType.MASTER][0])]
     if not is_distributed:
