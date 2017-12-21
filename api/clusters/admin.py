34fef5648cf79797cb736ed9322d2371252e97b6
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-21 15:13:24 +0100

    Add cluster events admin

--- api/clusters/admin.py
+++ api/clusters/admin.py
@@ -3,8 +3,9 @@ from __future__ import absolute_import, division, print_function
 
 from django.contrib import admin
 
-from clusters.models import Cluster, ClusterNode, NodeGPU
+from clusters.models import Cluster, ClusterNode, NodeGPU, ClusterEvent
 
 admin.site.register(Cluster)
 admin.site.register(ClusterNode)
 admin.site.register(NodeGPU)
+admin.site.register(ClusterEvent)
