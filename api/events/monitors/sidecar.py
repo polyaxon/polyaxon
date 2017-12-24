1b59e839d70d33547692306d652913c74c11d948
mouradmourafiq <mouradmourafiq@gmail.com>
2017-12-24 19:44:55 +0100

    Extract publish logs logic to publisher

--- api/events/monitors/sidecar.py
+++ api/events/monitors/sidecar.py
@@ -5,14 +5,11 @@ import logging
 
 import time
 
-from django.conf import settings
 from polyaxon_schemas.experiment import JobLabelConfig
 
-from api.config_settings import RoutingKeys
-from api.celery_api import app as celery_app
-from libs.redis_db import RedisToStream
-from events.tasks import handle_events_job_logs
-from spawner.utils.constants import PodLifeCycle
+from events import publisher
+
+from spawner.utils.constants import PodLifeCycle, ExperimentLifeCycle
 
 logger = logging.getLogger('polyaxon.monitors.sidecar')
 
@@ -24,28 +21,11 @@ def run(k8s_manager, pod_id, experiment_uuid, job_uuid, container_job_name, pers
                                                       follow=True,
                                                       _preload_content=False)
     for log_line in raw.stream():
-        log_line = log_line.decode('utf-8')
-        logger.info("Publishing log event for experiment: {}, {}".format(job_uuid, experiment_uuid))
-        handle_events_job_logs.delay(experiment_uuid=experiment_uuid,
-                                     job_uuid=job_uuid,
-                                     log_line=log_line,
-                                     persist=persist)
-        if (RedisToStream.is_monitored_job_logs(job_uuid) or
-                RedisToStream.is_monitored_experiment_logs(experiment_uuid)):
-            logger.info("Streaming new log event for experiment: {}".format(experiment_uuid))
-
-            with celery_app.producer_or_acquire(None) as producer:
-                producer.publish(
-                    {
-                        'experiment_uuid': experiment_uuid,
-                        'job_uuid': job_uuid,
-                        'log_line': log_line
-                    },
-                    routing_key='{}.{}.{}'.format(RoutingKeys.LOGS_SIDECARS,
-                                                  experiment_uuid,
-                                                  job_uuid),
-                    exchange=settings.INTERNAL_EXCHANGE,
-                )
+        publisher.publish_log(log_line=log_line,
+                              status=ExperimentLifeCycle.RUNNING,
+                              experiment_uuid=experiment_uuid,
+                              job_uuid=job_uuid,
+                              persist=persist)
 
 
 def can_log(k8s_manager, pod_id, log_sleep_interval):
