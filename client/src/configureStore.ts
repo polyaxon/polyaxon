f8801973b03d6661622c4e8454273db8c2cb6c7a
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-05 18:15:19 +0100

    Update npm build process and split webpack configuration

--- client/src/configureStore.ts
+++ client/src/configureStore.ts
@@ -3,26 +3,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as _ from "lodash";
 
 import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import {createLogger} from 'redux-logger'
 
 
 import appReducer from "./reducers/app";
 import {AppState} from "./constants/types";
-import { loadState, saveState } from './localStorage'
+import {loadState, saveState} from './localStorage'
 import {getToken} from "./constants/utils";
 import {receiveTokenActionCreator} from "./actions/token";
 
 const configureStore = () => {
   const persistedState = loadState();
-  const loggerMiddleware = createLogger();
+
+  let middleware = [thunkMiddleware];
+  let newMiddleware = [];
+  if (process.env.NODE_ENV !== 'production') {
+    const loggerMiddleware = createLogger();
+    newMiddleware = [...middleware, loggerMiddleware]
+  }
+  else {
+    newMiddleware = middleware;
+  }
+
 
   const store = createStore<AppState>(
     appReducer,
     // persistedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
-    )
+    applyMiddleware(...newMiddleware)
   );
 
   let token = getToken();
