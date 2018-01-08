5a8f49fbc022cf31cad3e03471769c12e0f2f1b1
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-08 12:52:31 +0100

    Get secret and config name from chart

--- polyaxon/polyaxon/config_settings/k8s.py
+++ polyaxon/polyaxon/config_settings/k8s.py
@@ -8,7 +8,8 @@ from polyaxon.utils import config
 
 K8S_NAMESPACE = config.get_string('POLYAXON_K8S_NAMESPACE')
 POLYAXON_K8S_APP_NAME = config.get_string('POLYAXON_K8S_APP_NAME')
-POLYAXON_K8S_APP_SECRET_NAME = POLYAXON_K8S_APP_NAME + 'secret'
+POLYAXON_K8S_APP_CONFIG_NAME = config.get_string('POLYAXON_K8S_APP_CONFIG_NAME')
+POLYAXON_K8S_APP_SECRET_NAME = config.get_string('POLYAXON_K8S_APP_SECRET_NAME')
 CLUSTER_ID = config.get_string('POLYAXON_CLUSTER_ID', is_optional=True)
 
 K8S_AUTHORISATION = config.get_string('POLYAXON_K8S_AUTHORISATION',
