7f666dc1415119be76a61074a7c2527a79a0b945
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-09 19:27:20 +0100

    Fix dashboard url

--- polyaxon_cli/cli/dashboard.py
+++ polyaxon_cli/cli/dashboard.py
@@ -9,7 +9,7 @@ from polyaxon_cli.utils.clients import PolyaxonClients
 @click.command()
 def dashboard():
     """Open dashboard in browser."""
-    dashboard_url = "{}/app/token".format(PolyaxonClients().auth.http_host)
+    dashboard_url = "{}/app/".format(PolyaxonClients().auth.http_host)
     click.confirm('Dashboard page will now open in your browser. Continue?',
                   abort=True, default=True)
 
