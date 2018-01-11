fa102eb10a2e2788f3e3b1dbd8471391e3435f9c
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-11 22:47:57 +0100

    Update cluster commands

--- polyaxon_cli/cli/cluster.py
+++ polyaxon_cli/cli/cluster.py
@@ -12,11 +12,11 @@ from polyaxon_cli.utils.formatting import Printer, list_dicts_to_tabulate, dict_
 
 def get_cluster_info(cluster_config):
     Printer.print_header("Cluster info:")
-    cluster_dict = cluster_config.to_dict()
+    cluster_dict = cluster_config.to_light_dict()
     cluster_info = cluster_dict['version_api']
     dict_tabulate(cluster_info)
 
-    nodes = list_dicts_to_tabulate(cluster_dict['nodes'])
+    nodes = list_dicts_to_tabulate([node.to_light_dict() for node in cluster_config.nodes])
 
     if nodes:
         Printer.print_header("Cluster Nodes:")
@@ -27,21 +27,20 @@ def get_node_info(node_config):
     if not node_config:
         Printer.print_error('No node was found.')
         sys.exit(1)
-    node = node_config.to_dict()
-    gpus = node.pop('gpus', [])
+    node = node_config.to_light_dict()
+    node.pop('gpus')
     Printer.print_header("Node info:")
     dict_tabulate(node)
 
-    gpus_items = list_dicts_to_tabulate(gpus)
+    gpus_items = list_dicts_to_tabulate([gpu.to_light_dict() for gpu in node_config.gpus])
 
     if gpus_items:
         Printer.print_header("Node GPUs:")
-        gpus_items.pop('cluster_node')
         dict_tabulate(gpus_items, is_list_dict=True)
 
 
 @click.command()
-@click.option('--node', type=str)
+@click.option('--node', type=int)
 def cluster(node):
     """Get cluster and nodes info."""
     cluster_client = PolyaxonClients().cluster
