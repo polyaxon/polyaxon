a5b89c184d52f29c274ea4a6cf7f78f2b3a01936
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-18 12:46:24 +0100

    Bump version 0.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-client',
-      version='0.0.1',
+      version='0.0.2',
       description='Python base clients to interact with Polyaxon API.',
       maintainer='Mourad Mourafiq',
       maintainer_email='mouradmourafiq@gmail.com',
@@ -42,7 +42,7 @@ setup(name='polyaxon-client',
       ],
       install_requires=[
           "clint==0.5.1",
-          "polyaxon-schemas==0.0.15",
+          "polyaxon-schemas==0.0.2",
           "requests==2.18.4",
           "requests-toolbelt==0.8.0",
           "six==1.11.0",
