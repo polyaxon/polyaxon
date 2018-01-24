e8bf862b4d12595dfc53472ac497c39ac16324fe
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-24 01:52:44 +0100

    Update base url handling for dev

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,1 +1,9 @@
-export const BASE_URL = location.origin + "/api/v1";
+let baseUrl;
+if (process.env.NODE_ENV !== 'production') {
+  baseUrl = "http://localhost:8000/api/v1";
+}
+else {
+  baseUrl = location.origin + "/api/v1";
+}
+
+export const BASE_URL = baseUrl;
