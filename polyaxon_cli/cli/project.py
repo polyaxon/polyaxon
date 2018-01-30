d4fe26056b14adc021e725900b8d1f51329180a8
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-30 17:08:59 +0100

    Add trailing slash for tensorboard url

--- polyaxon_cli/cli/project.py
+++ polyaxon_cli/cli/project.py
@@ -341,7 +341,7 @@ def start_tensorboard(ctx):
     clint.textui.puts("It may take some time before you can access the dashboard.\n")
     clint.textui.puts("If you used an ingress, your dashboard will be available on:\n")
     with clint.textui.indent(4):
-        clint.textui.puts("{}/tensorboard/{}/{}\n".format(
+        clint.textui.puts("{}/tensorboard/{}/{}/\n".format(
             PolyaxonClients().auth.http_host, user, project_name))
 
     clint.textui.puts("Ohterwise you can use kubectl to get the url.")
