6cd8deaa36525d9eec69a92ab4b4ec6448446cd4
mouradmourafiq <mouradmourafiq@gmail.com>
2018-01-31 02:33:11 +0100

    Add upload and download views

--- polyaxon/libs/views.py
+++ polyaxon/libs/views.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+import os
+
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
 
 
 class ListCreateAPIView(generics.ListCreateAPIView):
@@ -11,3 +19,48 @@ class ListCreateAPIView(generics.ListCreateAPIView):
         if self.create_serializer_class and self.request.method.lower() == 'post':
             return self.create_serializer_class
         return self.serializer_class
+
+
+class DownloadView(APIView):
+    """Base view for to redirect to a file by instructing Nginx
+    to do so via special HTTP response headers.
+    """
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def _redirect_to_file(path):
+        response = HttpResponse()
+        response['Content-Type'] = u''
+        response['X-Accel-Redirect'] = path.encode('utf-8')
+
+        return response
+
+    def get(self, request, file_path):
+
+        if settings.DEBUG:  # pragma: no cover
+            # Redirect to the checked-in test data. Works only with development settings.
+            return HttpResponseRedirect(file_path)
+
+        return self._redirect_to_file(file_path)
+
+
+class UploadView(APIView):
+    """Base view to handle data upload."""
+    parser_classes = (MultiPartParser,)
+    permission_classes = (IsAuthenticated,)
+
+    @staticmethod
+    def _handle_posted_data(request, filename, directory, upload_filename):
+        file_path = os.path.join(directory, filename)
+        file_data = request.data[upload_filename]
+
+        # filename might already exist, if uploads are done in quick succession
+        # we just delete the previous one and assume that its changes are already committed
+        while os.path.exists(file_path):
+            os.remove(file_path)
+
+        # Creating the new file
+        with open(file_path, 'wb') as destination:
+            for chunk in file_data.chunks():
+                destination.write(chunk)
+        return file_path
--- polyaxon/repos/views.py
+++ polyaxon/repos/views.py
@@ -7,14 +7,11 @@ import os
 from django.conf import settings
 from django.http import HttpResponseServerError
 
-from rest_framework.generics import (
-    RetrieveUpdateDestroyAPIView,
-    get_object_or_404)
-from rest_framework.parsers import MultiPartParser
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
+from libs.views import UploadView
 from projects.permissions import get_permissible_project
 from repos.serializers import RepoSerializer
 from repos.tasks import handle_new_files
@@ -32,30 +29,12 @@ class RepoDetailView(RetrieveUpdateDestroyAPIView):
         return get_object_or_404(Repo, project=get_permissible_project(view=self))
 
 
-class UploadFilesView(APIView):
-    parser_classes = (MultiPartParser,)
-    permission_classes = (IsAuthenticated,)
+class UploadFilesView(UploadView):
 
     def get_object(self):
         project = get_permissible_project(view=self)
         repo, _ = Repo.objects.get_or_create(project=project)
         return repo
-
-    @staticmethod
-    def _handle_posted_data(request, repo, directory):
-        filename = os.path.join(directory, '{}.tar.gz'.format(repo.project.name))
-        file_data = request.data['repo']
-
-        # filename might already exist, if uploads are done in quick succession
-        # we just delete the previous one and assume that its changes are already committed
-        while os.path.exists(filename):
-            os.remove(filename)
-
-        # Creating the new file
-        with open(filename, 'wb') as destination:
-            for chunk in file_data.chunks():
-                destination.write(chunk)
-        return filename
 
     def put(self, request, *args, **kwargs):
         user = request.user
@@ -64,7 +43,10 @@ class UploadFilesView(APIView):
         if not os.path.exists(path):
             os.makedirs(path)
         try:
-            tar_file_name = self._handle_posted_data(request, repo, path)
+            tar_file_name = self._handle_posted_data(request=request,
+                                                     filename='{}.tar.gz'.format(repo.project.name),
+                                                     directory=path,
+                                                     upload_filename='repo')
         except (IOError, os.error) as e:  # pragma: no cover
             logger.warning(
                 'IOError while trying to save posted data ({}): {}'.format(e.errno, e.strerror))
