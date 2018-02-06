b214e477e859037e81570e493ec9ba23452710ad
mouradmourafiq <mouradmourafiq@gmail.com>
2018-02-06 11:44:28 +0100

    Update schemas

--- client/src/constants/schemas.ts
+++ client/src/constants/schemas.ts
@@ -1,13 +1,29 @@
 import { schema } from 'normalizr';
 
 export const JobSchema = new schema.Entity(
-  'jobs', {}, { idAttribute: 'unique_name' });
+  'jobs',
+  {},
+  {
+    idAttribute: 'unique_name'
+  });
 
 export const GroupSchema = new schema.Entity(
-  'groups', {}, { idAttribute: 'unique_name' });
+  'groups',
+  {},
+  {
+    idAttribute: 'unique_name'
+  });
 
 export const ExperimentSchema = new schema.Entity(
-  'experiments', {}, { idAttribute: 'unique_name' });
+  'experiments',
+  {},
+  {
+    idAttribute: 'unique_name'
+  });
 
 export const ProjectSchema = new schema.Entity(
-  'projects', {experiments: [ExperimentSchema]}, { idAttribute: 'unique_name' });
+  'projects',
+  {experiments: [ExperimentSchema]},
+  {
+    idAttribute: 'unique_name'
+  });
