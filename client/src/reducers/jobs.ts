7c88c306b5c4d4ec23da2b1cad8834ec3eeb51cc
mouradmourafiq <mouradmourafiq@gmail.com>
2018-02-06 15:36:10 +0100

    Update job reducer  * Add experiment jobs reducer

--- client/src/reducers/jobs.ts
+++ client/src/reducers/jobs.ts
@@ -5,12 +5,26 @@ import * as _ from 'lodash';
 
 import { JobSchema } from '../constants/schemas';
 import { JobAction, actionTypes } from '../actions/job';
-import { JobStateSchema, JobsEmptyState } from '../models/job';
-import { getJobIndexName } from '../constants/utils';
+import { JobStateSchema, JobsEmptyState, JobModel } from '../models/job';
+import { getExperimentIndexName, getJobIndexName } from '../constants/utils';
+import { ExperimentsEmptyState, ExperimentStateSchema } from '../models/experiment';
 
 export const jobsReducer: Reducer<JobStateSchema> =
   (state: JobStateSchema = JobsEmptyState, action: JobAction) => {
     let newState = {...state};
+
+    let processJob = function (job: JobModel) {
+      let uniqueName = getJobIndexName(job.unique_name);
+      if (!_.includes(newState.uniqueNames, uniqueName)) {
+        newState.uniqueNames.push(uniqueName);
+      }
+      let normalizedJobs = normalize(job, JobSchema).entities.jobs;
+      newState.byUniqueNames[uniqueName] = {
+        ...newState.byUniqueNames[uniqueName], ...normalizedJobs[job.unique_name]
+      };
+      return newState;
+    };
+
     switch (action.type) {
       case actionTypes.CREATE_JOB:
         return {
@@ -36,22 +50,39 @@ export const jobsReducer: Reducer<JobStateSchema> =
         };
       case actionTypes.RECEIVE_JOBS:
         for (let job of action.jobs) {
-          let jobUniqueName = getJobIndexName(job.unique_name);
-          if (!_.includes(newState.uniqueNames, jobUniqueName)) {
-            newState.uniqueNames.push(jobUniqueName);
-            newState.byUniqueNames[jobUniqueName] = job;
-          }
-          newState.byUniqueNames[jobUniqueName] = job;
+          newState = processJob(job);
         }
         return newState;
       case actionTypes.RECEIVE_JOB:
-        let jobUniqueName = getJobIndexName(action.job.unique_name);
-        if (!_.includes(newState.uniqueNames, jobUniqueName)) {
-          newState.uniqueNames.push(jobUniqueName);
-        }
-        let normalizedJobs = normalize(action.job, JobSchema).entities.jobs;
-        newState.byUniqueNames[jobUniqueName] = normalizedJobs[action.job.unique_name];
-        return newState;
+        return processJob(action.job);
+      default:
+        return state;
     }
-    return state;
+  };
+
+export const ExperimentJobsReducer: Reducer<ExperimentStateSchema> =
+  (state: ExperimentStateSchema = ExperimentsEmptyState, action: JobAction) => {
+    let newState = {...state};
+
+    let processJob = function (job: JobModel) {
+      let uniqueName = getJobIndexName(job.unique_name);
+      let experimentName = getExperimentIndexName(job.experiment_name);
+      if (_.includes(newState.uniqueNames, experimentName) &&
+        !_.includes(newState.byUniqueNames[experimentName].jobs, uniqueName)) {
+        newState.byUniqueNames[experimentName].jobs.push(uniqueName);
+      }
+      return newState;
+    };
+
+    switch (action.type) {
+      case actionTypes.RECEIVE_JOB:
+        return processJob(action.job);
+      case actionTypes.RECEIVE_JOBS:
+        for (let job of action.jobs) {
+          newState = processJob(job);
+        }
+        return newState;
+      default:
+        return state;
+    }
   };
