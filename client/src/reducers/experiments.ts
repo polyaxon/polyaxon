359375a390702ab15636060eb966475dd0760afa
mouradmourafiq <mouradmourafiq@gmail.com>
2018-02-06 15:34:26 +0100

    Update experiments reducer  * Add project and group experiments reducers

--- client/src/reducers/experiments.ts
+++ client/src/reducers/experiments.ts
@@ -4,13 +4,30 @@ import { normalize } from 'normalizr';
 
 import { ExperimentSchema } from '../constants/schemas';
 import { ExperimentAction, actionTypes } from '../actions/experiment';
-import { ExperimentStateSchema, ExperimentsEmptyState } from '../models/experiment';
+import { ExperimentStateSchema, ExperimentsEmptyState, ExperimentModel } from '../models/experiment';
 import { getExperimentIndexName } from '../constants/utils';
-import { ProjectStateSchema } from '../models/project';
+import { ProjectsEmptyState, ProjectStateSchema } from '../models/project';
+import { GroupsEmptyState, GroupStateSchema } from '../models/group';
 
 export const experimentsReducer: Reducer<ExperimentStateSchema> =
   (state: ExperimentStateSchema = ExperimentsEmptyState, action: ExperimentAction) => {
     let newState = {...state};
+
+    let processExperiment = function (experiment: ExperimentModel) {
+      let uniqueName = getExperimentIndexName(experiment.unique_name);
+      if (!_.includes(newState.uniqueNames, uniqueName)) {
+        newState.uniqueNames.push(uniqueName);
+      }
+      let normalizedExperiments = normalize(experiment, ExperimentSchema).entities.experiments;
+      newState.byUniqueNames[uniqueName] = {
+        ...newState.byUniqueNames[uniqueName], ...normalizedExperiments[experiment.unique_name]
+      };
+      if (newState.byUniqueNames[uniqueName].jobs == null) {
+        newState.byUniqueNames[uniqueName].jobs = [];
+      }
+      return newState;
+    };
+
     switch (action.type) {
       case actionTypes.CREATE_EXPERIMENT:
         return {
@@ -40,23 +57,68 @@ export const experimentsReducer: Reducer<ExperimentStateSchema> =
           }
         };
       case actionTypes.RECEIVE_EXPERIMENTS:
-        for (let xp of action.experiments) {
-          let uniqueName = getExperimentIndexName(xp.unique_name);
-          if (!_.includes(newState.uniqueNames, uniqueName)) {
-            newState.uniqueNames.push(uniqueName);
-            newState.byUniqueNames[uniqueName] = xp;
-          }
-          newState.byUniqueNames[uniqueName] = xp;
+        for (let experiment of action.experiments) {
+          newState = processExperiment(experiment);
         }
         return newState;
       case actionTypes.RECEIVE_EXPERIMENT:
-        let uniqueName = getExperimentIndexName(action.experiment.unique_name);
-        if (!_.includes(newState.uniqueNames, uniqueName)) {
-          newState.uniqueNames.push(uniqueName);
-        }
-        let normalizedExperiments = normalize(action.experiment, ExperimentSchema).entities.experiments;
-        newState.byUniqueNames[uniqueName] = normalizedExperiments[action.experiment.unique_name];
-        return newState;
+        return processExperiment(action.experiment);
+      default:
+        return state;
     }
-    return state;
+  };
+
+export const ProjectExperimentsReducer: Reducer<ProjectStateSchema> =
+  (state: ProjectStateSchema = ProjectsEmptyState, action: ExperimentAction) => {
+    let newState = {...state};
+
+    let processExperiment = function (experiment: ExperimentModel) {
+      let uniqueName = getExperimentIndexName(experiment.unique_name);
+      let projectName = experiment.project_name;
+      if (_.includes(newState.uniqueNames, projectName) &&
+        !_.includes(newState.byUniqueNames[projectName].experiments, uniqueName)) {
+        newState.byUniqueNames[projectName].experiments.push(uniqueName);
+      }
+      return newState;
+    };
+
+    switch (action.type) {
+      case actionTypes.RECEIVE_EXPERIMENT:
+        return processExperiment(action.experiment);
+      case actionTypes.RECEIVE_EXPERIMENTS:
+        for (let experiment of action.experiments) {
+          newState = processExperiment(experiment);
+        }
+        return newState;
+      default:
+        return state;
+    }
+  };
+
+export const GroupExperimentsReducer: Reducer<GroupStateSchema> =
+  (state: GroupStateSchema = GroupsEmptyState, action: ExperimentAction) => {
+    let newState = {...state};
+
+    let processExperiment = function (experiment: ExperimentModel) {
+      let uniqueName = getExperimentIndexName(experiment.unique_name);
+      let groupName = experiment.experiment_group_name;
+      if (groupName != null &&
+        _.includes(newState.uniqueNames, groupName) &&
+        !_.includes(newState.byUniqueNames[groupName].experiments, uniqueName)) {
+        newState.byUniqueNames[groupName].experiments.push(uniqueName);
+      }
+      return newState;
+    };
+
+    switch (action.type) {
+      case actionTypes.RECEIVE_EXPERIMENT:
+        return processExperiment(action.experiment);
+      case actionTypes.RECEIVE_EXPERIMENTS:
+        for (let experiment of action.experiments) {
+          newState = processExperiment(experiment);
+        }
+        return newState;
+      default:
+        return state;
+    }
   };
