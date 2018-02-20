052a53d4e2eaa3ac38aa21eb6f8613bc079b2490
mouradmourafiq <mouradmourafiq@gmail.com>
2018-02-20 10:46:53 +0100

    Update notebook spawner config

--- polyaxon/spawner/spawner.py
+++ polyaxon/spawner/spawner.py
@@ -529,6 +529,8 @@ class K8SProjectSpawner(K8SManager):
                   "--port=8888 "
                   "--ip=0.0.0.0 "
                   "--allow-root "
+                  "--NotebookApp.token=''"
+                  "--NotebookApp.trust_xheaders=True"  
                   "--NotebookApp.base_url={notebook_path}".format(notebook_path=notebook_path)],
             ports=target_ports,
             resources=resources,
