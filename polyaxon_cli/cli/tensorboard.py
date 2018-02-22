c71600577179405bc4f486f0d05d85c38c10a0cb
mouradmourafiq <mouradmourafiq@gmail.com>
2018-02-22 13:20:11 +0100

    Add support for custom polyaxonfile for tensorboard  * Update documentation

--- polyaxon_cli/cli/tensorboard.py
+++ polyaxon_cli/cli/tensorboard.py
@@ -6,8 +6,11 @@ import sys
 
 import clint
 from polyaxon_client.exceptions import PolyaxonShouldExitError, PolyaxonHTTPError
+from polyaxon_schemas.plugins import PluginJobConfig
 
-from polyaxon_cli.cli.project import get_project_or_local
+from polyaxon_cli.cli.check import check_polyaxonfile
+from polyaxon_cli.cli.project import get_project_or_local, equal_projects
+from polyaxon_cli.managers.project import ProjectManager
 from polyaxon_cli.utils.clients import PolyaxonClients
 from polyaxon_cli.utils.formatting import Printer
 
@@ -21,17 +24,48 @@ def tensorboard(ctx, project):
 
 
 @tensorboard.command()
+@click.option('--file', '-f', multiple=True, type=click.Path(exists=True),
+              help='The polyaxon files to run.')
 @click.pass_context
-def start(ctx):
+def start(ctx, file):
     """Start a tensorboard deployment for this project.
 
     It will show a tensorboard with all experiments under the project.
 
     Uses [Caching](/polyaxon_cli/introduction#Caching)
+
+    Example: using the default tensorflow image 1.4.1.
+
+    \b
+    ```bash
+    $ polyaxon tensorboard start
+    ```
+
+    Example: with custom image and resources
+
+    \b
+    ```bash
+    $ polyaxon tensorboard start -f file -f file_override ...
+    ```
     """
+    plx_file = None
+    plugin_job = None
+    if file:
+        plx_file = check_polyaxonfile(file, log=False, is_plugin=True)
+
+    if plx_file:
+        project = ProjectManager.get_config_or_raise()
+        if not equal_projects(plx_file.project.name, project.unique_name):
+            Printer.print_error('Your polyaxonfile defined a different project '
+                                'than the one set in this repo.')
+            sys.exit(1)
+
+        plugin_job = PluginJobConfig(content=plx_file._data,
+                                     config=plx_file.parsed_data)
+
     user, project_name = get_project_or_local(ctx.obj['project'])
     try:
-        PolyaxonClients().project.start_tensorboard(user, project_name)
+        PolyaxonClients().project.start_tensorboard(user, project_name, plugin_job)
     except (PolyaxonHTTPError, PolyaxonShouldExitError) as e:
         Printer.print_error('Could not start tensorboard project `{}`.'.format(project_name))
         Printer.print_error('Error message `{}`.'.format(e))
@@ -39,12 +73,10 @@ def start(ctx):
 
     Printer.print_success('Tensorboard is being deployed for project `{}`'.format(project_name))
     clint.textui.puts("It may take some time before you can access the dashboard.\n")
-    clint.textui.puts("If you used an ingress, your dashboard will be available on:\n")
+    clint.textui.puts("Your Tensorboard will be available on:\n")
     with clint.textui.indent(4):
         clint.textui.puts("{}/tensorboard/{}/{}/\n".format(
             PolyaxonClients().auth.http_host, user, project_name))
-
-    clint.textui.puts("Ohterwise you can use kubectl to get the url.")
 
 
 @tensorboard.command()
