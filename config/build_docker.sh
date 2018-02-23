ad9b923cbecf3cbf195011471274031a00bc834a
mouradmourafiq <mouradmourafiq@gmail.com>
2018-02-23 11:57:11 +0100

    Only build for master

--- config/build_docker.sh
+++ config/build_docker.sh
@@ -1,12 +1,22 @@
-body='{
-"request": {
-"branch":$1
-}}'
+#!/usr/bin/env bash
+if [ $# -lt 1 ]
+  then
+    echo "You should provide at least 1 args: branch"
+     exit 1
+fi
 
-curl -s -X POST \
-   -H "Content-Type: application/json" \
-   -H "Accept: application/json" \
-   -H "Travis-API-Version: 3" \
-   -H "Authorization: token 6SE268-eR1WMZUTvmYjUrg" \
-   -d "$body" \
-   https://api.travis-ci.org/repo/polyaxon%2Fpolyaxon-docker/requests
+if [ "$1" == "master" ]
+    then
+        body='{
+        "request": {
+        "branch":master
+        }}'
+
+        curl -s -X POST \
+           -H "Content-Type: application/json" \
+           -H "Accept: application/json" \
+           -H "Travis-API-Version: 3" \
+           -H "Authorization: token 6SE268-eR1WMZUTvmYjUrg" \
+           -d "$body" \
+           https://api.travis-ci.org/repo/polyaxon%2Fpolyaxon-docker/requests
+fi
