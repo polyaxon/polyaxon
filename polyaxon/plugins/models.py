f28fb565691db4b3fad4daa8406ccf4a33f6dd40
mouradmourafiq <mouradmourafiq@gmail.com>
2018-03-06 20:33:41 +0100

    #58 Add plugin jobs statuses

--- polyaxon/plugins/models.py
+++ polyaxon/plugins/models.py
@@ -9,13 +9,14 @@ from django.db import models
 from django.utils.functional import cached_property
 from polyaxon_schemas.polyaxonfile.specification import PluginSpecification
 
-from libs.models import DiffModel
+from jobs.models import Job, JobStatus
 from libs.spec_validation import validate_tensorboard_spec_content
+from spawner.utils.constants import JobLifeCycle
 
 logger = logging.getLogger('polyaxon.plugins')
 
 
-class PluginJobBase(DiffModel):
+class PluginJobBase(Job):
     """A base model for plugin jobs."""
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -40,26 +41,68 @@ class PluginJobBase(DiffModel):
     def resources(self):
         return self.compiled_spec.total_resources
 
+    @cached_property
+    def unique_name(self):
+        return self.__str__()
+
 
 class TensorboardJob(PluginJobBase):
     """A model that represents the configuration for tensorboard job."""
+
+    job_status = models.OneToOneField(
+        'TensorboardJobStatus',
+        related_name='+',
+        blank=True,
+        null=True,
+        editable=True)
 
     def __str__(self):
         if hasattr(self, 'project'):
             return '{} tensorboard<{}>'.format(self.project, self.image)
         logger.warning('Tensorboard with id `{}` is orphan.'.format(self.id))
-        return self.image
+        return 'Tensorboard {}'.format(self.id)
 
     @cached_property
     def image(self):
         return self.compiled_spec.run_exec.image
 
+    def set_status(self, status, message=None, details=None):
+        return self._set_status(status_model=TensorboardJobStatus,
+                                logger=logger,
+                                status=status,
+                                message=message,
+                                details=details)
+
 
 class NotebookJob(PluginJobBase):
     """A model that represents the configuration for tensorboard job."""
+
+    job_status = models.OneToOneField(
+        'NotebookJobStatus',
+        related_name='+',
+        blank=True,
+        null=True,
+        editable=True)
 
     def __str__(self):
         if hasattr(self, 'project'):
             return '{} notebook'.format(self.project)
         logger.warning('Notebook with id `{}` is orphan.'.format(self.id))
         return 'Notebook {}'.format(self.id)
+
+    def set_status(self, status, message=None, details=None):
+        return self._set_status(status_model=NotebookJobStatus,
+                                logger=logger,
+                                status=status,
+                                message=message,
+                                details=details)
+
+
+class TensorboardJobStatus(JobStatus):
+    """A model that represents tensorboard job status at certain time."""
+    job = models.ForeignKey(TensorboardJob, related_name='statuses')
+
+
+class NotebookJobStatus(JobStatus):
+    """A model that represents notebook job status at certain time."""
+    job = models.ForeignKey(NotebookJob, related_name='statuses')
