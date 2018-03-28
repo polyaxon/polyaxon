c0349b91c13ba69204aa482b69993a8c395697c9
mouradmourafiq <mouradmourafiq@gmail.com>
2018-03-28 18:45:56 +0200

    Update pipelines admin

--- polyaxon/pipelines/admin.py
+++ polyaxon/pipelines/admin.py
@@ -6,10 +6,23 @@ from pipelines.models import (
     Pipeline,
     OperationRun,
     PipelineRun,
+    PipelineRunStatus,
+    OperationRunStatus,
 )
+
+
+class PipelineRunStatusAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+
+
+class OperationRunStatusAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+
 
 admin.site.register(Schedule)
 admin.site.register(Pipeline)
 admin.site.register(Operation)
+admin.site.register(PipelineRunStatus, PipelineRunStatusAdmin)
+admin.site.register(OperationRunStatus, OperationRunStatusAdmin)
 admin.site.register(PipelineRun)
 admin.site.register(OperationRun)
