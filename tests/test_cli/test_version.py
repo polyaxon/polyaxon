1fc4399d8c7e92a1b8064e357680b0a3f9400bf5
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-08 19:26:41 +0200

    Fix version tests

--- tests/test_cli/test_version.py
+++ tests/test_cli/test_version.py
@@ -18,21 +18,27 @@ class TestVersion(BaseCommandTestCase):
 
     @patch('polyaxon_client.version.VersionClient.get_cli_version')
     @patch('polyaxon_cli.cli.version.dict_tabulate')
-    def test_version_cli_default(self, dict_tabulate, get_cli_version):
+    @patch('polyaxon_cli.cli.version.get_version')
+    def test_version_cli_default(self, dict_tabulate, get_cli_version, get_version):
         self.runner.invoke(version)
         get_cli_version.assert_called_once()
         dict_tabulate.assert_called_once()
+        get_version.assert_called_once()
 
     @patch('polyaxon_client.version.VersionClient.get_cli_version')
     @patch('polyaxon_cli.cli.version.dict_tabulate')
-    def test_version_cli(self, dict_tabulate, get_cli_version):
+    @patch('polyaxon_cli.cli.version.get_version')
+    def test_version_cli(self, dict_tabulate, get_cli_version, get_version):
         self.runner.invoke(version, ['--cli'])
         get_cli_version.assert_called_once()
         dict_tabulate.assert_called_once()
+        get_version.assert_called_once()
 
     @patch('polyaxon_client.version.VersionClient.get_platform_version')
+    @patch('polyaxon_client.version.VersionClient.get_chart_version')
     @patch('polyaxon_cli.cli.version.dict_tabulate')
-    def test_version(self, dict_tabulate, get_version):
+    def test_version(self, dict_tabulate, get_chart_version, get_platform_version):
         self.runner.invoke(version, ['--platform'])
-        get_version.assert_called_once()
+        get_platform_version.assert_called_once()
+        get_chart_version.assert_called_once()
         dict_tabulate.assert_called_once()
