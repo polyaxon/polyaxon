2c3185de202337360f7153166e488fb8aaecc48d
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-08 18:28:07 +0200

    Add auth tests

---
+++ tests/test_cli/test_auth.py
@@ -0,0 +1,23 @@
+from click.testing import CliRunner
+import unittest
+
+from mock import patch
+from polyaxon_cli.cli.auth import login, logout, whoami
+
+
+class TestAuthClient(unittest.TestCase):
+    """
+    Tests Experiment CLI functionality
+    """
+    def setUp(self):
+        self.runner = CliRunner()
+
+    @patch('polyaxon_cli.managers.auth.AuthConfigManager.purge')
+    def test_logout(self, purge_patch):
+        self.runner.invoke(logout)
+        assert purge_patch.call_count == 1
+
+    @patch('polyaxon_client.auth.AuthClient.get_user')
+    def test_whoami(self, purge_patch):
+        self.runner.invoke(whoami)
+        assert purge_patch.call_count == 1
