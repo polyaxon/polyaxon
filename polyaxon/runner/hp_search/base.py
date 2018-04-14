22411b46fcd134f83c75feea73c1be86e7dd139a
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-14 09:41:06 +0200

    Add hyperparams search runners

---
+++ polyaxon/runner/hp_search/base.py
@@ -0,0 +1,45 @@
+import logging
+
+from experiments.models import Experiment
+from runner.tasks.experiments import build_experiment
+
+logger = logging.getLogger('polyaxon.runner.hp_search')
+
+
+def create_group_experiments(experiment_group):
+    # Parse polyaxonfile content and create the experiments
+    specification = experiment_group.specification
+    suggestions = experiment_group.get_suggestions()
+
+    if not suggestions:
+        logger.warning('Search algorithm was not found `%s`', specification.search_algorithm)
+        return
+
+    for suggestion in suggestions:
+        # We need to check if we should create or restart
+        Experiment.objects.create(
+            project_id=experiment_group.project_id,
+            user_id=experiment_group.user_id,
+            experiment_group=experiment_group,
+            config=specification.get_experiment_spec(matrix_declaration=suggestion).parsed_data,
+            code_reference_id=experiment_group.code_reference_id)
+
+
+def start_group_experiments(experiment_group):
+    # Check for early stopping before starting new experiments from this group
+    if experiment_group.should_stop_early():
+        from runner.tasks.experiment_groups import stop_group_experiments
+
+        stop_group_experiments(experiment_group_id=experiment_group.id,
+                               pending=True,
+                               message='Early stopping')
+        return
+
+    experiment_to_start = experiment_group.n_experiments_to_start
+    pending_experiments = experiment_group.pending_experiments[:experiment_to_start]
+    n_pending_experiment = experiment_group.pending_experiments.count()
+
+    for experiment in pending_experiments:
+        build_experiment.delay(experiment_id=experiment.id)
+
+    return n_pending_experiment - experiment_to_start > 0
---
+++ polyaxon/runner/hp_search/hyperband.py
@@ -0,0 +1,51 @@
+from experiment_groups.utils import get_valid_experiment_group
+from polyaxon.celery_api import app as celery_app
+from polyaxon.settings import HPCeleryTasks, Intervals
+from runner.hp_search import base
+
+
+def create(experiment_group):
+    base.create_group_experiments(experiment_group=experiment_group)
+
+    hp_hyperband_start.apply_async((experiment_group.id,), countdown=1)
+
+
+@celery_app.task(name=HPCeleryTasks.HP_HYPERBAND_CREATE)
+def hp_hyperband_create(experiment_group_id):
+    experiment_group = get_valid_experiment_group(experiment_group_id=experiment_group_id)
+    if not experiment_group:
+        return
+
+    create(experiment_group)
+
+
+@celery_app.task(name=HPCeleryTasks.HP_HYPERBAND_START, bind=True, max_retries=None)
+def hp_hyperband_start(self, experiment_group_id):
+    experiment_group = get_valid_experiment_group(experiment_group_id=experiment_group_id)
+    if not experiment_group:
+        return
+
+    should_retry = base.start_group_experiments(experiment_group=experiment_group)
+    if should_retry:
+        # Schedule another task
+        self.retry(countdown=Intervals.EXPERIMENTS_SCHEDULER)
+
+    hp_hyperband_iterate.delay(experiment_group_id=experiment_group_id)
+
+
+@celery_app.task(name=HPCeleryTasks.HP_HYPERBAND_ITERATE)
+def hp_hyperband_iterate(experiment_group_id):
+    experiment_group = get_valid_experiment_group(experiment_group_id=experiment_group_id)
+    if not experiment_group:
+        return
+
+    iteration_config = experiment_group.get_iteration_config()
+    search_manager = experiment_group.search_manager
+
+    if search_manager.should_reschedule(iteration=iteration_config.iteration):
+        hp_hyperband_create.delay(experiment_group_id=experiment_group_id)
+        return
+
+    if search_manager.should_reduce_configs(iteration=iteration_config.iteration,
+                                            bracket_iteration=iteration_config.bracket_iteration):
+        return
