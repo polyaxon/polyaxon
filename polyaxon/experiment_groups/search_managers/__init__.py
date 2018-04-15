e824ba05445770359bc619eab2736f57af728127
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-15 11:10:52 +0200

    Update search algorithm managers

--- polyaxon/experiment_groups/search_managers/__init__.py
+++ polyaxon/experiment_groups/search_managers/__init__.py
@@ -5,13 +5,16 @@ from experiment_groups.search_managers.random import RandomSearchManager
 from experiment_groups.search_managers.hyperband import HyperbandSearchManager
 
 
-def get_search_algorithm_manager(specification):
-    if SearchAlgorithms.is_grid(specification.search_algorithm):
-        return GridSearchManager(specification=specification)
-    elif SearchAlgorithms.is_random(specification.search_algorithm):
-        return RandomSearchManager(specification=specification)
-    elif SearchAlgorithms.is_hyperband(specification.search_algorithm):
-        return HyperbandSearchManager(specification=specification)
+def get_search_algorithm_manager(params_config):
+    if not params_config:
+        return None
+
+    if SearchAlgorithms.is_grid(params_config.search_algorithm):
+        return GridSearchManager(params_config=params_config)
+    elif SearchAlgorithms.is_random(params_config.search_algorithm):
+        return RandomSearchManager(params_config=params_config)
+    elif SearchAlgorithms.is_hyperband(params_config.search_algorithm):
+        return HyperbandSearchManager(params_config=params_config)
 
     return None
 
