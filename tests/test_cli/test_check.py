9d3f2878899ee57884f0e1be18b7b4baf6c94cd0
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-15 17:24:35 +0200

    Remove unused commands tests

--- tests/test_cli/test_check.py
+++ tests/test_cli/test_check.py
@@ -15,23 +15,9 @@ class TestCheck(BaseCommandTestCase):
         assert check_polyaxonfile.call_count == 1
 
     @patch('polyaxon_cli.cli.check.check_polyaxonfile')
-    @patch('polyaxon_cli.cli.check.click.echo')
-    def test_check_file_all(self, echo, check_polyaxonfile):
-        self.runner.invoke(check, ['--all'])
-        assert check_polyaxonfile.call_count == 1
-        assert echo.call_count == 1
-
-    @patch('polyaxon_cli.cli.check.check_polyaxonfile')
     @patch('polyaxon_cli.cli.check.Printer.decorate_format_value')
     def test_check_file_version(self, decorate_format_value, check_polyaxonfile):
         self.runner.invoke(check, ['--version'])
-        assert check_polyaxonfile.call_count == 1
-        assert decorate_format_value.call_count == 1
-
-    @patch('polyaxon_cli.cli.check.check_polyaxonfile')
-    @patch('polyaxon_cli.cli.check.Printer.decorate_format_value')
-    def test_check_file_run_type(self, decorate_format_value, check_polyaxonfile):
-        self.runner.invoke(check, ['--run-type'])
         assert check_polyaxonfile.call_count == 1
         assert decorate_format_value.call_count == 1
 
