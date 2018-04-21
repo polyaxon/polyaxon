ad1dadb748076268a140c84ea8b8df2ccca3f9b1
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-21 15:39:55 +0200

    #100 Add bayesian optimization search manager and iteration manager

---
+++ polyaxon/experiment_groups/iteration_managers/bayesian_optimization.py
@@ -0,0 +1,9 @@
+import logging
+
+from experiment_groups.iteration_managers.base import BaseIterationManger
+
+logger = logging.getLogger('polyaxon.experiments_groups.iteration_manager')
+
+
+class BOIterationManager(BaseIterationManger):
+    pass
---
+++ polyaxon/experiment_groups/search_managers/bayesian_optimization/manager.py
@@ -0,0 +1,17 @@
+from experiment_groups.search_managers.base import BaseSearchAlgorithmManager
+from polyaxon_schemas.utils import SearchAlgorithms
+
+from experiment_groups.search_managers.bayesian_optimization.acquisition_function import (
+    UtilityFunction
+)
+
+
+class BOManager(BaseSearchAlgorithmManager):
+    """Bayesian optimization algorithm manager for hyperparameter optimization."""
+
+    NAME = SearchAlgorithms.BO
+
+    def __init__(self, params_config):
+        super(BOManager, self).__init__(params_config=params_config)
+        self.utility_function = UtilityFunction(
+            config=self.params_config.bo.utility_function, seed=params_config.seed)
