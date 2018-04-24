3734c41172d7e4064a158a96e4ac349ec032a550
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-24 10:46:01 +0200

    #100 Update bayesian optimization schema

--- polyaxon/experiment_groups/schemas/bayesian_optimization.py
+++ polyaxon/experiment_groups/schemas/bayesian_optimization.py
@@ -9,14 +9,14 @@ from polyaxon_schemas.base import BaseConfig
 class BOIterationSchema(Schema):
     iteration = fields.Int()
     experiment_ids = fields.List(fields.Int(), allow_none=True)
-    experiment_configs = fields.List(
+    experiments_configs = fields.List(
         fields.List(fields.Raw(), validate=validate.Length(equal=2)),
         allow_none=True)
     experiments_metrics = fields.List(
         fields.List(fields.Raw(), validate=validate.Length(equal=2)),
         allow_none=True)
     new_experiment_ids = fields.List(fields.Int(), allow_none=True)
-    new_experiment_configs = fields.List(
+    new_experiments_configs = fields.List(
         fields.List(fields.Raw(), validate=validate.Length(equal=2)),
         allow_none=True)
     new_experiments_metrics = fields.List(
@@ -42,14 +42,14 @@ class BOIterationConfig(BaseConfig):
                  iteration,
                  experiment_ids=None,
                  experiments_metrics=None,
-                 experiment_configs=None,
+                 experiments_configs=None,
                  new_experiment_ids=None,
                  new_experiments_metrics=None,
-                 new_experiment_configs=None):
+                 new_experiments_configs=None):
         self.iteration = iteration
         self.experiment_ids = experiment_ids
-        self.experiment_configs = experiment_configs
+        self.experiments_configs = experiments_configs
         self.experiments_metrics = experiments_metrics
         self.new_experiment_ids = new_experiment_ids
         self.new_experiments_metrics = new_experiments_metrics
-        self.new_experiment_configs = new_experiment_configs
+        self.new_experiments_configs = new_experiments_configs
