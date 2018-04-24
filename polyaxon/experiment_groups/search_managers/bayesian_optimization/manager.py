ad80e844de81329d78d2330f767dc05df0b058ae
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-24 14:26:57 +0200

    #100 Extract optimizer's logic

--- polyaxon/experiment_groups/search_managers/bayesian_optimization/manager.py
+++ polyaxon/experiment_groups/search_managers/bayesian_optimization/manager.py
@@ -1,8 +1,5 @@
 from experiment_groups.search_managers.base import BaseSearchAlgorithmManager
-from experiment_groups.search_managers.bayesian_optimization.acquisition_function import (
-    UtilityFunction
-)
-from experiment_groups.search_managers.bayesian_optimization.space import SearchSpace
+from experiment_groups.search_managers.bayesian_optimization.optimizer import BOOptimizer
 from experiment_groups.search_managers.utils import get_random_suggestions
 from polyaxon_schemas.utils import SearchAlgorithms
 
@@ -15,19 +12,6 @@ class BOSearchManager(BaseSearchAlgorithmManager):
     def __init__(self, params_config):
         super(BOSearchManager, self).__init__(params_config=params_config)
         self.n_initial_trials = self.params_config.bo.n_initial_trials
-        self.space = SearchSpace(params_config=params_config)
-        self.utility_function = UtilityFunction(
-            config=params_config.bo.utility_function, seed=params_config.seed)
-
-    def _maximize(self):
-        """ Find argmax of the acquisition function."""
-        y_max = self.space.y.max()
-        self.utility_function.gaussian_process.fit(self.space.x, self.space.y)
-        return self.utility_function.max_compute(y_max=y_max, bounds=self.space.bounds)
-
-    def _get_next_suggestion(self):
-        x = self._maximize()
-        return self.space.get_suggestion(x)
 
     def get_suggestions(self, iteration_config=None):
         if not iteration_config:
@@ -35,10 +19,13 @@ class BOSearchManager(BaseSearchAlgorithmManager):
                                           n_suggestions=self.n_initial_trials,
                                           seed=self.params_config.seed)
         # Use the iteration_config to construct observed point and metrics
-        experiments_configs = iteration_config.exepriments_configs
-        experiments_metrics = iteration_config.exepriments_metrics
-        # Turn configs and metrics into data points
-        self.space.add_observations(configs=experiments_configs,
-                                    metrics=experiments_metrics)
-
-        return [self._get_next_suggestion()]
+        experiments_configs = dict(iteration_config.combined_experiments_configs)
+        experiments_metrics = dict(iteration_config.combined_experiments_metrics)
+        configs = []
+        metrics = []
+        for key in experiments_metrics.keys():
+            configs.append(experiments_configs[key])
+            metrics.append(experiments_metrics[key])
+        optimizer = BOOptimizer(params_config=self.params_config)
+        optimizer.add_observations(configs=configs, metrics=metrics)
+        return [optimizer.get_suggestion()]
---
+++ polyaxon/experiment_groups/search_managers/bayesian_optimization/optimizer.py
@@ -0,0 +1,26 @@
+from experiment_groups.search_managers.bayesian_optimization.acquisition_function import \
+    UtilityFunction
+from experiment_groups.search_managers.bayesian_optimization.space import SearchSpace
+
+
+class BOOptimizer(object):
+    def __init__(self, params_config):
+        self.params_config = params_config
+        self.n_initial_trials = self.params_config.bo.n_initial_trials
+        self.space = SearchSpace(params_config=params_config)
+        self.utility_function = UtilityFunction(
+            config=params_config.bo.utility_function, seed=params_config.seed)
+
+    def _maximize(self):
+        """ Find argmax of the acquisition function."""
+        y_max = self.space.y.max()
+        self.utility_function.gaussian_process.fit(self.space.x, self.space.y)
+        return self.utility_function.max_compute(y_max=y_max, bounds=self.space.bounds)
+
+    def add_observations(self, configs, metrics):
+        # Turn configs and metrics into data points
+        self.space.add_observations(configs=configs, metrics=metrics)
+
+    def get_suggestion(self):
+        x = self._maximize()
+        return self.space.get_suggestion(x)
