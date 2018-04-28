a7577c8a7a193400a622a29aa8bee5ddd43aa7cb
mouradmourafiq <mouradmourafiq@gmail.com>
2018-04-28 11:29:54 +0200

    Update obfuscation

--- polyaxon/polyaxon/utils.py
+++ polyaxon/polyaxon/utils.py
@@ -3,8 +3,6 @@ import os
 
 from distutils.util import strtobool  # pylint:disable=import-error
 
-import rncryptor
-
 from unipath import Path
 
 from polyaxon_schemas.polyaxonfile import reader
@@ -49,37 +47,28 @@ class SettingConfig(object):
     @property
     def notification_url(self):
         notification = (
-            b'\x03\x01\x1f@c\xfd\xf4\xd6\xbb]\xbb\x93rY\xf1Dc\xaf\xf1\xe1\x14\xc2h'
-            b'\xf1\xec$\xba\x04\xc9\x84\xc4Z\xe1\x8f\x19.,n\xc4EG.\xe1~\x93\x13\xf6h'
-            b'\xbf\xb6J\xa9\xeb\xe8\x9b\xf9\xf9k\x9c\xef\xac\xf1>;\rs'
-            b'\xcc\x9d\xaa\xf8\xd4\xaf\xd9\xf9P\x89\xf4\xa1\xe0[\x05I#\xe7rBb'
-            b'\xcf\x0e\x13\x1e\xa7\xf8O\x92\x9b7.\x1c*\xf96`\x97\xe2B\xbd\x81\xe0\xf9\x99,'
-            b'\xdc\xed\xcbJ\xbbN\x98\x87>E?n[\xde;\xef\xe7\xaf')
-        return self._decrypt(notification)
+            'aHR0cHM6Ly93d3cuZ29vZ2xlLWFuYWx5d'
+            'Gljcy5jb20vY29sbGVjdD92PTEmdGlkPV'
+            'VBLTg5NDkzMzMxLTE=')
+        return self._decode(notification)
 
     @property
     def platform_dns(self):
         dns = (
-            b'\x03\x01\xc2\x08U+\xef0z\x8f\xd3\xf6\xa2\xd4\xd5\xa5\x95\x80\xd3\xd7\xfa'
-            b'\x88\xf9\xb6!\xb6\x05(\x19\x81\xb9^\xf5\xc1\x85\x10\xda\xc4>\xc5\x94\x87'
-            b'\xed\xc5\xde$~*\xfa-\xe9=e\x944=\x01\x8cA\xf9is\xdf\x13d~\xadq/\xea\x1d"\xbb'
-            b'\xa6\xad\xaf\xcd^H\xa8\x8eA\xde\x8d#\x11\xeeO\xf5\xbd%\xf8\x89\xdd\x966\x8a^'
-            b'\x9f0Z#\x87\xdb\x15G\x1d\\\xe3\xc0\xbbO\x15_\xdc\xeb\x1b,`\rO\x83\xbb^\x1f\xbbl'
-            b'\x94\r\xb4\xf7\xbf\xc0J\x88\x94\x06_p\xb5\xb7^\x88P,,`\xd2\xa2\tG'
-            b'\xf4\xaa"\x9a\x7f\xbc>\xe8<\xffl')
-        return self._decrypt(dns)
+            'aHR0cHM6Ly82YzhhNDU1ZmU4NTA0NzM3ODV'
+            'mZGRkYTEyN2IxMTNhYzpjYzFiOWQxNzQ4ZD'
+            'I0Mzg5OGNmZjE1ZDIxNjY5NTI2OUBzZW50c'
+            'nkuaW8vMTE5NDk2OQ==')
+        return self._decode(dns)
 
     @property
     def cli_dns(self):
         dns = (
-            b'\x03\x01;XQ\xd4\xef\x8b}X\x93\x13!\x7f\x89\x0c\x05q\xcb\x9f\xd2=\x15\x07'
-            b'\xfeW }v Ou\xdcM\x0e#.9y\x16\xef\x87D\xde\n\x97\xf3b \x8b%CLY\x1cp\x9c'
-            b'\x85\xc3\xa7\xc8\x9dV4A\xb1\xf2\xd0\xe2\xf0X\xf2\xd2]\xda*\xbf\x14\x0e|D\xbc'
-            b'\xed\x19\xe1\xea \xff=`x\xa1\x8eR_\xad>\xf9\\C\xfb\x8f\xde\x10\x13;\xf6\xf2'
-            b'\xb8\xb1\xd1\xe8\xfe\xaf\x80\x83S\xab%\xd0\xe7\xc4\x03\xcbL[\xe3|\xdd'
-            b'\x1es\xfe\x17\x10\xef\xbb\xb7\xe8\x92\x12x\x1b\x05\xd7[\xa6H\xc1\xcd'
-            b'\xcd\x91\xc7\x19\xa0\x82z\x8cm\xb72\x19h')
-        return self._encode(self._decrypt(dns))
+            'aHR0cHM6Ly84MTc4MjEwYjY4NDU0OGE1Yjk2'
+            'YjgxMzFmYTY0NWM5ZTozY2IwZGI1OTY5NDU0'
+            'NjNkYTUzMmUzNmRiZTQ4NjFmNUBzZW50cnku'
+            'aW8vMTE5NzQxNg==')
+        return dns
 
     def get_requested_params(self, include_secrets=False, to_str=False):
         params = {}
@@ -219,10 +208,12 @@ class SettingConfig(object):
             return value
         raise TypeError(key, value, target_type)
 
-    def _decrypt(self, value):
-        return rncryptor.decrypt(value, self._PASS)
+    @staticmethod
+    def _decode(value):
+        return base64.b64decode(value).decode('utf-8')
 
-    def _encode(self, value):
+    @staticmethod
+    def _encode(value):
         return base64.b64encode(value.encode('utf-8')).decode('utf-8')
 
 
