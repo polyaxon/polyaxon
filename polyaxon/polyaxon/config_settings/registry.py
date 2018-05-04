d162ddde6ca09acd13e4aff564cd615611d33dc0
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-04 11:43:28 +0200

    Rename REGISTRY_HOST_LOCAL to REGISTRY_HOST

--- polyaxon/polyaxon/config_settings/registry.py
+++ polyaxon/polyaxon/config_settings/registry.py
@@ -5,4 +5,4 @@ REGISTRY_PASSWORD = config.get_string('POLYAXON_REGISTRY_PASSWORD', is_optional=
 REGISTRY_HOST_NAME = config.get_string('POLYAXON_REGISTRY_HOST', is_optional=True)
 REGISTRY_PORT = config.get_string('POLYAXON_REGISTRY_PORT', is_optional=True)
 REGISTRY_NODE_PORT = config.get_string('POLYAXON_REGISTRY_NODE_PORT', is_optional=True)
-REGISTRY_HOST_LOCAL = '{}:{}'.format('127.0.0.1', REGISTRY_NODE_PORT)
+REGISTRY_HOST = '{}:{}'.format('127.0.0.1', REGISTRY_NODE_PORT)
