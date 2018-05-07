9de5182de49e56391137fbb28dbf07507ccf1130
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-07 14:29:10 +0200

    Add string import utils

---
+++ polyaxon/libs/imports.py
@@ -0,0 +1,27 @@
+class ImportStringCache(dict):
+    def __missing__(self, key):
+        if '.' not in key:
+            return __import__(key)
+
+        module_name, class_name = key.rsplit('.', 1)
+
+        module = __import__(module_name, {}, {}, [class_name])
+        handler = getattr(module, class_name)
+
+        # We cache a NoneType for missing imports to avoid repeated lookups
+        self[key] = handler
+
+        return handler
+
+
+_cache = ImportStringCache()
+
+
+def import_string(path):
+    """
+    Path must be module.path.ClassName
+
+    >>> cls = import_string('experiments.models.Experiment')
+    """
+    result = _cache[path]
+    return result
