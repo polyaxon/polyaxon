281cedfbb8c59e76d1a06cd635e35248ac6c3666
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-08 00:05:46 +0200

    Add auditor cluster's events

---
+++ polyaxon/auditor/events/cluster.py
@@ -0,0 +1,29 @@
+import auditor
+from libs.event_manager import event_types
+from libs.event_manager.event import Event
+
+
+class ClusterCreatedEvent(Event):
+    type = event_types.CLUSTER_CREATED
+
+
+class ClusterUpdatedEvent(Event):
+    type = event_types.CLUSTER_UPDATED
+
+
+class ClusterNodeCreatedEvent(Event):
+    type = event_types.CLUSTER_NODE_CREATED
+
+
+class ClusterNodeUpdatedEvent(Event):
+    type = event_types.CLUSTER_NODE_UPDATED
+
+
+class ClusterNodeDeletedEvent(Event):
+    type = event_types.CLUSTER_NODE_DELETED
+
+
+auditor.register(ClusterCreatedEvent)
+auditor.register(ClusterUpdatedEvent)
+auditor.register(ClusterNodeCreatedEvent)
+auditor.register(ClusterNodeUpdatedEvent)
