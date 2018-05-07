e67daa592c374382e8deeb5d0c19474cb9d7a16e
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-07 22:00:57 +0200

    Add json utils

---
+++ polyaxon/libs/json_utils.py
@@ -0,0 +1,89 @@
+from enum import Enum
+from json import JSONEncoder, _default_decoder
+import datetime
+import uuid
+import decimal
+
+from django.utils.timezone import is_aware
+from django.utils.html import mark_safe
+
+
+def better_default_encoder(o):
+    if isinstance(o, uuid.UUID):
+        return o.hex
+    elif isinstance(o, datetime.datetime):
+        return o.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    elif isinstance(o, datetime.date):
+        return o.isoformat()
+    elif isinstance(o, datetime.time):
+        if is_aware(o):
+            raise ValueError("JSON can't represent timezone-aware times.")
+        r = o.isoformat()
+        if o.microsecond:
+            r = r[:12]
+        return r
+    elif isinstance(o, (set, frozenset)):
+        return list(o)
+    elif isinstance(o, decimal.Decimal):
+        return str(o)
+    elif isinstance(o, Enum):
+        return o.value
+    elif callable(o):
+        return '<function>'
+    raise TypeError(repr(o) + ' is not JSON serializable')
+
+
+class JSONEncoderForHTML(JSONEncoder):
+    # Our variant of JSONEncoderForHTML that also accounts for apostrophes
+    # See: https://github.com/simplejson/simplejson/blob/master/simplejson/encoder.py#L380-L386
+    def encode(self, o):
+        # Override JSONEncoder.encode because it has hacks for
+        # performance that make things more complicated.
+        chunks = self.iterencode(o, True)
+        if self.ensure_ascii:
+            return ''.join(chunks)
+        else:
+            return u''.join(chunks)
+
+    def iterencode(self, o, _one_shot=False):
+        chunks = super(JSONEncoderForHTML, self).iterencode(o, _one_shot)
+        for chunk in chunks:
+            chunk = chunk.replace('&', '\\u0026')
+            chunk = chunk.replace('<', '\\u003c')
+            chunk = chunk.replace('>', '\\u003e')
+            chunk = chunk.replace("'", '\\u0027')
+            yield chunk
+
+
+_default_encoder = JSONEncoder(
+    separators=(',', ':'),
+    skipkeys=False,
+    ensure_ascii=True,
+    check_circular=True,
+    allow_nan=True,
+    indent=None,
+    default=better_default_encoder,
+)
+
+_default_escaped_encoder = JSONEncoderForHTML(
+    separators=(',', ':'),
+    skipkeys=False,
+    ensure_ascii=True,
+    check_circular=True,
+    allow_nan=True,
+    indent=None,
+    default=better_default_encoder,
+)
+
+
+def dump(value, fp, **kwargs):
+    for chunk in _default_encoder.iterencode(value):
+        fp.write(chunk)
+
+
+def loads(value, **kwargs):
+    return _default_decoder.decode(value)
+
+
+def dumps_htmlsafe(value):
+    return mark_safe(_default_escaped_encoder.encode(value))
