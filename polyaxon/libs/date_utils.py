77df5121fad2731e73bc56eb6987a7a1297c2939
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-07 21:59:54 +0200

    Add date utils

---
+++ polyaxon/libs/date_utils.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+
+import pytz
+
+
+epoch = datetime(1970, 1, 1, tzinfo=pytz.utc)
+
+
+def to_timestamp(value):
+    """Convert a time zone aware datetime to a POSIX timestamp (with fractional component.)"""
+    return (value - epoch).total_seconds()
+
+
+def to_datetime(value):
+    """Convert a POSIX timestamp to a time zone aware datetime.
+
+    The timestamp value must be a numeric type (either a integer or float,
+    since it may contain a fractional component.)
+    """
+    return epoch + timedelta(seconds=value)
