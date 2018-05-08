1d2ebb16f7e117da010289471786e39dbae1979a
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-08 18:11:37 +0200

    Add repo events attributes

--- polyaxon/event_manager/events/repo.py
+++ polyaxon/event_manager/events/repo.py
@@ -1,12 +1,25 @@
-from event_manager.event import Event
+from event_manager.event import Event, Attribute
 
 REPO_CREATED = 'repo.created'
-REPO_NEW_COMMIT = 'repo.new_commit'  # attempt, fullfilled
+REPO_NEW_COMMIT = 'repo.new_commit'
 
 
 class RepoCreatedEvent(Event):
     type = REPO_CREATED
+    attributes = (
+        Attribute('repo_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('project_owner_uuid', is_uuid=True),
+        Attribute('actor_uuid', is_uuid=True)
+    )
 
 
 class RepoNewCommitEvent(Event):
     type = REPO_NEW_COMMIT
+    attributes = (
+        Attribute('repo_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('project_owner_uuid', is_uuid=True),
+        Attribute('actor_uuid', is_uuid=True),
+        Attribute('failed', is_required=False)
+    )
