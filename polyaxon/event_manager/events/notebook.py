0772494fe3708e13c9df5b89aea0d3dad88e24cf
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-08 18:09:07 +0200

    Add tensorboard and notebook attributes

--- polyaxon/event_manager/events/notebook.py
+++ polyaxon/event_manager/events/notebook.py
@@ -1,4 +1,4 @@
-from event_manager.event import Event
+from event_manager.event import Event, Attribute
 
 NOTEBOOK_STARTED = 'notebook.started'
 NOTEBOOK_STOPPED = 'notebook.stopped'
@@ -7,11 +7,28 @@ NOTEBOOK_NEW_STATUS = 'notebook.new_status'
 
 class NotebookStartedEvent(Event):
     type = NOTEBOOK_STARTED
+    attributes = (
+        Attribute('notebook_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('project_owner_uuid', is_uuid=True),
+        Attribute('actor_uuid')
+    )
 
 
 class NotebookSoppedEvent(Event):
     type = NOTEBOOK_STOPPED
+    attributes = (
+        Attribute('notebook_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('project_owner_uuid', is_uuid=True),
+        Attribute('actor_uuid')
+    )
 
 
 class NotebookNewStatusEvent(Event):
     type = NOTEBOOK_NEW_STATUS
+    attributes = (
+        Attribute('notebook_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('statue')
+    )
--- polyaxon/event_manager/events/tensorboard.py
+++ polyaxon/event_manager/events/tensorboard.py
@@ -1,4 +1,4 @@
-from event_manager.event import Event
+from event_manager.event import Event, Attribute
 
 TENSORBOARD_STARTED = 'tensorboard.started'
 TENSORBOARD_STOPPED = 'tensorboard.stopped'
@@ -7,11 +7,28 @@ TENSORBOARD_NEW_STATUS = 'tensorboard.new_status'
 
 class TensorboardStartedEvent(Event):
     type = TENSORBOARD_STARTED
+    attributes = (
+        Attribute('tensorboard_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('project_owner_uuid', is_uuid=True),
+        Attribute('actor_uuid')
+    )
 
 
 class TensorboardSoppedEvent(Event):
     type = TENSORBOARD_STOPPED
+    attributes = (
+        Attribute('tensorboard_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('project_owner_uuid', is_uuid=True),
+        Attribute('actor_uuid')
+    )
 
 
 class TensorboardNewStatusEvent(Event):
     type = TENSORBOARD_NEW_STATUS
+    attributes = (
+        Attribute('tensorboard_uuid', is_uuid=True),
+        Attribute('project_uuid', is_uuid=True),
+        Attribute('statue')
+    )
