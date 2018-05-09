ced50bc5a0d9c4a57ace8df7d05b4d5136a788e8
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-09 11:06:17 +0200

    Add attribute tests

---
+++ tests/test_event_manager/test_attribute.py
@@ -0,0 +1,33 @@
+import uuid
+from datetime import datetime
+
+from dateutil.tz import UTC
+
+from event_manager.event import Attribute
+from tests.utils import BaseTest
+
+
+class TestAttribute(BaseTest):
+    def test_props(self):
+        attr = Attribute(name='test')
+        assert attr.name == 'test'
+        assert attr.attr_type == str
+        assert attr.is_datetime is False
+        assert attr.is_uuid is False
+        assert attr.is_required is True
+
+    def test_extract(self):
+        attr = Attribute(name='test')
+        assert attr.extract(value='some value') == 'some value'
+        assert attr.extract(value=1) == '1'
+
+        attr = Attribute(name='test', attr_type=int)
+        assert attr.extract(value=1) == 1
+
+        attr = Attribute(name='test', is_datetime=True)
+        dt = datetime(2000, 12, 12, tzinfo=UTC)
+        assert attr.extract(value=dt) == 976579200.0
+
+        attr = Attribute(name='test', is_uuid=True)
+        uid = uuid.uuid4()
+        assert attr.extract(value=uid) == uid.hex
