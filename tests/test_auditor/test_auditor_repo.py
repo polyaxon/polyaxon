59d01f2866550512a789429da30192a599e04e2c
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-10 14:58:04 +0200

    Add auditor repo tests

---
+++ tests/test_auditor/test_auditor_repo.py
@@ -0,0 +1,48 @@
+# pylint:disable=ungrouped-imports
+
+from unittest.mock import patch
+
+from django.test import override_settings
+
+import activitylogs
+import auditor
+import tracker
+
+from event_manager.events import repo as repo_events
+from factories.factory_repos import RepoFactory
+from tests.utils import BaseTest
+
+
+@override_settings(DEPLOY_RUNNER=False)
+class AuditorRepoTest(BaseTest):
+    """Testing subscribed events"""
+
+    def setUp(self):
+        self.project = RepoFactory()
+        auditor.validate()
+        auditor.setup()
+        tracker.validate()
+        tracker.setup()
+        activitylogs.validate()
+        activitylogs.setup()
+        super(AuditorRepoTest, self).setUp()
+
+    @patch('tracker.service.TrackerService.record_event')
+    @patch('activitylogs.service.ActivityLogService.record_event')
+    def test_repo_created(self, activitylogs_record, tracker_record):
+        auditor.record(event_type=repo_events.REPO_CREATED,
+                       instance=self.project,
+                       actor_id=1)
+
+        assert tracker_record.call_count == 1
+        assert activitylogs_record.call_count == 1
+
+    @patch('tracker.service.TrackerService.record_event')
+    @patch('activitylogs.service.ActivityLogService.record_event')
+    def test_repo_new_commit(self, activitylogs_record, tracker_record):
+        auditor.record(event_type=repo_events.REPO_NEW_COMMIT,
+                       instance=self.project,
+                       actor_id=1)
+
+        assert tracker_record.call_count == 1
+        assert activitylogs_record.call_count == 1
