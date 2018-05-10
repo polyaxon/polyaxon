5f3043968ebd8201f4dee18757471ff60ffa50b0
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-10 10:25:31 +0200

    Add tracker tests

---
+++ tests/test_tracker/test_tracker.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.test import override_settings
+
+import tracker
+
+from tests.utils import BaseTest
+
+
+class TrackerTest(BaseTest):
+    def test_default_backend_tracker(self):
+        backend = tracker.get_tracker_backend()
+        assert backend == 'tracker.service.TrackerService'
+
+    @override_settings(TRACKER_BACKEND=settings.TRACKER_BACKEND_PUBLISHER)
+    def test_publisher_backend_tracker(self):
+        backend = tracker.get_tracker_backend()
+        assert backend == 'tracker.publish_tracker.PublishTrackerService'
