5eaf963334a8f8ac9f69f34f083dc6311d0660cb
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-16 13:44:54 +0200

    Update core settings

--- polyaxon/polyaxon/config_settings/core.py
+++ polyaxon/polyaxon/config_settings/core.py
@@ -11,11 +11,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 SSL_ENABLED = config.get_boolean('POLYAXON_SSL_ENABLED', is_optional=True, default=False)
-CORS_ORIGIN_WHITELIST = config.get_string('POLYAXON_CORS_ORIGIN_WHITELIST',
-                                          is_optional=True,
-                                          default=False)
+CORS_ORIGIN_WHITELIST = config.get_string('POLYAXON_CORS_ORIGIN_WHITELIST', is_optional=True)
 if CORS_ORIGIN_WHITELIST:
     CORS_ORIGIN_WHITELIST = [i.strip() for i in CORS_ORIGIN_WHITELIST.split(',')]
+else:
+    CORS_ORIGIN_WHITELIST = []
 
 CORS_ALLOW_HEADERS = default_headers + (
     'x-polyaxon-cli-version',
