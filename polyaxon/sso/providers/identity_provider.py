d8ba5b63578f33c147ed7799d28ab317434cd23d
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-17 11:53:36 +0200

    Add identity provider

---
+++ polyaxon/sso/providers/identity_provider.py
@@ -0,0 +1,39 @@
+from libs.wizards import WizardProvider
+
+
+class IdentityProvider(WizardProvider):
+    """The `identityBuilder` class provides a wizard provider to construct an identity."""
+
+    def __init__(self, **kwargs):
+        self.config = kwargs
+
+    def build_identity(self, state):
+        """Return a mapping containing the identity information.
+
+        The resulting data captured by the pipeline
+
+            >>> {
+            >>>     "id":     "foo@example.com",
+            >>>     "email":  "foo@example.com",
+            >>>     "name":   "Foo Bar",
+            >>>     "scopes": ['emaill', ...],
+            >>>     "data":   { ... },
+            >>> }
+
+
+        Raises:
+             InvalidIdentity
+        """
+        raise NotImplementedError
+
+    def update_identity(self, new_data, current_data):
+        """Return the new state which should be used for an identity."""
+        return new_data
+
+    def refresh_identity(self, auth_identity):
+        """Refresh identity from upstream.
+
+        Raises:
+             InvalidIdentity
+        """
+        raise NotImplementedError
