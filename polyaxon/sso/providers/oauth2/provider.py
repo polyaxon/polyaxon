125a9df704709c8109be7f14b29cb44a435d2e98
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-17 13:47:12 +0200

    Add oauth providers

---
+++ polyaxon/sso/providers/oauth2/provider.py
@@ -0,0 +1,54 @@
+from time import time
+
+from sso.providers.identity_provider import IdentityProvider
+from sso.providers.oauth2.views import OAuth2LoginView, OAuth2CallbackView
+
+
+class OAuth2Provider(IdentityProvider):
+    """The `OAuth2Provider` provides a generic identity provider
+    that uses the OAuth 2.0 protocol as a means for authenticating a user.
+
+    OAuth scopes are configured through the oauth_scopes class property,
+    however may be overriden using the ``config['oauth_scopes']`` object.
+    """
+    oauth_access_token_url = ''
+    oauth_authorize_url = ''
+    refresh_token_url = ''
+
+    oauth_scopes = ()
+
+    def get_oauth_client_id(self):
+        raise NotImplementedError
+
+    def get_oauth_client_secret(self):
+        raise NotImplementedError
+
+    def get_oauth_scopes(self):
+        return self.config.get('oauth_scopes', self.oauth_scopes)
+
+    def get_wizard_views(self):
+        return [
+            OAuth2LoginView(
+                authorize_url=self.oauth_authorize_url,
+                client_id=self.get_oauth_client_id(),
+                scope=' '.join(self.get_oauth_scopes()),
+            ),
+            OAuth2CallbackView(
+                access_token_url=self.oauth_access_token_url,
+                client_id=self.get_oauth_client_id(),
+                client_secret=self.get_oauth_client_secret(),
+            ),
+        ]
+
+    @staticmethod
+    def get_oauth_data(payload):
+        data = {'access_token': payload['access_token']}
+
+        if 'expires_in' in payload:
+            data['expires_in'] = int(time()) + payload['expires_in']
+        if 'refresh_token' in payload:
+            data['refresh_token'] = payload['refresh_token']
+        if 'token_type' in payload:
+            data['token_type'] = payload['token_type']
+
+        return data
---
+++ polyaxon/sso/providers/oauth2/views.py
@@ -0,0 +1,122 @@
+import uuid
+from urllib.parse import urljoin, urlencode, parse_qsl
+from urllib.request import urlopen
+
+from django.http import HttpResponseRedirect
+from django.views import View
+
+from libs.json_utils import loads
+
+ERR_INVALID_STATE = 'An error occurred while validating your request.'
+
+
+class OAuth2LoginView(View):
+    authorize_url = None
+    client_id = None
+    scope = ''
+
+    def __init__(self, authorize_url=None, client_id=None, scope=None, *args, **kwargs):
+        super(OAuth2LoginView, self).__init__(*args, **kwargs)
+        if authorize_url is not None:
+            self.authorize_url = authorize_url
+        if client_id is not None:
+            self.client_id = client_id
+        if scope is not None:
+            self.scope = scope
+
+    def get_scope(self):
+        return self.scope
+
+    def get_authorize_url(self):
+        return self.authorize_url
+
+    def get_authorize_params(self, state, redirect_uri):
+        return {
+            'client_id': self.client_id,
+            'response_type': "code",
+            'scope': self.get_scope(),
+            'state': state,
+            'redirect_uri': redirect_uri,
+        }
+
+    def dispatch(self, request, wizard, *args, **kwargs):
+        if 'code' in request.GET:
+            return wizard.next_step()
+
+        state = uuid.uuid4().hex
+
+        params = self.get_authorize_params(
+            state=state,
+            redirect_uri=urljoin('/', wizard.redirect_url()),
+        )
+        redirect_uri = '{}?{}'.format(self.get_authorize_url(), urlencode(params))
+
+        wizard.bind_state('state', state)
+
+        return self.redirect(redirect_uri)
+
+    def redirect(self, url):
+        return HttpResponseRedirect(url)
+
+
+class OAuth2CallbackView(View):
+    access_token_url = None
+    client_id = None
+    client_secret = None
+
+    def __init__(self, access_token_url=None, client_id=None, client_secret=None, *args, **kwargs):
+        super(OAuth2CallbackView, self).__init__(*args, **kwargs)
+        if access_token_url is not None:
+            self.access_token_url = access_token_url
+        if client_id is not None:
+            self.client_id = client_id
+        if client_secret is not None:
+            self.client_secret = client_secret
+
+    def get_token_params(self, code, redirect_uri):
+        return {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': redirect_uri,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+        }
+
+    def exchange_token(self, request, wizard, code):
+        data = self.get_token_params(
+            code=code,
+            redirect_uri=urljoin('/', wizard.redirect_url()),
+        )
+        response = urlopen(self.access_token_url, data=data)
+        content = response.content
+        if response.headers['Content-Type'].startswith('application/x-www-form-urlencoded'):
+            return dict(parse_qsl(content))
+        return loads(content)
+
+    def dispatch(self, request, wizard, *args, **kwargs):
+        error = request.GET.get('error')
+        state = request.GET.get('state')
+        code = request.GET.get('code')
+
+        if error:
+            wizard.logger.info('identity.token-exchange-error', extra={'error': error})
+            return wizard.error(error)
+
+        if state != wizard.fetch_state('state'):
+            wizard.logger.info('identity.token-exchange-error', extra={'error': 'invalid_state'})
+            return wizard.error(ERR_INVALID_STATE)
+
+        data = self.exchange_token(request, wizard, code)
+
+        if 'error_description' in data:
+            error = data.get('error')
+            wizard.logger.info('identity.token-exchange-error', extra={'error': error})
+            return wizard.error(data['error_description'])
+
+        if 'error' in data:
+            wizard.logger.info('identity.token-exchange-error', extra={'error': data['error']})
+            return wizard.error('Failed to retrieve token from the upstream service.')
+
+        wizard.bind_state('data', data)
+
+        return wizard.next_step()
