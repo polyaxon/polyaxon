e86bd481873727f2b7ca6149e5bc5b091950766d
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-17 18:37:09 +0200

    Add account create identity

---
+++ polyaxon/sso/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.views import View
+
+from sso.models import SSOProvider
+from sso.wizard import IdentityWizard
+
+
+class AccountCreateIdentityView(View):
+    def dispatch(self, request, provider, *args, **kwargs):
+        if not IdentityWizard.manager.knows(provider):
+            raise Http404
+        wizard = IdentityWizard.get_for_request(request)
+
+        if wizard is None or not wizard.is_valid():
+            wizard = IdentityWizard(
+                provider_key=provider,
+                provider_model=SSOProvider,
+                request=request,
+            )
+
+            if request.method != 'POST' and not wizard.is_valid():
+                return HttpResponseRedirect(reverse('users:login'))
+
+            wizard.initialize()
+
+        return wizard.current_step()
