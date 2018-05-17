a8beb5364c42238605222ef6769c53b595073f8b
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-17 10:33:31 +0200

    Add wizard provider

---
+++ polyaxon/libs/wizards/provider.py
@@ -0,0 +1,14 @@
+class WizardProvider(object):
+    """The base provider class implementing an interface for wizard views."""
+
+    def get_wizard_views(self):
+        """Returns a list of WizardView instance which will be dispatched in order.
+
+        Example:
+            >>> return [View1(), View2(), View3()]
+        """
+        raise NotImplementedError
+
+    def set_config(self, config):
+        """Used to allow additional provider configuration to be added to the provider instance."""
+        self.config = config
