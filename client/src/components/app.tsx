5c48651c290e38594966431492969bf76d171924
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-18 09:58:07 +0200

    Remove header from react app

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Header from './header';
 import Routes from './routes';
 
 import './app.less';
@@ -10,10 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <Header/>
-        <div className="container container-fluid">
-          <Routes/>
-        </div>
+        <Routes/>
       </div>
     </BrowserRouter>
   );
