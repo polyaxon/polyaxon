50f0ea44600c69eb77e5e651a3d210a3bad8b273
gzc <zhecongguo@linkdoc.com>
2018-05-18 20:00:51 +0800

    add tests for ldap auth

--- polyaxon/auth_ldap/signals.py
+++ polyaxon/auth_ldap/signals.py
@@ -6,6 +6,8 @@ from django_auth_ldap.backend import populate_user
 
 logger = logging.getLogger('polyaxon.auth_ldap')
 
+DEFAULT_EMAIL_DOMAIN = 'polyaxon-ldap-users.com'
+
 @receiver(populate_user)
 def populate_user(sender, **kwargs):
     user = kwargs['user']
@@ -17,6 +19,6 @@ def populate_user(sender, **kwargs):
             value = ldap_user.attrs['mail'][0]
         except LookupError:
             logger.warning("{} does not have a value for the attribute {}".format(ldap_user.dn, 'mail'))
-            user.email = '%s@polyaxon-ldap-users.com' % user.username
+            user.email = '%s@%s' % (user.username, DEFAULT_EMAIL_DOMAIN)
         else:
             user.email = value
