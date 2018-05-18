334b8f473e6848c02105351158675227459ecda3
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-18 09:57:38 +0200

    Add base api url

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,8 +1,9 @@
 let baseUrl;
 if (process.env.NODE_ENV !== 'production') {
-  baseUrl = 'http://localhost:8000/api/v1';
+  baseUrl = 'http://localhost:8000';
 } else {
-  baseUrl = location.origin + '/api/v1';
+  baseUrl = location.origin;
 }
 
 export const BASE_URL = baseUrl;
+export const BASE_API_URL = `${baseUrl}/api/v1`;
