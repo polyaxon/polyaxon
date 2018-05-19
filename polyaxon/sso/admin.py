29a9d56348f131545b08d4b7b1f17733b6fbb32c
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-19 12:05:21 +0200

    Update sso models

--- polyaxon/sso/admin.py
+++ polyaxon/sso/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 
 from libs.admin import DiffModelAdmin
-from sso.models import SSOIdentity, SSOProvider
+from sso.models import SSOIdentity
 
 
 class SSOIdentityAdmin(DiffModelAdmin):
     pass
 
 
-class SSOProviderAdmin(DiffModelAdmin):
-    pass
-
-
 admin.site.register(SSOIdentity, SSOIdentityAdmin)
-admin.site.register(SSOProvider, SSOProviderAdmin)
--- polyaxon/sso/models.py
+++ polyaxon/sso/models.py
@@ -1,20 +1,10 @@
 from django.conf import settings
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField, JSONField
 from django.db import models
 from django.utils import timezone
-from rest_framework.fields import JSONField
 
 from libs.models import DiffModel
 from sso.providers.constants import PROVIDERS
-
-
-class SSOProvider(DiffModel):
-    name = models.CharField(max_length=54,
-                            choices=PROVIDERS.CHOICES)
-    is_enabled = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
 
 
 class SSOIdentity(DiffModel):
@@ -23,31 +13,29 @@ class SSOIdentity(DiffModel):
         on_delete=models.CASCADE,
         related_name='identities'
     )
-    provider = models.ForeignKey(
-        SSOProvider,
-        on_delete=models.CASCADE,
-        related_name='identities'
-    )
-    is_valid = models.BooleanField(default=False)
-    auth_provider = models.ForeignKey(SSOProvider, models.CASCADE)
+    provider = models.CharField(
+        max_length=32,
+        choices=PROVIDERS.CHOICES)
     external_id = models.CharField(max_length=64, null=True)
+    valid = models.BooleanField(default=False)
     last_verified = models.DateTimeField(default=timezone.now)
     last_synced = models.DateTimeField(default=timezone.now)
     scopes = ArrayField(models.CharField(max_length=200), blank=True, null=True)
     data = JSONField()
 
     class Meta:
-        unique_together = (('auth_provider', 'user'),)
+        unique_together = (('provider', 'user'), ('provider', 'external_id'),)
+        verbose_name_plural = 'SSOIdentities'
 
     def __str__(self):
-        return '{} - {}'.format(self.user, self.auth_provider)
+        return '{} - {}'.format(self.user, self.provider)
 
     def is_valid(self):
-        if not self.auth_provider.is_enabled:
+        if not self.valid:
             return False
         if not self.last_verified:
             return False
-        verification_schedule = PROVIDERS.get_verification_schedule(self.auth_provider.name)
+        verification_schedule = PROVIDERS.get_verification_schedule(self.provider)
         if self.last_verified < timezone.now() - verification_schedule:
             return False
         return True
