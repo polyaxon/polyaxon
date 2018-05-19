ee85d29ab2ab22c30e9a9ccc7236f83a8461e3be
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-19 18:24:41 +0200

    Add gitlab oauth provider

--- polyaxon/sso/providers/__init__.py
+++ polyaxon/sso/providers/__init__.py
@@ -1,9 +1,11 @@
 from libs.wizards import WizardManager
 from sso.providers.bitbucket_provider import BitbucketIdentityProvider
 from sso.providers.github_provider import GitHubIdentityProvider
+from sso.providers.gitlab_provider import GitLabIdentityProvider
 
 default_manager = WizardManager()
 subscribe = default_manager.subscribe
 
 subscribe(BitbucketIdentityProvider)
 subscribe(GitHubIdentityProvider)
+subscribe(GitLabIdentityProvider)
---
+++ polyaxon/sso/providers/gitlab_provider.py
@@ -0,0 +1,69 @@
+import requests
+
+from django.conf import settings
+
+from sso.providers.constants import PROVIDERS
+from sso.providers.oauth2.provider import OAuth2Provider
+
+
+class GitLabIdentityProvider(OAuth2Provider):
+    key = 'gitlab'
+    name = 'GitLab'
+
+    web_url = settings.OAUTH_PROVIDERS.GITLAB.URL or 'https://gitlab.com'
+    api_url = '{}/api/v3'.format(web_url)
+    oauth_access_token_url = '{}/oauth/token'.format(web_url)
+    oauth_authorize_url = '{}/oauth/authorize'.format(web_url)
+    user_url = '{}/user'.format(api_url)
+
+    oauth_scopes = ('read_user',)
+
+    def get_oauth_client_id(self):
+        return settings.OAUTH_PROVIDERS.GITLAB.CLIENT_ID
+
+    def get_oauth_client_secret(self):
+        return settings.OAUTH_PROVIDERS.GITLAB.CLIENT_SECRET
+
+    def get_user(self, access_token):
+        resp = requests.get(self.user_url, params={'access_token': access_token})
+        resp.raise_for_status()
+        return resp.json()
+
+    def get_emails(self, access_token):
+        resp = requests.get(self.emails_url, params={'access_token': access_token})
+        resp.raise_for_status()
+        return resp.json()
+
+    def get_email(self, email, username):
+        return email if email else '{}@{}'.format(username, settings.DEFAULT_EMAIL_DOMAIN)
+
+    @staticmethod
+    def get_first_last_names(username, name):
+        name = name or username
+        name = name.split()
+        if len(name) > 1:
+            first = name[0]
+            last = ' '.join(name[1:])
+        else:
+            first = name[0]
+            last = ''
+        return first, last
+
+    def build_identity(self, state_data):
+        data = state_data['data']
+        access_token = data['access_token']
+        user = self.get_user(access_token=access_token)
+        username = user['username']
+        first_name, last_name = self.get_first_last_names(username=username, name=user.get('name'))
+        email = self.get_email(user.get('email'), username=username)
+
+        return {
+            'type': PROVIDERS.GITLAB,
+            'id': user['id'],
+            'email': email,
+            'username': username,
+            'first_name': first_name,
+            'last_name': last_name,
+            'scopes': [],
+            'data': self.get_oauth_data(data),
+        }
