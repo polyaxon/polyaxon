cea858e70f8e5496e77f2e9507669aff935bf177
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-19 12:00:10 +0200

    Extract login and logout logic

---
+++ polyaxon/users/utils.py
@@ -0,0 +1,21 @@
+from rest_framework.authtoken.models import Token
+
+from django.contrib.auth import login as auth_login, logout as auth_logout
+
+
+def login_user(request, response, user, login=True):
+    if login:
+        auth_login(request, user)
+
+    if user.is_authenticated:
+        token, _ = Token.objects.get_or_create(user=request.user)
+        response.set_cookie('token', value=token)
+        response.set_cookie('user', value=request.user.username)
+
+
+def logout_user(request, response, logout=True):
+    if logout:
+        auth_logout(request)
+
+    response.delete_cookie('token')
+    response.delete_cookie('user')
