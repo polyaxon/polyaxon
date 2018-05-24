b9a98c3626a9eb93e741cf9b5cf183a0e4656787
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-24 16:02:02 +0200

    Refactor settings

---
+++ polyaxon/polyaxon/config_settings/__init__.py
@@ -0,0 +1,7 @@
+from polyaxon.config_settings.admin import *
+from polyaxon.config_settings.celery_settings import *
+from polyaxon.config_settings.core import *
+from polyaxon.config_settings.debug import *
+from polyaxon.config_settings.logging import *
+from polyaxon.config_settings.secrets import *
+from polyaxon.config_settings.versions import *
