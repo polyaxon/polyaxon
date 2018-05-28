d0781c183fb00e021892f442a85258895bacc8dd
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-28 18:09:29 +0200

    Import services configs

--- polyaxon/polyaxon/config_settings/__init__.py
+++ polyaxon/polyaxon/config_settings/__init__.py
@@ -1,8 +1,34 @@
-from polyaxon.config_settings.admin import *
-from polyaxon.config_settings.celery_settings import *
-from polyaxon.config_settings.core import *
-from polyaxon.config_settings.debug import *
-from polyaxon.config_settings.logging import *
-from polyaxon.config_settings.secrets import *
-from polyaxon.config_settings.versions import *
-from polyaxon.config_settings.tracker import *
+from .admin import *
+from .celery_settings import *
+from .context_processors import *
+from .core import *
+from .debug import *
+from .logging import *
+from .secrets import *
+from .versions import *
+from .tracker import *
+
+from polyaxon.utils import config
+
+if config.is_monolith_service:
+    from .monolith import *
+elif config.is_api_service:
+    from .monolith import *
+elif config.is_dockerizer_service:
+    from .dockerizer import *
+elif config.is_crons_service:
+    from .crons import *
+elif config.is_namespace_monitor_service:
+    from .namespace_monitor import *
+elif config.is_resources_monitor_service:
+    from .resources_monitor import *
+elif config.is_statuses_monitor_service:
+    from .statuses_monitor import *
+elif config.is_statuses_monitor_service:
+    from .statuses_monitor import *
+elif config.is_sidecar_service:
+    from .sidecar import *
+elif config.is_streams_service:
+    from .streams import *
+elif config.is_hpsearch_service:
+    from .streams import *
