ad9ac6fe8c71f3c044d5cf24819cd93a845b3894
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-28 16:21:11 +0200

    Disable i18n

--- polyaxon/polyaxon/config_settings/context_processors.py
+++ polyaxon/polyaxon/config_settings/context_processors.py
@@ -3,7 +3,6 @@ from polyaxon.utils import ROOT_DIR, config
 LIST_TEMPLATE_CONTEXT_PROCESSORS = [
     'django.contrib.auth.context_processors.auth',
     'django.template.context_processors.debug',
-    'django.template.context_processors.i18n',
     'django.template.context_processors.media',
     'django.template.context_processors.static',
     'django.template.context_processors.tz',
@@ -20,19 +19,3 @@ if JS_DEBUG:
         return {'js_debug': True}
 
     LIST_TEMPLATE_CONTEXT_PROCESSORS += ('polyaxon.settings.js_debug_processor',)
-
-TEMPLATES_DEBUG = (config.get_boolean('DJANGO_TEMPLATE_DEBUG', is_optional=True) or
-                   config.is_debug_mode)
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            ROOT_DIR.child('polyaxon').child('polyaxon').child('templates'),
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'debug': TEMPLATES_DEBUG,
-            'context_processors': LIST_TEMPLATE_CONTEXT_PROCESSORS,
-        },
-    },
-]
