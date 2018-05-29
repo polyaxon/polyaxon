d81dbc435d85789335a308526e897bab6265e887
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-29 15:55:45 +0200

    Setup streams service

--- polyaxon/polyaxon/config_settings/streams/apps.py
+++ polyaxon/polyaxon/config_settings/streams/apps.py
@@ -5,11 +5,4 @@ PROJECT_APPS = AUDITOR_APPS + (
     'statuses_monitor.apps.StatusesMonitorConfig',
 )
 
-THIRD_PARTY_APPS = (
-    'rest_framework',
-    'rest_framework.authtoken',
-    'corsheaders',
-)
-
-
 INSTALLED_APPS += THIRD_PARTY_APPS + PROJECT_APPS
