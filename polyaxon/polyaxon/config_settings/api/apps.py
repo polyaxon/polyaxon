84eedb4b0d80113a6b09ad1b625c299f5767b88f
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-29 16:05:09 +0200

    Setup api service

--- polyaxon/polyaxon/config_settings/api/apps.py
+++ polyaxon/polyaxon/config_settings/api/apps.py
@@ -3,6 +3,8 @@ from ..auditor_apps import AUDITOR_APPS
 
 PROJECT_APPS = AUDITOR_APPS + (
     'api.apps.APIsConfig',
+    'publisher.apps.PublisherConfig',
+    'scheduler.apps.SchedulerConfig',
 )
 
 INSTALLED_APPS += PROJECT_APPS
