45aba34442be9e0e41dadd187bc153adbf25bb94
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-30 11:01:26 +0200

    Add streams app

--- polyaxon/polyaxon/config_settings/streams/apps.py
+++ polyaxon/polyaxon/config_settings/streams/apps.py
@@ -2,7 +2,7 @@ from ..apps import *
 from ..auditor_apps import AUDITOR_APPS
 
 PROJECT_APPS = AUDITOR_APPS + (
-    'monitor_statuses.apps.StatusesMonitorConfig',
+    'streams.apps.StreamsConfig',
 )
 
 INSTALLED_APPS += THIRD_PARTY_APPS + PROJECT_APPS
