9b04fcbb609f3b776fece56d0944a88d81cbc322
mouradmourafiq <mouradmourafiq@gmail.com>
2018-05-31 21:30:40 +0200

    Add dockerizer spawner

--- polyaxon/scheduler/spawners/dockerizer_spawner.py
+++ polyaxon/scheduler/spawners/dockerizer_spawner.py
@@ -2,54 +2,61 @@ import logging
 
 from django.conf import settings
 
-from scheduler.spawners.project_spawner import ProjectSpawner
-from scheduler.spawners.templates import constants, deployments
+from polyaxon.utils import config
+from scheduler.spawners.project_job_spawner import ProjectJobSpawner
+from scheduler.spawners.templates import constants
+from scheduler.spawners.templates.env_vars import get_env_var, get_from_app_secret
+from scheduler.spawners.templates.project_jobs import pods
 
-logger = logging.getLogger('polyaxon.spawners.tensorboard')
+logger = logging.getLogger('polyaxon.spawners.dockerizer')
 
 
-class DockerizerSpawner(ProjectSpawner):
-    DOCKERIZER_JOB_NAME = 'build-{}'
+class DockerizerSpawner(ProjectJobSpawner):
+    DOCKERIZER_JOB_NAME = 'build'
 
-    def build_notebook(self, project_uuid, commit, resources=None, node_selectors=None):
-        command_args = ["notebook", "{}".format(project_uuid)]
-        self.start_dockerizer(commit=commit,
-                              command_args=command_args,
-                              resources=resources,
-                              node_selectors=node_selectors)
+    def get_env_vars(self):
+        env_vars = [
+            get_env_var(name=constants.CONFIG_MAP_API_KEY_NAME,
+                        value='http://{}:{}'.format(settings.POLYAXON_K8S_API_HOST,
+                                                    settings.POLYAXON_K8S_API_PORT)),
+            get_env_var(name='POLYAXON_K8S_NAMESPACE', value=self.namespace),
+            get_from_app_secret(key_name='POLYAXON_SECRET_KEY', secret_key_name='polyaxon-secret'),
+            get_from_app_secret(key_name='POLYAXON_RABBITMQ_PASSWORD',
+                                secret_key_name='rabbitmq-password',
+                                secret_ref_name=settings.POLYAXON_K8S_RABBITMQ_SECRET_NAME)
+        ]
+        for k, v in config.get_requested_params(to_str=True).items():
+            env_vars.append(get_env_var(name=k, value=v))
 
-    def build_experiment(self, experiment_uuid, commit, resources=None, node_selectors=None):
-        command_args = ["experiment", "{}".format(experiment_uuid)]
-        self.start_dockerizer(commit=commit,
-                              command_args=command_args,
-                              resources=resources,
-                              node_selectors=node_selectors)
+        return env_vars
 
-    def start_dockerizer(self, commit, command_args, resources=None, node_selectors=None):
-        deployment = deployments.get_deployment(
+    def start_dockerizer(self, commit, resources=None, node_selectors=None):
+        deployment = pods.get_pod(
             namespace=self.namespace,
             app=settings.APP_LABELS_TENSORBOARD,
             name=self.DOCKERIZER_JOB_NAME.format(commit),
             project_name=self.project_name,
             project_uuid=self.project_uuid,
+            job_name=self.job_name,
+            job_uuid=self.job_uuid,
             volume_mounts=[],
             volumes=[],
             image=settings.DOCKERIZER_IMAGE,
             command=None,
-            args=command_args,
+            args=["--build_job_uuid={}".format(self.job_uuid)],
             ports=[],
+            env_vars=self.get_env_vars(),
             container_name=settings.CONTAINER_NAME_DOCKERIZER_JOB,
             resources=resources,
             node_selector=node_selectors,
             role=settings.ROLE_LABELS_DOCKERIZER,
             type=settings.TYPE_LABELS_EXPERIMENT)
-        deployment_name = constants.DEPLOYMENT_NAME.format(
-            project_uuid=self.project_uuid, name=self.DOCKERIZER_JOB_NAME)
+        pod_name = constants.DEPLOYMENT_NAME.format(
+            job_uuid=self.job_uuid, name=self.DOCKERIZER_JOB_NAME)
 
-        self.create_or_update_deployment(name=deployment_name, data=deployment)
+        self.create_or_update_pod(name=pod_name, data=deployment)
 
-    def stop_dockerizer(self, commit):
-        name = self.DOCKERIZER_JOB_NAME.format(commit)
-        deployment_name = constants.DEPLOYMENT_NAME.format(project_uuid=self.project_uuid,
-                                                           name=name)
-        self.delete_deployment(name=deployment_name)
+    def stop_dockerizer(self):
+        pod_name = constants.DEPLOYMENT_NAME.format(job_uuid=self.job_uuid,
+                                                    name=self.DOCKERIZER_JOB_NAME)
+        self.delete_pod(name=pod_name)
