0c79d2fee14d5d2bff51ade9d643df22dde7f301
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-03 00:08:51 +0200

    Add registry settings to scheduler

--- polyaxon/polyaxon/config_settings/scheduler/__init__.py
+++ polyaxon/polyaxon/config_settings/scheduler/__init__.py
@@ -1,4 +1,5 @@
 from polyaxon.config_settings.k8s import *
 from polyaxon.config_settings.dirs import *
 from polyaxon.config_settings.spawner import *
+from polyaxon.config_settings.registry import *
 from .apps import *
