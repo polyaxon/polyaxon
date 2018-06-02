0876a8abc99ac3aa93f5b44aef63d0aa739c75b3
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-02 21:16:56 +0200

    Remove unused import

--- polyaxon_schemas/polyaxonfile/specification/plugin.py
+++ polyaxon_schemas/polyaxonfile/specification/plugin.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-import copy
 from collections import Mapping
 
 from polyaxon_schemas.exceptions import PolyaxonConfigurationError
