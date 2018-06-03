d72e4395485e82e8bfbaf787a7423373068ac22d
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-03 12:31:15 +0200

    Add code reference factory

---
+++ polyaxon/factories/code_reference.py
@@ -0,0 +1,16 @@
+from faker import Factory as FakerFactory
+
+import factory
+
+from db.models.repos import CodeReference
+from factories.factory_repos import RepoFactory
+
+fake = FakerFactory.create()
+
+
+class CodeReferenceFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = CodeReference
+
+    repo = factory.SubFactory(RepoFactory)
+    commit = factory.Sequence(lambda i: 'commit{}'.format(i))
