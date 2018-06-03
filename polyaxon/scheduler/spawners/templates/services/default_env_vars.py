f943aa57d6ee462146ff0ab2a091c406d009acce
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-04 01:09:45 +0200

    Add api url to default env vars

--- polyaxon/scheduler/spawners/templates/services/default_env_vars.py
+++ polyaxon/scheduler/spawners/templates/services/default_env_vars.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
-from scheduler.spawners.templates.env_vars import get_from_app_secret
+from libs.api import API_KEY_NAME, get_settings_api_url
+from scheduler.spawners.templates.env_vars import get_env_var, get_from_app_secret
 
 
 def get_service_env_vars():
@@ -8,5 +9,6 @@ def get_service_env_vars():
         get_from_app_secret('POLYAXON_SECRET_KEY', 'polyaxon-secret'),
         get_from_app_secret('POLYAXON_INTERNAL_SECRET_TOKEN', 'polyaxon-internal-secret-token'),
         get_from_app_secret('POLYAXON_RABBITMQ_PASSWORD', 'rabbitmq-password',
-                            settings.POLYAXON_K8S_RABBITMQ_SECRET_NAME)
+                            settings.POLYAXON_K8S_RABBITMQ_SECRET_NAME),
+        get_env_var(name=API_KEY_NAME, value=get_settings_api_url()),
     ]
