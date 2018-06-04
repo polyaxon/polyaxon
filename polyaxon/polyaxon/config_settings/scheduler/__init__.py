517bb590edb65baedc603d8ea64a5b6f5988f076
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-04 18:47:04 +0200

    Add volume claims to scheduler

--- polyaxon/polyaxon/config_settings/scheduler/__init__.py
+++ polyaxon/polyaxon/config_settings/scheduler/__init__.py
@@ -3,4 +3,5 @@ from polyaxon.config_settings.dirs import *
 from polyaxon.config_settings.k8s import *
 from polyaxon.config_settings.spawner import *
 from polyaxon.config_settings.registry import *
+from polyaxon.config_settings.volume_claims import *
 from .apps import *
