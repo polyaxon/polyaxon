79f8394b392a9f5afdf80b446342aaf0f7a3ac05
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-06 23:37:18 +0200

    Refactor build_jobs_notify_done

--- polyaxon/scheduler/tasks/build_jobs.py
+++ polyaxon/scheduler/tasks/build_jobs.py
@@ -37,6 +37,77 @@ def build_jobs_stop(build_job_id, update_status=True):
     dockerizer_scheduler.stop_dockerizer(build_job, update_status=update_status)
 
 
+def notify_build_job_failed(build_job):
+    message = 'Build failed'
+    details = 'build_job_id: {}, {}'.format(build_job.id, build_job.uuid.hex)
+
+    jobs = Job.objects.filter(build_job=build_job)
+    for job in jobs:
+        job.set_status(JobLifeCycle.FAILED, message=message, details=details)
+
+    tensorboard_jobs = TensorboardJob.objects.filter(build_job=build_job)
+    for tensorboard_job in tensorboard_jobs:
+        tensorboard_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
+
+    notebook_jobs = NotebookJob.objects.filter(build_job=build_job)
+    for notebook_job in notebook_jobs:
+        notebook_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
+
+    experiments = Experiment.objects.filter(build_job=build_job)
+    for experiment in experiments:
+        experiment.set_status(ExperimentLifeCycle.FAILED, message=message, details=details)
+
+
+def notify_build_job_stopped(build_job):
+    message = 'Build stopped'
+    details = 'build_job_id: {}, {}'.format(build_job.id, build_job.uuid.hex)
+
+    jobs = Job.objects.filter(build_job=build_job)
+    for job in jobs:
+        job.set_status(JobLifeCycle.STOPPED, message=message, details=details)
+
+    tensorboard_jobs = TensorboardJob.objects.filter(build_job=build_job)
+    for tensorboard_job in tensorboard_jobs:
+        tensorboard_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
+
+    notebook_jobs = NotebookJob.objects.filter(build_job=build_job)
+    for notebook_job in notebook_jobs:
+        notebook_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
+
+    experiments = Experiment.objects.filter(build_job=build_job)
+    for experiment in experiments:
+        experiment.set_status(ExperimentLifeCycle.FAILED, message=message, details=details)
+
+
+def notify_build_job_succeeded(build_job):
+    job_ids = Job.objects.filter(build_job=build_job).values_list('id', flat=True)
+    for job_id in job_ids:
+        celery_app.send_task(
+            SchedulerCeleryTasks.JOBS_START,
+            kwargs={'job_id': job_id})
+
+    tensorboard_job_ids = TensorboardJob.objects.filter(
+        build_job=build_job).values_list('id', flat=True)
+    for tensorboard_job_id in tensorboard_job_ids:
+        celery_app.send_task(
+            SchedulerCeleryTasks.PROJECTS_TENSORBOARD_START,
+            kwargs={'tensorboard_job_id': tensorboard_job_id})
+
+    notebook_job_ids = NotebookJob.objects.filter(
+        build_job=build_job).values_list('id', flat=True)
+    for notebook_job_id in notebook_job_ids:
+        celery_app.send_task(
+            SchedulerCeleryTasks.PROJECTS_NOTEBOOK_START,
+            kwargs={'notebook_job_id': notebook_job_id})
+
+    experiment_ids = Experiment.objects.filter(
+        build_job=build_job).values_list('id', flat=True)
+    for experiment_id in experiment_ids:
+        celery_app.send_task(
+            SchedulerCeleryTasks.EXPERIMENTS_START,
+            kwargs={'experiment_id': experiment_id})
+
+
 @celery_app.task(name=SchedulerCeleryTasks.BUILD_JOBS_NOTIFY_DONE, ignore_result=True)
 def build_jobs_notify_done(build_job_id):
     build_job = get_valid_build_job(build_job_id=build_job_id)
@@ -48,75 +119,14 @@ def build_jobs_notify_done(build_job_id):
     # Notify all dependent jobs, notebooks, tensorboards, and experiments
     # Build job Failed -> Set status Failed with message: build failed
     if build_job.failed:
-        message = 'Build failed'
-        details = 'build_job_id: {}, {}'.format(build_job.id, build_job.uuid.hex)
-
-        jobs = Job.objects.filter(build_job=build_job)
-        for job in jobs:
-            job.set_status(JobLifeCycle.FAILED, message=message, details=details)
-
-        tensorboard_jobs = TensorboardJob.objects.filter(build_job=build_job)
-        for tensorboard_job in tensorboard_jobs:
-            tensorboard_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
-
-        notebook_jobs = NotebookJob.objects.filter(build_job=build_job)
-        for notebook_job in notebook_jobs:
-            notebook_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
-
-        experiments = Experiment.objects.filter(build_job=build_job)
-        for experiment in experiments:
-            experiment.set_status(ExperimentLifeCycle.FAILED, message=message, details=details)
+        notify_build_job_failed(build_job)
         return
 
     # Build job Stopped -> Stop the dependent jobs
     if build_job.stopped:
-        message = 'Build stopped'
-        details = 'build_job_id: {}, {}'.format(build_job.id, build_job.uuid.hex)
-
-        jobs = Job.objects.filter(build_job=build_job)
-        for job in jobs:
-            job.set_status(JobLifeCycle.STOPPED, message=message, details=details)
-
-        tensorboard_jobs = TensorboardJob.objects.filter(build_job=build_job)
-        for tensorboard_job in tensorboard_jobs:
-            tensorboard_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
-
-        notebook_jobs = NotebookJob.objects.filter(build_job=build_job)
-        for notebook_job in notebook_jobs:
-            notebook_job.set_status(JobLifeCycle.FAILED, message=message, details=details)
-
-        experiments = Experiment.objects.filter(build_job=build_job)
-        for experiment in experiments:
-            experiment.set_status(ExperimentLifeCycle.FAILED, message=message, details=details)
+        notify_build_job_stopped(build_job)
         return
 
     # Build job Succeeded -> Start the dependent jobs
     if build_job.succeeded:
-        job_ids = Job.objects.filter(build_job=build_job).values_list('id', flat=True)
-        for job_id in job_ids:
-            celery_app.send_task(
-                SchedulerCeleryTasks.JOBS_START,
-                kwargs={'job_id': job_id})
-
-        tensorboard_job_ids = TensorboardJob.objects.filter(
-            build_job=build_job).values_list('id', flat=True)
-        for tensorboard_job_id in tensorboard_job_ids:
-            celery_app.send_task(
-                SchedulerCeleryTasks.PROJECTS_TENSORBOARD_START,
-                kwargs={'tensorboard_job_id': tensorboard_job_id})
-
-        notebook_job_ids = NotebookJob.objects.filter(
-            build_job=build_job).values_list('id', flat=True)
-        for notebook_job_id in notebook_job_ids:
-            celery_app.send_task(
-                SchedulerCeleryTasks.PROJECTS_NOTEBOOK_START,
-                kwargs={'notebook_job_id': notebook_job_id})
-
-        experiment_ids = Experiment.objects.filter(
-            build_job=build_job).values_list('id', flat=True)
-        for experiment_id in experiment_ids:
-            celery_app.send_task(
-                SchedulerCeleryTasks.EXPERIMENTS_START,
-                kwargs={'experiment_id': experiment_id})
-
-        return
+        notify_build_job_succeeded(build_job)
