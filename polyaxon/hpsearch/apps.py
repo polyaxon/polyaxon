a3e5c60aa123aeabe16da6dbeb14b6daf27f9ef5
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-13 21:53:47 +0200

    Subscribe hpsearch service to experiment and experiment groups signals

--- polyaxon/hpsearch/apps.py
+++ polyaxon/hpsearch/apps.py
@@ -7,3 +7,5 @@ class HPSearchConfig(AppConfig):
 
     def ready(self):
         from hpsearch.signals import new_experiment_group_iteration  # noqa
+        import signals.experiments  # noqa
+        import signals.experiment_groups  # noqa
