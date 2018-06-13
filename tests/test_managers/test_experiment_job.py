41bc62e8cd9e52f443670f326dad84b39d4d9ca1
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-13 18:56:50 +0200

    Add experiment job manager tests

---
+++ tests/test_managers/test_experiment_job.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from unittest import TestCase
+
+from polyaxon_cli.managers.experiment_job import ExperimentJobManager
+from polyaxon_schemas.experiment import ExperimentJobConfig
+
+
+class TestJobManager(TestCase):
+    def test_default_props(self):
+        assert ExperimentJobManager.IS_GLOBAL is False
+        assert ExperimentJobManager.IS_POLYAXON_DIR is True
+        assert ExperimentJobManager.CONFIG_FILE_NAME == '.polyaxonjob'
+        assert ExperimentJobManager.CONFIG == ExperimentJobConfig
