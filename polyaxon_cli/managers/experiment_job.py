75f81ff20dc3953b3b7c2e064105da34dde2edbf
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-13 18:35:46 +0200

    Add experiment job manager

---
+++ polyaxon_cli/managers/experiment_job.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+from polyaxon_cli.managers.base import BaseConfigManager
+from polyaxon_cli.utils.formatting import Printer
+from polyaxon_schemas.experiment import ExperimentJobConfig
+
+
+class ExperimentJobManager(BaseConfigManager):
+    """Manages experiment job configuration .polyaxonjob file."""
+
+    IS_GLOBAL = False
+    IS_POLYAXON_DIR = True
+    CONFIG_FILE_NAME = '.polyaxonxpjob'
+    CONFIG = ExperimentJobConfig
+
+    @classmethod
+    def get_config_or_raise(cls):
+        job = cls.get_config()
+        if not job:
+            Printer.print_error('No job uuid was provided.')
+            sys.exit(1)
+
+        return job
