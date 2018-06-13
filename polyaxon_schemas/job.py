aa82c0df5ef0356aa7920f283a88f4c16e98121b
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-13 13:07:52 +0200

    Add job configuration

---
+++ polyaxon_schemas/job.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from marshmallow import Schema, fields, post_dump, post_load, validate
+
+from polyaxon_schemas.base import BaseConfig
+from polyaxon_schemas.environments import PodResourcesSchema
+from polyaxon_schemas.utils import UUID, humanize_timedelta
+
+
+class JobSchema(Schema):
+    sequence = fields.Int(allow_none=True)
+    uuid = UUID(allow_none=True)
+    unique_name = fields.Str(allow_none=True)
+    user = fields.Str(validate=validate.Regexp(regex=r'^[-a-zA-Z0-9_]+\Z'), allow_none=True)
+    project = UUID(allow_none=True)
+    project_name = fields.Str(allow_none=True)
+    description = fields.Str(allow_none=True)
+    last_status = fields.Str(allow_none=True)
+    created_at = fields.LocalDateTime(allow_none=True)
+    updated_at = fields.LocalDateTime(allow_none=True)
+    started_at = fields.LocalDateTime(allow_none=True)
+    finished_at = fields.LocalDateTime(allow_none=True)
+    total_run = fields.Str(allow_none=True)
+    is_running = fields.Bool(allow_none=True)
+    is_done = fields.Bool(allow_none=True)
+    is_clone = fields.Bool(allow_none=True)
+    config = fields.Dict(allow_none=True)
+    resources = fields.Nested(PodResourcesSchema, allow_none=True)
+    definition = fields.Dict(allow_none=True)
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make(self, data):
+        return JobConfig(**data)
+
+    @post_dump
+    def unmake(self, data):
+        return JobConfig.remove_reduced_attrs(data)
+
+
+class JobConfig(BaseConfig):
+    SCHEMA = JobSchema
+    IDENTIFIER = 'Job'
+    DEFAULT_INCLUDE_ATTRIBUTES = [
+        'sequence', 'unique_name', 'user', 'experiment_group_name', 'last_status', 'definition',
+        'created_at', 'started_at', 'finished_at', 'total_run', 'is_done', 'is_running', 'resources'
+    ]
+    DATETIME_ATTRIBUTES = ['created_at', 'updated_at', 'started_at', 'finished_at']
+
+    def __init__(self,
+                 sequence=None,
+                 user=None,
+                 uuid=None,
+                 unique_name=None,
+                 project=None,
+                 project_name=None,
+                 experiment_group=None,
+                 experiment_group_name=None,
+                 description=None,
+                 last_status=None,
+                 definition=None,
+                 created_at=None,
+                 updated_at=None,
+                 started_at=None,
+                 finished_at=None,
+                 is_clone=None,
+                 is_running=None,
+                 is_done=None,
+                 config=None,
+                 num_jobs=0,
+                 resources=None,
+                 jobs=None,
+                 total_run=None):
+        self.sequence = sequence
+        self.user = user
+        self.uuid = uuid
+        self.unique_name = unique_name
+        self.project = project
+        self.project_name = project_name
+        self.experiment_group = experiment_group
+        self.experiment_group_name = experiment_group_name
+        self.description = description
+        self.last_status = last_status
+        self.definition = definition
+        self.started_at = self.localize_date(started_at)
+        self.finished_at = self.localize_date(finished_at)
+        self.created_at = self.localize_date(created_at)
+        self.updated_at = self.localize_date(updated_at)
+        self.is_running = is_running
+        self.is_done = is_done
+        self.is_clone = is_clone
+        self.config = config  # The json compiled content of this experiment
+        self.num_jobs = num_jobs
+        self.resources = resources
+        self.jobs = jobs
+        self.total_run = None
+        if all([self.started_at, self.finished_at]):
+            self.total_run = humanize_timedelta((self.finished_at - self.started_at).seconds)
+
+
+class JobStatusSchema(Schema):
+    uuid = UUID()
+    job = UUID()
+    created_at = fields.LocalDateTime()
+    status = fields.Str()
+    message = fields.Str(allow_none=True)
+    details = fields.Str(allow_none=True)
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make(self, data):
+        return JobStatusConfig(**data)
+
+    @post_dump
+    def unmake(self, data):
+        return JobStatusConfig.remove_reduced_attrs(data)
+
+
+class JobStatusConfig(BaseConfig):
+    SCHEMA = JobStatusSchema
+    IDENTIFIER = 'ExperimentStatus'
+    DATETIME_ATTRIBUTES = ['created_at']
+    DEFAULT_EXCLUDE_ATTRIBUTES = ['job', 'uuid']
+
+    def __init__(self, uuid, job, created_at, status, message=None, details=None):
+        self.uuid = uuid
+        self.job = job
+        self.created_at = self.localize_date(created_at)
+        self.status = status
+        self.message = message
+        self.details = details
---
+++ tests/test_job.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import uuid
+
+from unittest import TestCase
+
+from polyaxon_schemas.job import JobConfig, JobStatusConfig
+from polyaxon_schemas.utils import local_now
+
+
+class TestJobConfigs(TestCase):
+    def test_job_config(self):
+        config_dict = {
+            'uuid': uuid.uuid4().hex,
+            'project': uuid.uuid4().hex,
+            'project_name': 'name.name',
+            'unique_name': 'user.proj.1',
+            'last_status': 'Running',
+            'description': 'description',
+            'config': {'k': 'v'},
+            'definition': None,
+            'created_at': local_now().isoformat(),
+            'updated_at': local_now().isoformat(),
+            'started_at': local_now().isoformat(),
+            'finished_at': local_now().isoformat(),
+        }
+        config = JobConfig.from_dict(config_dict)
+        config_to_dict = config.to_dict()
+        config_to_dict.pop('is_clone')
+        config_to_dict.pop('is_done')
+        config_to_dict.pop('is_running')
+        config_to_dict.pop('resources')
+        config_to_dict.pop('sequence')
+        config_to_dict.pop('total_run')
+        config_to_dict.pop('user')
+        assert config_to_dict == config_dict
+
+        config_to_dict = config.to_light_dict()
+        config_dict.pop('uuid')
+        config_dict.pop('description')
+        config_dict.pop('config')
+        config_dict.pop('project')
+        config_dict.pop('updated_at')
+        config_dict.pop('project_name')
+        config_to_dict.pop('is_done')
+        config_to_dict.pop('is_running')
+        config_to_dict.pop('sequence')
+        config_to_dict.pop('total_run')
+        config_to_dict.pop('resources')
+        config_to_dict.pop('user')
+        assert config_to_dict == config_dict
+
+        config_to_dict = config.to_light_dict(humanize_values=True)
+        assert config_to_dict.pop('total_run') == '0s'
+        assert config_to_dict.pop('created_at') == 'a few seconds ago'
+        assert config_to_dict.pop('started_at') == 'a few seconds ago'
+        assert config_to_dict.pop('finished_at') == 'a few seconds ago'
+
+    def test_job_status_config(self):
+        config_dict = {'uuid': uuid.uuid4().hex,
+                       'job': uuid.uuid4().hex,
+                       'created_at': local_now().isoformat(),
+                       'status': 'Running',
+                       'message': None,
+                       'details': None}
+        config = JobStatusConfig.from_dict(config_dict)
+        config_to_dict = config.to_dict()
+        assert config_to_dict == config_dict
+
+        config_dict.pop('job', None)
+        config_dict.pop('uuid', None)
+        config_to_dict = config.to_light_dict()
+        assert config_to_dict == config_dict
+
+        config_to_dict = config.to_dict(humanize_values=True)
+        assert config_to_dict.pop('created_at') == 'a few seconds ago'
