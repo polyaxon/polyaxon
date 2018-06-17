9b8ec06a16441457d326f5d966106db5634d3147
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-17 11:56:50 +0200

    Add api filters

---
+++ polyaxon/api/filters.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ImproperlyConfigured
+from rest_framework.filters import BaseFilterBackend, OrderingFilter as BaseOrderingFilter
+
+import query
+
+
+class QueryFilter(BaseFilterBackend):
+    # The URL query parameter used for the ordering.
+    query_param = 'filter'
+    query_manager = None
+
+    def get_query_param(self, view):
+        return getattr(view, 'query_param', self.query_param)
+
+    def get_query_manager(self, view):
+        query_manager = getattr(view, 'query_manager', self.query_manager)
+        if not query_manager:
+            raise ImproperlyConfigured('QueryFilter requires a query_manger to be set on the view.')
+        return query_manager
+
+    def filter_queryset(self, request, queryset, view):
+        query_param = self.get_query_param(view=view)
+        query_manager = self.get_query_manager(view=view)
+        query_spec = request.query_params.get(query_param)
+        if query_spec:
+            queryset = query.filter_queryset(manager=query_manager,
+                                             query_spec=query_spec,
+                                             queryset=queryset)
+        return queryset
+
+
+class OrderingFilter(BaseOrderingFilter):
+    ordering_param = 'sort'
