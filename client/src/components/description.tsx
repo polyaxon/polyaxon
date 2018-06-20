714be86f645d43d68457886b4b1602bf81496869
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-20 17:09:41 +0200

    Add description component

---
+++ client/src/components/description.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import './description.less';
+
+export interface Props {
+  description?: string;
+  command: string;
+  entity: string;
+}
+
+function Description({description, entity, command}: Props) {
+  function getDescription() {
+    if (description) {
+      return description;
+    }
+    return (
+      <div>
+        <p>No description!</p>
+
+        <p>You can add a description to this {entity} by using CLI: <b> {command} </b></p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="description">
+      {getDescription()}
+    </div>
+  );
+}
+
+export default Description;
