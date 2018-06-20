f1c3d2ff11893c2b42c6b39c89ee8d198a2f0248
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-20 16:54:14 +0200

    Add linked tab component

---
+++ client/src/components/linkedTab.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { Tab, Nav, NavItem, Col, Row } from 'react-bootstrap';
+
+import './linkedTab.less';
+
+export interface Props {
+  baseUrl: string;
+  tabs: Array<{ title: string, component: React.ReactNode, relUrl: string }>;
+}
+
+function LinkedTab({baseUrl, tabs}: Props) {
+  let getActiveTab = () => {
+    let pieces = location.hash.split('?')[0].split('#');
+    if (pieces.length === 1) {
+      return 1;
+    }
+    let activeTab: string = pieces[1];
+    let eventKey = 1;
+    for (let tab of tabs) {
+      if (tab.relUrl === activeTab) {
+        break;
+      }
+      eventKey++;
+    }
+    return eventKey;
+  };
+
+  return (
+    <Tab.Container defaultActiveKey={getActiveTab()} id="project-tabs" className="plx-nav">
+      <Row className="clearfix">
+        <Col sm={12}>
+          <Nav bsStyle="tabs">
+            {tabs.map((tab, idx) =>
+              <NavItem eventKey={idx + 1} href={`${baseUrl}#${tab.relUrl}`} key={idx}>{tab.title}</NavItem>
+            )}
+          </Nav>
+        </Col>
+        <Col sm={12}>
+          <Tab.Content animation={true} mountOnEnter={true}>
+            {tabs.map((tab, idx) =>
+              <Tab.Pane eventKey={idx + 1} key={idx}>
+                {tab.component}
+              </Tab.Pane>
+            )}
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+  );
+}
+
+export default LinkedTab;
