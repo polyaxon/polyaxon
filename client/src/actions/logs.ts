b387cf0eb4a033ddbd6f1fe3e56c52120e2695a1
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-22 21:12:47 +0200

    Update logs component to support multiple resources

--- client/src/actions/logs.ts
+++ client/src/actions/logs.ts
@@ -35,12 +35,12 @@ export type LogsAction =
   RequestLogsAction
   | ReceiveLogsAction;
 
-export function fetchLogs(projectUniqueName: string, experimentId: number): any {
+export function fetchLogs(projectUniqueName: string, resources: string, id: number): any {
   return (dispatch: any, getState: any) => {
     dispatch(requestLogsActionCreator());
 
     let logsUrl =
-      BASE_API_URL + `/${urlifyProjectName(projectUniqueName)}` + '/experiments/' + experimentId + '/logs';
+      BASE_API_URL + `/${urlifyProjectName(projectUniqueName)}/${resources}/${id}/logs`;
 
     return fetch(logsUrl, {
       headers: {
