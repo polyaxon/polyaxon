fe03038ee69c7716a2b99f531365f8c5963887e2
mouradmourafiq <mouradmourafiq@gmail.com>
2018-06-23 15:01:21 +0200

    Add gridList component

---
+++ client/src/components/gridList.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import * as ReactDataGrid from 'react-data-grid';
+
+import './gridList.less';
+
+export interface Props {
+  columns?: { key: string; name: string; }[];
+  rows: { [key: string]: any; }[];
+}
+
+function GridList({columns, rows}: Props) {
+  if (!columns) {
+    columns = [];
+    for (let row of rows) {
+      let rowColumns = Object.keys(row).map(v => { return {key: v, name: v}});
+      columns.push(...rowColumns);
+    }
+  }
+  return (
+    <div className="grid-list">
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={i => rows[i]}
+        rowsCount={rows.length}
+        minHeight={35 * rows.length + 35}
+      />
+    </div>
+  );
+}
+
+export default GridList;
