55b5c0cce75fa643dfa435f8f8f2c9fb9968c0a5
gzc <zhecongguo@linkdoc.com>
2018-06-26 22:29:03 +0800

    Format code

--- polyaxon/scheduler/spawners/templates/env_vars.py
+++ polyaxon/scheduler/spawners/templates/env_vars.py
@@ -60,8 +60,8 @@ def get_resources_env_vars(resources):
     # Fix https://github.com/kubernetes/kubernetes/issues/59629
     # When resources.gpu.limits is not set or set to 0, we explicitly
     # pass NVIDIA_VISIBLE_DEVICES=none into container to avoid exposing GPUs.
-    if not resources or not resources.gpu or \
-        not resources.gpu.limits or resources.gpu.limits == '0':
+    if (not resources or not resources.gpu or
+            not resources.gpu.limits or resources.gpu.limits == '0'):
         env_vars.append(
             client.V1EnvVar(
                 name='NVIDIA_VISIBLE_DEVICES',
