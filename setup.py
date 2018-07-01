1f6c3278ba444c09db39565d9ef171920512645e
mouradmourafiq <mouradmourafiq@gmail.com>
2018-07-01 20:59:04 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon',
-      version='0.1.0',
+      version='0.1.1',
       description='Deep Learning library for TensorFlow for '
                   'building end to end db and experiments.',
       maintainer='Mourad Mourafiq',
