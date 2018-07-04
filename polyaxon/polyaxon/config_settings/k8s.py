b5cc16f60800ef836956f0096d2258e2711f7df5
mouradmourafiq <mouradmourafiq@gmail.com>
2018-07-04 13:41:30 +0200

    Update config settings

--- polyaxon/polyaxon/config_settings/k8s.py
+++ polyaxon/polyaxon/config_settings/k8s.py
@@ -1,7 +1,3 @@
-import urllib3
-
-from kubernetes import client
-
 from polyaxon.config_manager import config
 
 POLYAXON_K8S_APP_NAME = config.get_string('POLYAXON_K8S_APP_NAME')
@@ -10,7 +6,6 @@ POLYAXON_K8S_API_PORT = config.get_int('POLYAXON_K8S_API_PORT')
 POLYAXON_K8S_APP_CONFIG_NAME = config.get_string('POLYAXON_K8S_APP_CONFIG_NAME')
 POLYAXON_K8S_APP_SECRET_NAME = config.get_string('POLYAXON_K8S_APP_SECRET_NAME')
 POLYAXON_K8S_RABBITMQ_SECRET_NAME = config.get_string('POLYAXON_K8S_RABBITMQ_SECRET_NAME')
-CLUSTER_ID = config.get_string('POLYAXON_CLUSTER_ID', is_optional=True)
 
 K8S_AUTHORISATION = config.get_string('POLYAXON_K8S_AUTHORISATION',
                                       is_optional=True,
@@ -20,14 +15,23 @@ SSL_CA_CERT = config.get_string('POLYAXON_K8S_SSL_CA_CERT', is_optional=True)
 
 K8S_CONFIG = None
 if K8S_AUTHORISATION and K8S_HOST:
-    K8S_CONFIG = client.Configuration()
-    K8S_CONFIG.api_key['authorization'] = K8S_AUTHORISATION
-    K8S_CONFIG.api_key_prefix['authorization'] = 'Bearer'
-    K8S_CONFIG.host = K8S_HOST
+    def set_config():
+        import urllib3
 
-    if SSL_CA_CERT:
-        K8S_CONFIG.verify_ssl = True
-        K8S_CONFIG.ssl_ca_cert = SSL_CA_CERT
-    else:
-        K8S_CONFIG.verify_ssl = False
-        urllib3.disable_warnings()
+        from kubernetes import client
+
+        K8S_CONFIG = client.Configuration()
+        K8S_CONFIG.api_key['authorization'] = K8S_AUTHORISATION
+        K8S_CONFIG.api_key_prefix['authorization'] = 'Bearer'
+        K8S_CONFIG.host = K8S_HOST
+
+        if SSL_CA_CERT:
+            K8S_CONFIG.verify_ssl = True
+            K8S_CONFIG.ssl_ca_cert = SSL_CA_CERT
+        else:
+            K8S_CONFIG.verify_ssl = False
+            urllib3.disable_warnings()
+
+
+    set_config()
+
--- polyaxon/polyaxon/config_settings/monitor_resources/__init__.py
+++ polyaxon/polyaxon/config_settings/monitor_resources/__init__.py
@@ -1,3 +1,2 @@
-from polyaxon.config_settings.k8s import *
 from polyaxon.config_settings.spawner import *
 from .apps import *
