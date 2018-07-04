1a74003816a7fb30c88595ba55b366b9f45ca0e5
mouradmourafiq <mouradmourafiq@gmail.com>
2018-07-04 13:56:06 +0200

    Update k8s settings

--- polyaxon/polyaxon/config_settings/k8s.py
+++ polyaxon/polyaxon/config_settings/k8s.py
@@ -15,23 +15,18 @@ SSL_CA_CERT = config.get_string('POLYAXON_K8S_SSL_CA_CERT', is_optional=True)
 
 K8S_CONFIG = None
 if K8S_AUTHORISATION and K8S_HOST:
-    def set_config():
-        import urllib3
+    import urllib3
 
-        from kubernetes import client
+    from kubernetes import client
 
-        K8S_CONFIG = client.Configuration()
-        K8S_CONFIG.api_key['authorization'] = K8S_AUTHORISATION
-        K8S_CONFIG.api_key_prefix['authorization'] = 'Bearer'
-        K8S_CONFIG.host = K8S_HOST
+    K8S_CONFIG = client.Configuration()
+    K8S_CONFIG.api_key['authorization'] = K8S_AUTHORISATION
+    K8S_CONFIG.api_key_prefix['authorization'] = 'Bearer'
+    K8S_CONFIG.host = K8S_HOST
 
-        if SSL_CA_CERT:
-            K8S_CONFIG.verify_ssl = True
-            K8S_CONFIG.ssl_ca_cert = SSL_CA_CERT
-        else:
-            K8S_CONFIG.verify_ssl = False
-            urllib3.disable_warnings()
-
-
-    set_config()
-
+    if SSL_CA_CERT:
+        K8S_CONFIG.verify_ssl = True
+        K8S_CONFIG.ssl_ca_cert = SSL_CA_CERT
+    else:
+        K8S_CONFIG.verify_ssl = False
+        urllib3.disable_warnings()
