1ab298215b4f63ef45ce12cc356f5def92f33eb4
mouradmourafiq <mouradmourafiq@gmail.com>
2018-07-10 15:40:24 +0200

    Update kube commands

--- polyaxon/check.sh
+++ polyaxon/check.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while [ ! "$(python3 ./manage.py showmigrations --plan | grep '\[ \]\|^[a-z]' | grep '[  ]' -B 1)" ]; do echo "Waiting for migrations..."; sleep 60; done;
+while [ "$(python3 ./manage.py showmigrations --plan | grep '\[ \]\|^[a-z]' | grep '[  ]' -B 1)" ]; do echo "Waiting for migrations..."; sleep 60; done;
