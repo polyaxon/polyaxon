781c182ce0fa7c9c66233bd07e317c1823c8953e
mouradmourafiq <mouradmourafiq@gmail.com>
2018-07-10 20:05:27 +0200

    Add bookmark model

---
+++ polyaxon/db/models/bookmarks.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class Bookmark(models.Model):
+    """The Bookmark model represents an instance of object that user bookmarked."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+')
+    created_at = models.DateTimeField()
+    enabled = models.BooleanField(default=True)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+        app_label = 'db'
+        verbose_name = 'bookmark'
+        verbose_name_plural = 'bookmarks'
+
+    def __str__(self):
+        return '{} - <{}-{}>'.format(self.user, self.content_type, self.created_at)
