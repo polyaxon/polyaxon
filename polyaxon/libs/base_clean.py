d4099c517af7557e26ca64bf2433b0b75ffb6291
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-02 22:12:19 +0200

    Debug Commands

--- polyaxon/libs/base_clean.py
+++ polyaxon/libs/base_clean.py
@@ -10,5 +10,7 @@ class BaseCleanCommand(BaseCommand):
     def handle(self, *args, **options):
         try:
             self._clean()
+            import time;
+            time.sleep(180)
         except DatabaseError:
             pass
