cba38fcdb8db5a5c823c091bc8384ce782515f2f
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-07 22:37:56 +0200

    Update project structure

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='demeter',
+      version='0.1.7',
+      description='An abstraction to make seamless storage integrations '
+                  'with variety of storage backends.',
+      maintainer='Mourad Mourafiq',
+      maintainer_email='mouradmourafiq@gmail.com',
+      author='Mourad Mourafiq',
+      author_email='mouradmourafiq@gmail.com',
+      url='https://github.com/polyaxon/demeter',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'aws',
+          's3',
+          'microsoft',
+          'azure',
+          'google cloud storage',
+          'gcs',
+          'tensorFlow',
+          'deep-learning',
+          'machine-learning',
+          'data-science',
+          'neural-networks',
+          'artificial-intelligence',
+          'ai',
+          'reinforcement-learning',
+          'kubernetes',
+      ],
+      install_requires=[
+          "azure-storage==0.36.0",
+          "boto3==1.7.70",
+          "google-cloud-storage==1.10.0",
+          "six==1.11.0",
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest})
