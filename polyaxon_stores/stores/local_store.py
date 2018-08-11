098739c5eec034ddb1985069c58c2b2172d40d9d
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-11 13:05:17 +0200

    Update local store

--- polyaxon_stores/stores/local_store.py
+++ polyaxon_stores/stores/local_store.py
@@ -8,5 +8,20 @@ from polyaxon_stores.stores.base_store import Store
 class LocalStore(Store):
     """
     Local filesystem store.
+
+    This store is noop store since all data is accessible through the filesystem.
     """
     STORE_TYPE = Store._LOCAL_STORE  # pylint:disable=protected-access
+
+    def download_file(self, *args, **kwargs):
+        pass
+
+    def upload_file(self, *args, **kwargs):
+        pass
+
+    def upload_files(self, dirname, path=None):
+        pass
+
+    def download_files(self, *args, **kwargs):
+        pass
+
