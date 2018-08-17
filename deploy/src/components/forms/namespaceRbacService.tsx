0c6ac08402b2af94e8d7ce3acc1129778b9b55bb
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-17 14:49:29 +0200

    Add rbac, namespace, and service type form section

---
+++ deploy/src/components/forms/namespaceRbacService.tsx
@@ -0,0 +1,171 @@
+import * as React from 'react';
+import * as _ from 'lodash';
+
+import { SERVICE_TYPES, IngressInterface, ConfigInterface, ResourcesInterface } from '../../interfaces/config';
+import Resources from './resources';
+import PreviewForm from './previewForm';
+
+export interface Props {
+  config: ConfigInterface;
+  defaultConfig: ConfigInterface;
+  updateConfig: (config: ConfigInterface) => void;
+}
+
+interface State {
+  config: ConfigInterface;
+}
+
+export default class NamespaceRbacService extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      config: this.props.config,
+    };
+  }
+
+  public updateNameSpace = (value: string) => {
+    const config = _.cloneDeep(this.state.config);
+    config.namespace = value;
+    this.setState({config});
+  };
+
+  public updateRbac = (value: boolean) => {
+    const config = _.cloneDeep(this.state.config);
+    config.rbac = {enabled: value};
+    this.setState({config});
+  };
+
+  public updateServiceType = (value: SERVICE_TYPES) => {
+    const config = _.cloneDeep(this.state.config);
+    if (config.ingress && config.ingress.enabled) {
+      config.serviceType = 'ClusterIP';
+    } else {
+      config.serviceType = value;
+    }
+    this.setState({config});
+  };
+
+  public updateResources = (resources: ResourcesInterface) => {
+    const config = _.cloneDeep(this.state.config);
+    if (_.isNil(config.ingress)) {
+      config.ingress = {resources} as IngressInterface;
+    } else {
+      config.ingress.resources = resources;
+    }
+    this.setState({config});
+  };
+
+  public updateIngress = (value: boolean) => {
+    const config = _.cloneDeep(this.state.config);
+    if (_.isNil(config.ingress)) {
+      config.ingress = {enabled: value} as IngressInterface;
+    } else {
+      config.ingress.enabled = value;
+    }
+    if (value) {
+      config.serviceType = 'ClusterIP';
+    } else {
+      config.serviceType = 'LoadBalancer';
+    }
+    this.setState({config});
+  };
+
+  public componentDidUpdate(prevProps: Props, prevState: State) {
+    if (!_.isEqual(this.state.config, prevState.config)) {
+      this.props.updateConfig(this.state.config);
+    }
+  }
+
+  public render() {
+    const currentConfig = () => {
+      const config: ConfigInterface = {};
+      if (!_.isNil(this.state.config.namespace)) {
+        config.namespace = this.state.config.namespace;
+      }
+      if (!_.isNil(this.state.config.rbac)) {
+        config.rbac = this.state.config.rbac;
+      }
+      if (!_.isNil(this.state.config.ingress)) {
+        config.ingress = this.state.config.ingress;
+      }
+      if (!_.isNil(this.state.config.serviceType)) {
+        config.serviceType = this.state.config.serviceType;
+      }
+      return config;
+    };
+
+    const defaultConfig = {
+      namespace: this.props.defaultConfig.namespace,
+      rbac: this.props.defaultConfig.rbac,
+      ingress: this.props.defaultConfig.ingress,
+      serviceType: this.props.defaultConfig.serviceType
+    };
+
+    return (
+      <div className="columns">
+        <div className="column is-7">
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">Namespace</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    value={this.state.config.namespace || ''}
+                    onChange={(event) =>  this.updateNameSpace(event.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">RBAC</label>
+            </div>
+            <div className="field-body">
+              <input
+                type="checkbox"
+                onChange={(event) => this.updateRbac(event.target.checked)}
+              />
+            </div>
+          </div>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">Ingress</label>
+            </div>
+            <div className="field-body">
+              <input
+                type="checkbox"
+                onChange={(event) => this.updateIngress(event.target.checked)}
+              />
+            </div>
+          </div>
+          {this.state.config.ingress && this.state.config.ingress.enabled &&
+            <Resources config={this.state.config.ingress.resources} updateConfig={this.updateResources}/>
+          }
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">Service type</label>
+            </div>
+            <div className="field-body">
+              <div className="select">
+                <select
+                  onChange={(event) => this.updateServiceType(event.target.value as SERVICE_TYPES)}
+                  value={this.state.config.serviceType || 'ClusterIp'}
+                >
+                  <option value="ClusterIp">ClusterIp</option>
+                  <option value="LoadBalancer">LoadBalancer</option>
+                  <option value="NodePort">NodePort</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <PreviewForm currentConfig={currentConfig()} defaultConfig={defaultConfig} />
+      </div>
+    );
+  }
+}
---
+++ deploy/src/components/forms/previewForm.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import * as jsYaml from 'js-yaml';
+
+export interface Props {
+  currentConfig: { [key: string]: any; };
+  defaultConfig: { [key: string]: any; };
+}
+
+export default class PreviewForm extends React.Component<Props, Object> {
+  public render() {
+    return (
+      <div className="column is-5 preview">
+        <div className="columns">
+          <div className="column">
+            <div className="content">
+              <h4> Preview Override config </h4>
+              <p>
+                {jsYaml.dump(this.props.currentConfig)}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column">
+            <div className="content">
+              <h4> Default config Section </h4>
+              <p>
+                {jsYaml.dump(this.props.defaultConfig)}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
