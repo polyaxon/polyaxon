067df99921a84e3057f2b65613353e71ee076469
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-18 23:10:55 +0200

    Update debug mode

--- polyaxon/polyaxon/config_settings/debug.py
+++ polyaxon/polyaxon/config_settings/debug.py
@@ -13,11 +13,19 @@ if config.is_debug_mode and config.is_monolith_service and not config.is_testing
     }
 
     DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
+        # 'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        # 'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        # 'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        # 'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        # 'debug_toolbar.panels.signals.SignalsPanel',
+        # 'debug_toolbar.panels.logging.LoggingPanel',
+        # 'debug_toolbar.panels.redirects.RedirectsPanel',
+        # 'debug_toolbar.panels.profiling.ProfilingPanel'
     )
 
     MIDDLEWARE += (
