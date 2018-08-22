f79f97f85962c3886e3c7276056ceccd94cfa02d
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-22 16:05:57 +0200

    Add nsenter

--- ci/test.sh
+++ ci/test.sh
@@ -3,8 +3,10 @@
 set -eux
 
 IP=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
-TEST_NAMESPACE=polyaxon-test
+TEST_NAMESPACE=polyaxon
 TEST_URL=http://$IP:31811
+
+kubectl create namespace $TEST_NAMESPACE
 
 helm install --name polyaxon-test --namespace $TEST_NAMESPACE ./polyaxon/ -f ./ci/test-config.yml
 
