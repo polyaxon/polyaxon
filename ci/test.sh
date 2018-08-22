55c2331bfb41bc6b537325ea69fc1aa3da763ee4
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-22 15:40:28 +0200

    Update travis

--- ci/test.sh
+++ ci/test.sh
@@ -6,7 +6,7 @@ IP=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 TEST_NAMESPACE=polyaxon-test
 TEST_URL=http://$IP:31811
 
-helm install --name polyaxon-test --namespace $TEST_NAMESPACE ./polyaxon/ -f ./ci/test-config.yaml
+helm install --name polyaxon-test --namespace $TEST_NAMESPACE ./polyaxon/ -f ./ci/test-config.yml
 
 echo "waiting for servers to become responsive"
 until curl --fail -s $TEST_URL/api/v1/versions/cli/; do
