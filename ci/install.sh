37a1cd16b445d06ed973ee8f721a72865fc408c8
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-23 14:36:38 +0200

    Add new type label for hooks

--- ci/install.sh
+++ ci/install.sh
@@ -14,11 +14,13 @@ fi
 echo "installing kubectl"
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubectl
 chmod +x kubectl
+sudo cp kubectl /usr/local/bin/
 mv kubectl bin/
 
 echo "installing minikube"
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64
 chmod +x minikube
+sudo cp minikube /usr/local/bin/
 mv minikube bin/
 
 
--- ci/test.sh
+++ ci/test.sh
@@ -8,11 +8,11 @@ TEST_URL=http://$IP:31811
 
 kubectl create namespace $TEST_NAMESPACE
 
-helm install --name polyaxon-test --namespace $TEST_NAMESPACE ./polyaxon/ -f ./ci/test-config.yml &
+helm install --name polyaxon-test --namespace $TEST_NAMESPACE ./polyaxon/ -f ./ci/test-config.yml
 
 echo "waiting for servers to become responsive"
 until curl --fail -s $TEST_URL/api/v1/versions/cli/; do
-    kubectl --namespace=$TEST_NAMESPACE logs -l type=polyaxon-core
+    kubectl --namespace=$TEST_NAMESPACE get pod
     sleep 10
 done
 
