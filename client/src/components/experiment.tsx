1bd4ca2783ae2c309ccaa8dc1ace7c1fc3312f55
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-24 19:11:32 +0200

    Turn experiment into a paginated table

--- client/src/components/experiment.tsx
+++ client/src/components/experiment.tsx
@@ -42,11 +42,11 @@ function Experiment({experiment, onDelete, onStop}: Props) {
     buildUrl = getBuildUrl(buildValues[0], buildValues[1], buildValues[3]);
   }
   return (
-    <div className="row">
-      <div className="col-md-1 block">
+    <tr className="list-item">
+      <td className="block">
         <Status status={experiment.last_status}/>
-      </div>
-      <div className="col-md-6 block">
+      </td>
+      <td className="block">
         <LinkContainer to={getExperimentUrl(values[0], values[1], experiment.id)}>
           <a className="title">
             <i className="fa fa-cube icon" aria-hidden="true"/>
@@ -63,8 +63,8 @@ function Experiment({experiment, onDelete, onStop}: Props) {
           />
         </div>
         <Tags tags={experiment.tags}/>
-      </div>
-      <div className="col-md-2 block">
+      </td>
+      <td className="block">
         <GroupLinkMetaInfo
           value={groupValues[2]}
           link={groupUrl}
@@ -73,18 +73,18 @@ function Experiment({experiment, onDelete, onStop}: Props) {
           value={buildValues[3]}
           link={buildUrl}
         />
-      </div>
-      <div className="col-md-2 block">
+      </td>
+      <td className="block">
         <TaskRunMetaInfo startedAt={experiment.started_at} finishedAt={experiment.finished_at}/>
-      </div>
-      <div className="col-md-1 block">
+      </td>
+      <td className="block">
         <Actions
           onDelete={onDelete}
           onStop={onStop}
           isRunning={!isDone(experiment.last_status)}
         />
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 }
 
--- client/src/components/experiments.tsx
+++ client/src/components/experiments.tsx
@@ -1,19 +1,13 @@
-import * as _ from 'lodash';
 import * as React from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-
-import * as queryString from 'query-string';
 
 import * as actions from '../actions/experiment';
-import { getExperimentUrl, splitUniqueName } from '../constants/utils';
 import { ExperimentModel } from '../models/experiment';
 import { EmptyBookmarks } from './empty/emptyBookmarks';
 import { EmptyList } from './empty/emptyList';
 import Experiment from './experiment';
 import ExperimentHeader from './experimentHeader';
 import { EXPERIMENT_FILTERS } from './filters/constants';
-import GridList from './gridList';
-import PaginatedList from './paginatedList';
+import PaginatedTable from './paginatedTable';
 
 export interface Props {
   isCurrentUser: boolean;
@@ -35,86 +29,22 @@ export default class Experiments extends React.Component<Props, Object> {
   }
 
   public render() {
-    const getExperimentLink = (experimentName: string, experimentId: string | number) => {
-      const values = splitUniqueName(experimentName);
-      return(
-        <LinkContainer to={getExperimentUrl(values[0], values[1], experimentId)}>
-          <a className="title">
-            <i className="fa fa-cube icon" aria-hidden="true"/>
-            {experimentName}
-          </a>
-        </LinkContainer>
-      );
-    };
-
     const filters = this.props.useFilters ? EXPERIMENT_FILTERS : false;
     const experiments = this.props.experiments;
     const listExperiments = () => {
       return (
-        <ul>
-          {experiments.filter(
-            (xp: ExperimentModel) => _.isNil(xp.deleted) || !xp.deleted
-          ).map(
+          <tbody>
+          {ExperimentHeader()}
+          {experiments.map(
             (xp: ExperimentModel) =>
-              <li className="list-item" key={xp.unique_name}>
-                <Experiment
-                  experiment={xp}
-                  onDelete={() => this.props.onDelete(xp.unique_name)}
-                  onStop={() => this.props.onStop(xp.unique_name)}
-                />
-              </li>)}
-        </ul>
+              <Experiment
+                key={xp.unique_name}
+                experiment={xp}
+                onDelete={() => this.props.onDelete(xp.unique_name)}
+                onStop={() => this.props.onStop(xp.unique_name)}
+              />)}
+          </tbody>
       );
-    };
-    const listExperimentMetrics = () => {
-      return (
-        <GridList
-          rows={experiments
-            .filter((xp: ExperimentModel) => xp.last_metric)
-            .map((xp: ExperimentModel) => (
-              {
-                ...{experiment: getExperimentLink(xp.unique_name, xp.id)},
-                ...xp.last_metric
-              }))}
-        />
-      );
-    };
-
-    const listExperimentDeclarations = () => {
-      return (
-        <GridList
-          rows={experiments
-            .filter((xp: ExperimentModel) => xp.declarations)
-            .map((xp: ExperimentModel) => (
-              {
-                ...{experiment: getExperimentLink(xp.unique_name, xp.id)},
-                ...xp.declarations
-              }))}
-        />
-      );
-    };
-
-    const getListType = () => {
-      const pieces = location.href.split('?');
-      if (pieces.length > 1) {
-        const search = queryString.parse(pieces[1]);
-        if (search.metrics === true || search.metrics === 'true') {
-          return 'metrics';
-        } else if (search.declarations === true || search.declarations === 'true') {
-          return 'declarations';
-        }
-      }
-      return 'info';
-    };
-    const listType = getListType();
-
-    const getList = () => {
-      if (listType === 'metrics') {
-        return listExperimentMetrics();
-      } else if (listType === 'declarations') {
-        return listExperimentDeclarations();
-      }
-      return listExperiments();
     };
 
     const empty = this.props.bookmarks ?
@@ -128,10 +58,9 @@ export default class Experiments extends React.Component<Props, Object> {
         'experiment',
         'polyaxon run --help');
     return (
-      <PaginatedList
+      <PaginatedTable
         count={this.props.count}
-        componentList={getList()}
-        componentHeader={listType === 'info' ? ExperimentHeader() : null}
+        componentList={listExperiments()}
         componentEmpty={empty}
         filters={filters}
         fetchData={this.props.fetchData}
