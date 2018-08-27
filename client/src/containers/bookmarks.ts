907c25e41518a78f5b8bf0f12f21eca17635ee53
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-27 17:09:27 +0200

    Lint

--- client/src/containers/bookmarks.ts
+++ client/src/containers/bookmarks.ts
@@ -1,8 +1,8 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-import { AppState } from '../constants/types';
 import Bookmarks from '../components/bookmarks';
+import { AppState } from '../constants/types';
 
 export function mapStateToProps(state: AppState, params: any) {
   return {user: params.match.params.user};
