534768c26257f93468c0c8fc291cce3993e9ffc9
mouradmourafiq <mouradmourafiq@gmail.com>
2018-08-28 16:56:33 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-cli',
-      version='0.1.9',
+      version='0.2.0',
       description='Command Line Interface (CLI) for Polyaxon.',
       maintainer='Mourad Mourafiq',
       maintainer_email='mouradmourafiq@gmail.com',
@@ -44,7 +44,7 @@ setup(name='polyaxon-cli',
       install_requires=[
           "click==6.7",
           "pathlib==1.0.1",
-          "polyaxon-client==0.1.9",
+          "polyaxon-client==0.2.0",
           "raven==6.7.0",
           "tabulate==0.8.2",
           "six==1.11.0",
