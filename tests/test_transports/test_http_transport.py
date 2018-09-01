f5641ca366747bc1d9dc50c4256b9958e5176559
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-01 22:28:37 +0200

    Fix http transport tests

--- tests/test_transports/test_http_transport.py
+++ tests/test_transports/test_http_transport.py
@@ -22,6 +22,6 @@ class TestHttpTransport(TestCase):
     def test_format_sizeof(self):
         assert self.transport.format_sizeof(10) == '10.0B'
         assert self.transport.format_sizeof(10000) == '9.8KiB'
-        assert self.transport.format_sizeof(10000) == '97.7KiB'
+        assert self.transport.format_sizeof(100000) == '97.7KiB'
         assert self.transport.format_sizeof(10000000) == '9.5MiB'
         assert self.transport.format_sizeof(10000000000) == '9.3GiB'
