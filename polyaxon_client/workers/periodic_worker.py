3eedb3706be6408cc9f14306b7d36d1624165be6
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-02 12:50:37 +0200

    Add thread workers

---
+++ polyaxon_client/workers/periodic_worker.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import threading
+
+from polyaxon_client.workers.base_worker import BaseWorker
+
+
+class PeriodicWorker(BaseWorker):
+    NAME = 'polyaxon.PeriodicWorker'
+
+    def __init__(self, interval, function, *args, **kwargs):
+        super(PeriodicWorker, self).__init__()
+        self._interval = interval
+        self._finished = threading.Event()
+        self._function = function
+        self._args = args
+        self._kwargs = kwargs
+
+    def stop(self):
+        with self._lock:
+            if self._thread:
+                self._finished.set()
+                self._thread.join()
+                self._thread = None
+                self._thread_for_pid = None
+
+    def atexist(self):
+        self.stop()
+
+    def _target(self):
+        while True:
+            if not self.is_alive() or self._finished.isSet():
+                break
+            self._finished.wait(self._interval)
+            self._function(*self._args, **self._kwargs)
---
+++ polyaxon_client/workers/queue_worker.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from time import sleep, time
+
+import os
+from six.moves.queue import Queue
+
+from polyaxon_client.logger import logger
+from polyaxon_client.workers.base_worker import BaseWorker
+
+
+class QueueWorker(BaseWorker):
+    TIMEOUT = 10
+    MIN_TIMEOUT = 0.1
+    QUEUE_SIZE = -1  # inf
+    END_EVENT = object()
+    NAME = 'polyaxon.QueueWorker'
+
+    def __init__(self, timeout=None, queue_size=None):
+        super(QueueWorker, self).__init__()
+        self._queue = Queue(queue_size or self.QUEUE_SIZE)
+        self._timeout = timeout or self.TIMEOUT
+
+    def atexist(self):
+        with self._lock:
+            if not self.is_alive():
+                return
+
+            self._queue.put_nowait(self.END_EVENT)
+            # ensure wait
+            initial_timeout = min(self.MIN_TIMEOUT, self._timeout)
+
+            def timeout_join(timeout, queue):
+                end = time() + timeout
+                queue.all_tasks_done.acquire()
+                try:
+                    while queue.unfinished_tasks:
+                        current_timeout = end - time()
+                        if current_timeout <= 0:
+                            # timed out
+                            return False
+
+                        queue.all_tasks_done.wait(timeout=current_timeout)
+
+                    return True
+
+                finally:
+                    queue.all_tasks_done.release()
+
+            if not timeout_join(timeout=initial_timeout, queue=self._queue):
+                # if that didn't work, wait a bit longer
+                # NB that size is an approximation, because other threads may
+                # add or remove items
+                size = self._queue.qsize()
+
+                print("Polyaxon worker is attempting to send %i pending messages" % size)
+                print("Waiting up to {} seconds".format(self._timeout))
+                if os.name == 'nt':
+                    print("Press Ctrl-Break to quit")
+                else:
+                    print("Press Ctrl-C to quit")
+
+                timeout_join(timeout=self._timeout - initial_timeout, queue=self._queue)
+
+            self._thread = None
+
+    def stop(self, timeout=None):
+        with self._lock:
+            if self._thread:
+                self._queue.put_nowait(self.END_EVENT)
+                self._thread.join(timeout=timeout)
+                self._thread = None
+                self._thread_for_pid = None
+
+    def queue(self, callback, *args, **kwargs):
+        self.is_running()
+        self._queue.put_nowait((callback, args, kwargs))
+
+    def _target(self):
+        while True:
+            record = self._queue.get()
+            try:
+                if record is self.END_EVENT:
+                    break
+                callback, args, kwargs = record
+                try:
+                    callback(*args, **kwargs)
+                except Exception:
+                    logger.error('Failed processing job', exc_info=True)
+            finally:
+                self._queue.task_done()
+
+            sleep(0)
