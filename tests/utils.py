365121d75df1f383f12523aef72af70b0f16a5c5
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-03 16:29:06 +0200

    Add env vars utils tests

---
+++ tests/utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import json
+from unittest import TestCase
+
+import os
+
+
+class TestEnvVarsCase(TestCase):
+    @staticmethod
+    def check_empty_value(key, expected_function):
+        os.environ.pop(key, None)
+        assert expected_function() is None
+
+    @staticmethod
+    def check_non_dict_value(key, expected_function, value='non dict random value'):
+        os.environ[key] = value
+        assert expected_function() is None
+
+    @staticmethod
+    def check_valid_dict_value(key, expected_function, value):
+        os.environ[key] = json.dumps(value)
+        assert expected_function() == value
+
+    @staticmethod
+    def check_valid_value(key, expected_function, value):
+        os.environ[key] = value
+        assert expected_function() == value
