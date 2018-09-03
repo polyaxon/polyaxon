8f213ed73791ff7bcaaef112bea60206dc1dabca
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-03 19:05:59 +0200

    Add base tracking class

--- polyaxon_client/settings.py
+++ polyaxon_client/settings.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
 import os
 
 
@@ -10,14 +13,14 @@ IN_CLUSTER = os.getenv('POLYAXON_IN_CLUSTER', False)
 API_HOST = os.getenv('POLYAXON_API_HOST', None)
 HTTP_PORT = os.getenv('POLYAXON_HTTP_PORT', None)
 WS_PORT = os.getenv('POLYAXON_WS_PORT', None)
-USE_HTTPS = os.getenv('POLYAXON_USE_HTTPS', None)
+USE_HTTPS = os.getenv('POLYAXON_USE_HTTPS', False)
 API_HTTP_HOST = os.getenv('POLYAXON_API_HTTP_HOST', None)
 API_WS_HOST = os.getenv('POLYAXON_API_WS_HOST', None)
 INTERNAL_SECRET_TOKEN = os.getenv('POLYAXON_INTERNAL_SECRET_TOKEN', None)
 SECRET_TOKEN = os.getenv('POLYAXON_SECRET_TOKEN', None)
 AUTHENTICATION_TYPE = os.getenv('POLYAXON_AUTHENTICATION_TYPE', AuthenticationTypes.TOKEN)
 API_VERSION = os.getenv('POLYAXON_API_VERSION', 'v1')
-USE_HTTPS = os.getenv('POLYAXON_API_VERSION', False)
+HASH_LENGTH = os.getenv('POLYAXON_HASH_LENGTH', 12)
 INTERNAL_HEADER = os.getenv('POLYAXON_INTERNAL_HEADER', None)
 INTERNAL_HEADER_VALUE = os.getenv('POLYAXON_INTERNAL_HEADER_VALUE', None)
 
---
+++ tests/test_tracking/test_base_tracking.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from polyaxon_client.tracking.base import BaseTracker
+from tests.utils import TestEnvVarsCase
+
+
+class TestTracker(TestEnvVarsCase):
+    def test_empty_outputs_path(self):
+        self.check_empty_value('POLYAXON_RUN_OUTPUTS_PATH', BaseTracker.get_outputs_path)
+
+    def test_valid_outputs_path(self):
+        self.check_valid_value('POLYAXON_RUN_OUTPUTS_PATH', BaseTracker.get_outputs_path, 'path')
+
+    def test_empty_data_path(self):
+        self.check_empty_value('POLYAXON_RUN_DATA_PATHS', BaseTracker.get_data_paths)
+
+    def test_valid_data_path(self):
+        self.check_valid_dict_value('POLYAXON_RUN_DATA_PATHS', BaseTracker.get_data_paths,
+                                    {'data': 'path'})
+
+    def test_empty_outputs_refs_paths(self):
+        self.check_empty_value('POLYAXON_REFS_OUTPUTS_PATHS', BaseTracker.get_outputs_refs_paths)
+
+    def test_valid_data_path(self):
+        self.check_valid_dict_value('POLYAXON_REFS_OUTPUTS_PATHS',
+                                    BaseTracker.get_outputs_refs_paths,
+                                    {
+                                        'jobs': ['path1', 'path12'],
+                                        'experiments': ['path1', 'path12']
+                                    })
+
+    def test_empty_log_level(self):
+        self.check_empty_value('POLYAXON_LOG_LEVEL', BaseTracker.get_log_level)
+
+    def test_valid_log_level(self):
+        self.check_valid_value('POLYAXON_LOG_LEVEL', BaseTracker.get_log_level, 'info')
