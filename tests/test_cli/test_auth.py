607532e2a565992c2de3cd9b056aac1f11adab58
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-07 20:08:53 +0200

    Update tests

--- tests/test_cli/test_auth.py
+++ tests/test_cli/test_auth.py
@@ -10,11 +10,11 @@ from polyaxon_cli.cli.auth import logout, whoami
 
 class TestAuth(BaseCommandTestCase):
     @patch('polyaxon_cli.managers.auth.AuthConfigManager.purge')
-    def test_logout(self, purge_patch):
+    def test_logout(self, get_user):
         self.runner.invoke(logout)
-        assert purge_patch.call_count == 1
+        assert get_user.call_count == 1
 
     @patch('polyaxon_client.api.auth.AuthApi.get_user')
-    def test_whoami(self, purge_patch):
+    def test_whoami(self, get_user):
         self.runner.invoke(whoami)
-        assert purge_patch.call_count == 1
+        assert get_user.call_count == 1
--- tests/test_cli/utils.py
+++ tests/test_cli/utils.py
@@ -4,8 +4,15 @@ from __future__ import absolute_import, division, print_function
 from unittest import TestCase
 
 from click.testing import CliRunner
+from mock import patch
 
 
 class BaseCommandTestCase(TestCase):
     def setUp(self):
         self.runner = CliRunner()
+        self.mock_config()
+
+    def mock_config(self):
+        patcher = patch('polyaxon_cli.managers.config.GlobalConfigManager.get_value')
+        patcher.start()
+        self.addCleanup(patcher.stop)
