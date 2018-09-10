b97a5108c26f3e6b4f4a57c9148393e0ba13cfb4
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-10 13:45:10 +0200

    Add ephemeral token redis tests

---
+++ tests/test_redis/test_redis_ephemeral_tokens.py
@@ -0,0 +1,42 @@
+import pytest
+
+from libs.redis_db import RedisEphemeralTokens
+from tests.utils import BaseTest
+
+
+@pytest.mark.redis_mark
+class TestRedisEphemeralTokens(BaseTest):
+    def test_objects(self):
+        token = RedisEphemeralTokens()
+        assert token.key is not None
+        assert token.redis_key == RedisEphemeralTokens.KEY_EPHEMERAL_TOKENS.format(token.key)
+
+        assert token.get_state() is None
+        assert token.salt is None
+        assert token.ttl is None
+        assert token.scope is None
+
+        token = RedisEphemeralTokens.generate(scope=token.get_scope('user', 'experiment', 1))
+
+        assert token.get_state() is not None
+        assert token.salt is not None
+        assert token.ttl == RedisEphemeralTokens.EXPIRATION_TTL
+        assert token.scope == token.get_scope('user', 'experiment', 1)
+        assert token.check_token('foo') is False
+        # Checking delete the token
+        assert token.get_state() is None
+
+        token = RedisEphemeralTokens.generate(scope=token.get_scope('user', 'experiment', 1))
+        assert token.check_token(None) is False
+        # Checking delete the token
+        assert token.get_state() is None
+
+        token = RedisEphemeralTokens.generate(scope=token.get_scope('user', 'experiment', 1))
+        valid = RedisEphemeralTokens.make_token(token)
+        assert token.check_token(valid) is True
+
+        # Checking delete the token
+        assert token.get_state() is None
+        assert token.salt is None
+        assert token.ttl is None
+        assert token.scope is None
