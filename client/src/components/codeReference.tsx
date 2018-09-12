81a7934f7a8c3d32f87d93b05ecc2a9da59b11b6
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-12 15:43:32 +0200

    Add code reference tab

--- client/src/components/codeReference.tsx
+++ client/src/components/codeReference.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import * as actions from '../actions/codeReference';
 import { CodeReferenceModel } from '../models/codeReference';
+import VerticalTable from './verticalTable';
 
 export interface Props {
   codeReference: CodeReferenceModel;
@@ -16,8 +17,16 @@ export default class CodeReference extends React.Component<Props, {}> {
   public render() {
     const getComponent = () => {
       if (this.props.codeReference) {
+        const values = {
+          'commit': this.props.codeReference.commit,
+          'branch': this.props.codeReference.branch,
+          'head': this.props.codeReference.head,
+          'is dirty': this.props.codeReference.is_dirty,
+          'git url': this.props.codeReference.git_url,
+          'repo': this.props.codeReference.repo
+        };
         return (
-          this.props.codeReference.id
+          <VerticalTable values={values}/>
         );
       }
       return (null);
