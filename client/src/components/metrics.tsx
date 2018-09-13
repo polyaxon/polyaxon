b31cc3d6a25d55a265f5ff46e066058032269d6a
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-13 14:37:38 +0200

    Lint file

--- client/src/components/metrics.tsx
+++ client/src/components/metrics.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
 import * as moment from 'moment';
+import * as React from 'react';
 
 import * as actions from '../actions/metrics';
 import { MetricModel } from '../models/metric';
 import MetricLineChart from './metricLineChart';
 
-import './metrics.less';
 import { Data, DataPoint } from '../constants/charts';
 import { EmptyList } from './empty/emptyList';
+import './metrics.less';
 
 export interface Props {
   metrics: MetricModel[];
@@ -16,24 +16,24 @@ export interface Props {
 }
 
 export default class Metrics extends React.Component<Props, {}> {
-  componentDidMount() {
+  public componentDidMount() {
     this.props.fetchData();
   }
 
   public render() {
 
-    let convertTimeFormat = (d: string) => {
+    const convertTimeFormat = (d: string) => {
       return moment(d).format('DD-MM HH:mm');
     };
 
-    let getMetricComponentData = () => {
-      let metrics = this.props.metrics;
-      let metricData: { [key: string]: DataPoint[] } = {};
+    const getMetricComponentData = () => {
+      const metrics = this.props.metrics;
+      const metricData: { [key: string]: DataPoint[] } = {};
 
-      for (let metric of metrics) {
-        let createdAt = metric.created_at;
-        for (let metricName of Object.keys(metric.values)) {
-          let dataPoint = {index: convertTimeFormat(createdAt), value: metric.values[metricName]};
+      for (const metric of metrics) {
+        const createdAt = metric.created_at;
+        for (const metricName of Object.keys(metric.values)) {
+          const dataPoint = {index: convertTimeFormat(createdAt), value: metric.values[metricName]};
           if (metricName in metricData) {
             metricData[metricName].push(dataPoint);
           } else {
@@ -42,9 +42,9 @@ export default class Metrics extends React.Component<Props, {}> {
         }
       }
 
-      let data: Data[] = [];
+      const data: Data[] = [];
 
-      for (let metricName of Object.keys(metricData)) {
+      for (const metricName of Object.keys(metricData)) {
         data.push(
           {
             key: metricName,
@@ -55,11 +55,11 @@ export default class Metrics extends React.Component<Props, {}> {
       return data;
     };
 
-    let getMetricComponent = () => {
+    const getMetricComponent = () => {
       if (this.props.count === 0) {
         return EmptyList(false, 'metric', 'metric');
       } else {
-        let data = getMetricComponentData();
+        const data = getMetricComponentData();
         return data.map(
           (mData, idx) => {
             return (
