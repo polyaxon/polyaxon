4286c6cc18990d4dffffd979916e8200b5da6929
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-14 18:58:54 +0200

    Update component did update to plotly chart

--- client/src/components/charts/plotlyChart.tsx
+++ client/src/components/charts/plotlyChart.tsx
@@ -2,60 +2,60 @@ import * as Plotly from 'plotly.js';
 import * as React from 'react';
 
 interface Frame {
-    name: string;
-    data: [{ x: Plotly.Datum, y: Plotly.Datum }];
-    group: 'lower' | 'upper';
-  }
+  name: string;
+  data: [{ x: Plotly.Datum, y: Plotly.Datum }];
+  group: 'lower' | 'upper';
+}
 
 interface Figure {
-    data: Plotly.Data[];
-    layout: Partial<Plotly.Layout>;
-  }
+  data: Plotly.Data[];
+  layout: Partial<Plotly.Layout>;
+}
 
 interface PlotParams {
-    data: Plotly.Data[];
-    layout: Partial<Plotly.Layout>;
-    frames?: Frame[];
-    config?: Partial<Plotly.Config>;
-    revision?: number;
-    onInitialized?: (figure: Readonly<Figure>, graphDiv: Readonly<HTMLElement>) => void;
-    onUpdate?: (figure: Readonly<Figure>, graphDiv: Readonly<HTMLElement>) => void;
-    onPurge?: (figure: Readonly<Figure>, graphDiv: Readonly<HTMLElement>) => void;
-    onError?: (err: Readonly<Error>) => void;
-    divId?: string;
-    className?: string;
-    style?: React.CSSProperties;
-    debug?: boolean;
-    useResizeHandler?: boolean;
+  data: Plotly.Data[];
+  layout: Partial<Plotly.Layout>;
+  frames?: Frame[];
+  config?: Partial<Plotly.Config>;
+  revision?: number;
+  onInitialized?: (figure: Readonly<Figure>, graphDiv: Readonly<HTMLElement>) => void;
+  onUpdate?: (figure: Readonly<Figure>, graphDiv: Readonly<HTMLElement>) => void;
+  onPurge?: (figure: Readonly<Figure>, graphDiv: Readonly<HTMLElement>) => void;
+  onError?: (err: Readonly<Error>) => void;
+  divId?: string;
+  className?: string;
+  style?: React.CSSProperties;
+  debug?: boolean;
+  useResizeHandler?: boolean;
 
-    onAfterExport?: () => void;
-    onAfterPlot?: () => void;
-    onAnimated?: () => void;
-    onAnimatingFrame?: (event: Readonly<Plotly.FrameAnimationEvent>) => void;
-    onAnimationInterrupted?: () => void;
-    onAutoSize?: () => void;
-    onBeforeExport?: () => void;
-    onButtonClicked?: (event: Readonly<Plotly.ButtonClickEvent>) => void;
-    onClick?: (event: Readonly<Plotly.PlotMouseEvent>) => void;
-    onClickAnnotation?: (event: Readonly<Plotly.ClickAnnotationEvent>) => void;
-    onDeselect?: () => void;
-    onDoubleClick?: () => void;
-    onFramework?: () => void;
-    onHover?: (event: Readonly<Plotly.PlotMouseEvent>) => void;
-    onLegendClick?: (event: Readonly<Plotly.LegendClickEvent>) => boolean;
-    onLegendDoubleClick?: (event: Readonly<Plotly.LegendClickEvent>) => boolean;
-    onRelayout?: (event: Readonly<Plotly.PlotRelayoutEvent>) => void;
-    onRestyle?: (event: Readonly<Plotly.PlotRestyleEvent>) => void;
-    onRedraw?: () => void;
-    onSelected?: (event: Readonly<Plotly.PlotSelectionEvent>) => void;
-    onSelecting?: (event: Readonly<Plotly.PlotSelectionEvent>) => void;
-    onSliderChange?: (event: Readonly<Plotly.SliderChangeEvent>) => void;
-    onSliderEnd?: (event: Readonly<Plotly.SliderEndEvent>) => void;
-    onSliderStart?: (event: Readonly<Plotly.SliderStartEvent>) => void;
-    onTransitioning?: () => void;
-    onTransitionInterrupted?: () => void;
-    onUnhover?: (event: Readonly<Plotly.PlotMouseEvent>) => void;
-  }
+  onAfterExport?: () => void;
+  onAfterPlot?: () => void;
+  onAnimated?: () => void;
+  onAnimatingFrame?: (event: Readonly<Plotly.FrameAnimationEvent>) => void;
+  onAnimationInterrupted?: () => void;
+  onAutoSize?: () => void;
+  onBeforeExport?: () => void;
+  onButtonClicked?: (event: Readonly<Plotly.ButtonClickEvent>) => void;
+  onClick?: (event: Readonly<Plotly.PlotMouseEvent>) => void;
+  onClickAnnotation?: (event: Readonly<Plotly.ClickAnnotationEvent>) => void;
+  onDeselect?: () => void;
+  onDoubleClick?: () => void;
+  onFramework?: () => void;
+  onHover?: (event: Readonly<Plotly.PlotMouseEvent>) => void;
+  onLegendClick?: (event: Readonly<Plotly.LegendClickEvent>) => boolean;
+  onLegendDoubleClick?: (event: Readonly<Plotly.LegendClickEvent>) => boolean;
+  onRelayout?: (event: Readonly<Plotly.PlotRelayoutEvent>) => void;
+  onRestyle?: (event: Readonly<Plotly.PlotRestyleEvent>) => void;
+  onRedraw?: () => void;
+  onSelected?: (event: Readonly<Plotly.PlotSelectionEvent>) => void;
+  onSelecting?: (event: Readonly<Plotly.PlotSelectionEvent>) => void;
+  onSliderChange?: (event: Readonly<Plotly.SliderChangeEvent>) => void;
+  onSliderEnd?: (event: Readonly<Plotly.SliderEndEvent>) => void;
+  onSliderStart?: (event: Readonly<Plotly.SliderStartEvent>) => void;
+  onTransitioning?: () => void;
+  onTransitionInterrupted?: () => void;
+  onUnhover?: (event: Readonly<Plotly.PlotMouseEvent>) => void;
+}
 
 class PlotlyChart extends React.Component<PlotParams, {}> {
   public container: Plotly.PlotlyHTMLElement | null = null;
@@ -80,7 +80,7 @@ class PlotlyChart extends React.Component<PlotParams, {}> {
   };
 
   public draw = async (props: PlotParams) => {
-    const { data, layout, config } = props;
+    const {data, layout, config} = props;
     if (this.container) {
       // plotly.react will not destroy the old plot: https://plot.ly/javascript/plotlyjs-function-reference/#plotlyreact
       this.container = await Plotly.react(this.container, data, Object.assign({}, layout), config);
@@ -93,6 +93,10 @@ class PlotlyChart extends React.Component<PlotParams, {}> {
   }
 
   public componentDidMount() {
+    this.draw(this.props);
+  }
+
+  public componentDidUpdate() {
     this.draw(this.props);
   }
 
