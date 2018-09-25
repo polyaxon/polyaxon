04dc91879a93aa8e920e6f61bca7ce4238b1b7d5
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-25 12:57:49 +0200

    Update django

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(name='polyaxon',
       ],
       install_requires=[
            "celery==4.2.1",
-           "Django==2.0.8",
+           "Django==2.1.2",
            "django-cors-headers==2.4.0",
            "djangorestframework==3.8.2",
            "docker==3.4.0",
