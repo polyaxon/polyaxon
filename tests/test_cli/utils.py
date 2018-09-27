622b1d752b146607c9f20d94e5fb6bc44ce017e8
mouradmourafiq <mouradmourafiq@gmail.com>
2018-09-27 17:23:21 +0200

    Order imports

--- tests/test_cli/utils.py
+++ tests/test_cli/utils.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+from mock import patch
 from unittest import TestCase
 
 from click.testing import CliRunner
-from mock import patch
 
 
 class BaseCommandTestCase(TestCase):
