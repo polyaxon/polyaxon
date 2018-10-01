373bb844aa8f19147705ab2d9e3d51c9011acb5e
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-01 15:05:17 +0200

    Update libs api urls

--- polyaxon/libs/api.py
+++ polyaxon/libs/api.py
@@ -6,13 +6,25 @@ API_HTTP_URL = 'POLYAXON_API_HTTP_HOST'
 API_WS_HOST = 'POLYAXON_API_WS_HOST'
 
 
-def get_settings_http_api_url():
+def get_settings_external_http_api_url():
+    return '{}://{}:{}'.format(settings.PROTOCOL,
+                               settings.POLYAXON_K8S_API_HOST,
+                               80)
+
+
+def get_settings_external_ws_api_url():
+    return '{}://{}:{}'.format(settings.PROTOCOL,
+                               settings.POLYAXON_K8S_API_HOST,
+                               1337)
+
+
+def get_settings_external_http_api_url():
     return '{}://{}:{}'.format(settings.PROTOCOL,
                                settings.POLYAXON_K8S_API_HOST,
                                settings.POLYAXON_K8S_API_HTTP_PORT)
 
 
-def get_settings_ws_api_url():
+def get_settings_external_ws_api_url():
     return '{}://{}:{}'.format(settings.PROTOCOL,
                                settings.POLYAXON_K8S_API_HOST,
                                settings.POLYAXON_K8S_API_WS_PORT)
