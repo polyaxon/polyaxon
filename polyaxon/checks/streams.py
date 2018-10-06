5ccc7a901261914a65f5ec73220559fe20d3ccdf
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-06 13:44:14 +0200

    Add streams status check

---
+++ polyaxon/checks/streams.py
@@ -0,0 +1,18 @@
+from checks.base import Check
+from checks.results import Result
+from libs.http import safe_request
+
+
+class StreamsCheck(Check):
+
+    @classmethod
+    def run(cls):
+        response = safe_request('', 'GET')
+        status_code = response.status_code
+        if status_code == 200:
+            result = Result()
+        else:
+            result = Result(message='Service is not healthy, response {}'.format(status_code),
+                            severity=Result.ERROR)
+
+        return result
