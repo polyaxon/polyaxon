d791e79837d060c6d46e25d405ee9e2a26be44cb
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-08 17:37:20 +0200

    Update rhea

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(name='polyaxon-schemas',
           'numpy==1.15.2',
           'python-dateutil==2.7.3',
           'pytz==2018.5',
-          'rhea==0.3.8'
+          'rhea==0.4.0'
       ],
       classifiers=[
           'Programming Language :: Python',
