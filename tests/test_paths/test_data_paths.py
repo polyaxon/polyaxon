f6a050a2c274ff2d22875b35f634f357fb722914
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-11 14:37:31 +0200

    Add tests for data and outputs paths

---
+++ tests/test_paths/test_data_paths.py
@@ -0,0 +1,46 @@
+import pytest
+
+from django.conf import settings
+from django.test import override_settings
+
+from libs.paths.data_paths import validate_persistence_data, get_data_paths
+from libs.paths.exceptions import VolumeNotFoundError
+from tests.utils import BaseTest
+
+
+@pytest.mark.paths_mark
+class TestDataPaths(BaseTest):
+    PERSISTENCE_DATA = {
+        'data1': {
+            'mountPath': '/data/1',
+            'existingClaim': 'test-claim-data-1'
+        },
+        'data2': {
+            'mountPath': '/data/2',
+            'hostPath': '/root/data'
+        },
+        'data3': {
+            'storage': 'gcs',
+            'bucket': 'gs://data-bucket',
+            'secret': 'secret-name',
+            'secretKey': 'secret-key'
+        }
+    }
+
+    def test_validate_persistence_data(self):
+        assert validate_persistence_data(['path1', 'path2']) == ['path1', 'path2']
+        assert validate_persistence_data(None) == settings.PERSISTENCE_DATA.keys()
+
+    def test_get_data_paths_raises_for_unrecognised_paths(self):
+        with self.assertRaises(VolumeNotFoundError):
+            get_data_paths(['path1', 'path2'])
+
+    @override_settings(PERSISTENCE_DATA=PERSISTENCE_DATA)
+    def test_get_data_paths_works_as_expected(self):
+        with self.assertRaises(VolumeNotFoundError):
+            get_data_paths(['path1', 'path2'])
+
+        assert get_data_paths(['data2']) == {'data2': '/data/2'}
+        assert get_data_paths(['data3']) == {'data3': 'gs://data-bucket'}
+        assert get_data_paths(['data2', 'data3']) == {'data2': '/data/2',
+                                                      'data3': 'gs://data-bucket'}
---
+++ tests/test_paths/test_outputs_paths.py
@@ -0,0 +1,47 @@
+import pytest
+
+from django.conf import settings
+from django.test import override_settings
+
+from libs.paths.outputs_paths import validate_persistence_outputs, get_outputs_paths
+from libs.paths.exceptions import VolumeNotFoundError
+from tests.utils import BaseTest
+
+
+@pytest.mark.paths_mark
+class TestOutputsPaths(BaseTest):
+    PERSISTENCE_OUTPUTS = {
+        'outputs1': {
+            'mountPath': '/outputs/1',
+            'existingClaim': 'test-claim-outputs-1'
+        },
+        'outputs2': {
+            'mountPath': '/outputs/2',
+            'hostPath': '/root/outputs'
+        },
+        'outputs3': {
+            'storage': 'gcs',
+            'bucket': 'gs://output-bucket',
+            'secret': 'secret-name',
+            'secretKey': 'secret-key'
+        }
+    }
+
+    def test_validate_persistence_outputs(self):
+        with self.assertRaises(VolumeNotFoundError):
+            validate_persistence_outputs(['path1', 'path2'])
+
+        assert validate_persistence_outputs('path2') == 'path2'
+        assert validate_persistence_outputs(None) == list(settings.PERSISTENCE_OUTPUTS.keys())[0]
+
+    def test_get_outputs_paths_raises_for_unrecognised_paths(self):
+        with self.assertRaises(VolumeNotFoundError):
+            get_outputs_paths(['path1', 'path2'])
+
+    @override_settings(PERSISTENCE_OUTPUTS=PERSISTENCE_OUTPUTS)
+    def test_get_outputs_paths_works_as_expected(self):
+        with self.assertRaises(VolumeNotFoundError):
+            get_outputs_paths('path1')
+
+        assert get_outputs_paths('outputs2') == '/outputs/2'
+        assert get_outputs_paths('outputs3') == 'gs://output-bucket'
