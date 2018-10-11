0484168c7f2ec70c8fd1460d1d5eab944124e3a4
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-11 14:37:04 +0200

    Extend data and outputs paths with support for buckets

--- polyaxon/libs/paths/data_paths.py
+++ polyaxon/libs/paths/data_paths.py
@@ -15,10 +15,15 @@ def get_data_paths(persistence_data):
         if persistence not in settings.PERSISTENCE_DATA:
             raise VolumeNotFoundError('Data volume with name `{}` was defined in specification, '
                                       'but was not found'.format(persistence))
-        if 'mountPath' not in settings.PERSISTENCE_DATA[persistence]:
+        persistence_type_condition = (
+            'mountPath' not in settings.PERSISTENCE_DATA[persistence] and
+            'bucket' not in settings.PERSISTENCE_DATA[persistence]
+        )
+        if persistence_type_condition:
             raise VolumeNotFoundError('Data volume with name `{}` '
-                                      'does not define a mountPath.'.format(persistence))
+                                      'does not define a mountPath or bucket.'.format(persistence))
 
-        persistence_paths[persistence] = settings.PERSISTENCE_DATA[persistence]['mountPath']
+        persistence_paths[persistence] = (settings.PERSISTENCE_DATA[persistence].get('mountPath') or
+                                          settings.PERSISTENCE_DATA[persistence].get('bucket'))
 
     return persistence_paths
--- polyaxon/libs/paths/outputs_paths.py
+++ polyaxon/libs/paths/outputs_paths.py
@@ -5,7 +5,12 @@ from libs.paths.exceptions import VolumeNotFoundError
 
 def validate_persistence_outputs(persistence_outputs):
     # If no persistence is defined we mount the first one as default
-    return persistence_outputs or list(settings.PERSISTENCE_OUTPUTS.keys())[0]
+    if not persistence_outputs:
+        return list(settings.PERSISTENCE_OUTPUTS.keys())[0]
+    if not isinstance(persistence_outputs, str):
+        raise VolumeNotFoundError('Persistence outputs value is not valid `{}`, '
+                                  'it should be a string.'.format(persistence_outputs))
+    return persistence_outputs
 
 
 def get_outputs_paths(persistence_outputs):
@@ -13,8 +18,14 @@ def get_outputs_paths(persistence_outputs):
     if persistence_outputs not in settings.PERSISTENCE_OUTPUTS:
         raise VolumeNotFoundError('Outputs volume with name `{}` was defined in specification, '
                                   'but was not found'.format(persistence_outputs))
-    if 'mountPath' not in settings.PERSISTENCE_OUTPUTS[persistence_outputs]:
-        raise VolumeNotFoundError('Outputs volume with name `{}` '
-                                  'does not define a mountPath.'.format(persistence_outputs))
+    persistence_type_condition = (
+        'mountPath' not in settings.PERSISTENCE_OUTPUTS[persistence_outputs] and
+        'bucket' not in settings.PERSISTENCE_OUTPUTS[persistence_outputs]
+    )
+    if persistence_type_condition:
+        raise VolumeNotFoundError(
+            'Outputs volume with name `{}` '
+            'does not define a mountPath or bucket.'.format(persistence_outputs))
 
-    return settings.PERSISTENCE_OUTPUTS[persistence_outputs]['mountPath']
+    return (settings.PERSISTENCE_OUTPUTS[persistence_outputs].get('mountPath') or
+            settings.PERSISTENCE_OUTPUTS[persistence_outputs].get('bucket'))
