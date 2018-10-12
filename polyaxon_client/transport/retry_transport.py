fc34ae3514b80a90ecc5c914d92bece80a55396c
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-12 16:24:10 +0200

    Extract retry transport

---
+++ polyaxon_client/transport/retry_transport.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
+
+class RetryTransportMixin(object):
+    """Threads operations transport."""
+
+    @property
+    def retry_session(self):
+        if not hasattr(self, '_retry_session'):
+            self._retry_session = requests.Session()
+            retry = Retry(
+                total=3,
+                read=3,
+                connect=3,
+                backoff_factor=2,
+                status_forcelist=[429, 500, 502, 503, 504],
+            )
+            adapter = HTTPAdapter(max_retries=retry)
+            self._retry_session.mount('http://', adapter)
+            self._retry_session.mount('https://', adapter)
+            self._periodic_done = 0
+            self._periodic_exceptions = 0
+        return self._retry_session
