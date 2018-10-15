9612785cc5bf0809577998d3f45b7c69fc8802c2
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-15 14:19:02 +0200

    Add slugify method

---
+++ polyaxon/libs/slugs.py
@@ -0,0 +1,16 @@
+import re
+import unicodedata
+
+from django.utils.safestring import mark_safe
+
+
+def slugify(value):
+    """
+    Convert spaces/dots to hyphens.
+    Remove characters that aren't alphanumerics, underscores, or hyphens.
+    Also strip leading and trailing whitespace.
+    """
+    value = str(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\.\s-]', '', value).strip()
+    return mark_safe(re.sub(r'[-\.\s]+', '-', value))
