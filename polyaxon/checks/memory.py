f9e6e9a8be0a80d1d642e60b801eeecf56a26511
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-16 15:52:16 +0200

    Add memory check

---
+++ polyaxon/checks/memory.py
@@ -0,0 +1,36 @@
+import psutil
+
+from checks.base import Check
+from checks.results import Result
+
+
+class MemoryCheck(Check):
+    WARNING_THRESHOLD = 10
+    ERROR_THRESHOLD = 10
+    HOST = None
+
+    @classmethod
+    def run(cls):
+        try:
+            memory = psutil.virtual_memory()
+            available = '{:n}'.format(int(memory.available / 1024 / 1024))
+            if memory.available < (cls.ERROR_THRESHOLD * 1024 * 1024):
+                result = Result(
+                    message='Memory available ({}) is below error threshold ({})'.format(
+                        available, cls.ERROR_THRESHOLD),
+                    severity=Result.ERROR
+                )
+            elif memory.available < (cls.ERROR_THRESHOLD * 1024 * 1024):
+                result = Result(
+                    message='Memory available ({}) is below warning threshold ({})'.format(
+                        available, cls.WARNING_THRESHOLD),
+                    severity=Result.WARNING
+                )
+            else:
+                result = Result()
+        except ValueError:
+            result = Result(
+                message='An error was raised, try again later.',
+                severity=Result.ERROR
+            )
+        return {'MEMORY': result}
