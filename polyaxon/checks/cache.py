06196b71bc179ba621bc73d546539ac8b9e020a1
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-16 15:31:17 +0200

    Add cache health status

---
+++ polyaxon/checks/cache.py
@@ -0,0 +1,36 @@
+from django.core.cache import cache, CacheKeyWarning
+
+from checks.base import Check
+from checks.results import Result
+
+
+class CacheCheck(Check):
+
+    @classmethod
+    def run(cls):
+        try:
+            cache.set('health_check', 'test', 1)
+            if cache.get('health_check') != 'test':
+                result = Result(
+                    message='Cache key does not match.',
+                    severity=Result.ERROR
+                )
+            else:
+                result = Result()
+        except CacheKeyWarning:
+            result = Result(
+                message='Cache key warning.',
+                severity=Result.ERROR
+            )
+        except ValueError:
+            result = Result(
+                message='Cache raised a ValueError.',
+                severity=Result.ERROR
+            )
+        except ConnectionError:
+            result = Result(
+                message='Cache raised a ConnectionError.',
+                severity=Result.ERROR
+            )
+
+        return {'CACHE': result}
