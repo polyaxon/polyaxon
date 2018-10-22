a9bfe6c71b7c567091085bce41831d7682099a6e
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-22 16:31:42 +0200

    Update publisher to allow to stream only without invoking tasks

--- polyaxon/publisher/service.py
+++ polyaxon/publisher/service.py
@@ -24,15 +24,18 @@ class PublisherService(Service):
                                    log_lines,
                                    experiment_uuid,
                                    experiment_name,
-                                   job_uuid):
+                                   job_uuid,
+                                   send_task=True):
         log_lines = to_list(log_lines)
         self._logger.debug("Publishing log event for task: %s, %s", job_uuid, experiment_name)
-        celery_app.send_task(
-            LogsCeleryTasks.LOGS_HANDLE_EXPERIMENT_JOB,
-            kwargs={
-                'experiment_name': experiment_name,
-                'experiment_uuid': experiment_uuid,
-                'log_lines': '\n'.join(log_lines)})
+
+        if send_task:
+            celery_app.send_task(
+                LogsCeleryTasks.LOGS_HANDLE_EXPERIMENT_JOB,
+                kwargs={
+                    'experiment_name': experiment_name,
+                    'experiment_uuid': experiment_uuid,
+                    'log_lines': '\n'.join(log_lines)})
         try:
             should_stream = (RedisToStream.is_monitored_job_logs(job_uuid) or
                              RedisToStream.is_monitored_experiment_logs(experiment_uuid))
@@ -52,7 +55,7 @@ class PublisherService(Service):
                             'log_lines': log_lines,
                         },
                         retry=True,
-                        routing_key='{}.{}.{}'.format(RoutingKeys.LOGS_SIDECARS_EXPERIMENTS,
+                        routing_key='{}.{}.{}'.format(RoutingKeys.STREAM_LOGS_SIDECARS_EXPERIMENTS,
                                                       experiment_uuid,
                                                       job_uuid),
                         exchange=settings.INTERNAL_EXCHANGE,
@@ -82,35 +85,37 @@ class PublisherService(Service):
                 except (TimeoutError, AMQPError):
                     pass
 
-    def publish_build_job_log(self, log_lines, job_uuid, job_name):
+    def publish_build_job_log(self, log_lines, job_uuid, job_name, send_task=True):
         log_lines = to_list(log_lines)
 
         self._logger.info("Publishing log event for task: %s", job_uuid)
-        celery_app.send_task(
-            LogsCeleryTasks.LOGS_HANDLE_BUILD_JOB,
-            kwargs={
-                'job_uuid': job_uuid,
-                'job_name': job_name,
-                'log_lines': '\n'.join(log_lines)
-            })
+        if send_task:
+            celery_app.send_task(
+                LogsCeleryTasks.LOGS_HANDLE_BUILD_JOB,
+                kwargs={
+                    'job_uuid': job_uuid,
+                    'job_name': job_name,
+                    'log_lines': '\n'.join(log_lines)
+                })
         self._stream_job_log(job_uuid=job_uuid,
                              log_lines=log_lines,
-                             routing_key=RoutingKeys.LOGS_SIDECARS_BUILDS)
+                             routing_key=RoutingKeys.STREAM_LOGS_SIDECARS_BUILDS)
 
-    def publish_job_log(self, log_lines, job_uuid, job_name):
+    def publish_job_log(self, log_lines, job_uuid, job_name, send_task=True):
         log_lines = to_list(log_lines)
 
         self._logger.info("Publishing log event for task: %s", job_uuid)
-        celery_app.send_task(
-            LogsCeleryTasks.LOGS_HANDLE_JOB,
-            kwargs={
-                'job_uuid': job_uuid,
-                'job_name': job_name,
-                'log_lines': '\n'.join(log_lines)
-            })
+        if send_task:
+            celery_app.send_task(
+                LogsCeleryTasks.LOGS_HANDLE_JOB,
+                kwargs={
+                    'job_uuid': job_uuid,
+                    'job_name': job_name,
+                    'log_lines': '\n'.join(log_lines)
+                })
         self._stream_job_log(job_uuid=job_uuid,
                              log_lines=log_lines,
-                             routing_key=RoutingKeys.LOGS_SIDECARS_JOBS)
+                             routing_key=RoutingKeys.STREAM_LOGS_SIDECARS_JOBS)
 
     def setup(self):
         import logging
