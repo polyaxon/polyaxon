845492400d1050a36bde4bd8ba9fa23949598b05
mouradmourafiq <mouradmourafiq@gmail.com>
2018-10-23 15:05:34 +0200

    Remove unused import

--- polyaxon_client/api_config.py
+++ polyaxon_client/api_config.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, division, print_function
 
 from polyaxon_client import settings
 from polyaxon_client.exceptions import PolyaxonClientException
-from polyaxon_client.settings import AuthenticationTypes
 
 
 class ApiConfig(object):
