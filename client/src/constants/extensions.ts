026829a8de12d28324d91cb9e341741c1bfd9f46
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-03 15:06:55 +0100

    Add supported extensions

---
+++ client/src/constants/extensions.ts
@@ -0,0 +1,27 @@
+export const IMAGE_EXTENSIONS = new Set([
+  'jpg',
+  'bmp',
+  'jpeg',
+  'png',
+  'gif',
+  'svg'
+]);
+
+export const CODE_EXTENSIONS = new Set([
+  'py',
+  'js',
+  'java',
+  'go',
+]);
+
+export const TEXT_EXTENSIONS = new Set([
+  'txt',
+  'log',
+  'yaml',
+  'yml',
+  'json',
+  'csv',
+  'tsv',
+  'md',
+  'rst'
+]);
