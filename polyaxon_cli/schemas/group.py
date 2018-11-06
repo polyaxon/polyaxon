a010b86cf0363b7540e6ff34a8c29d7d0f926415
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-06 09:29:12 +0100

    Remove experiment group patch

--- polyaxon_cli/schemas/group.py
+++ polyaxon_cli/schemas/group.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-from polyaxon_client.schemas import ExperimentGroupConfig  # noqa
+from polyaxon_schemas.project import ExperimentGroupConfig  # noqa
