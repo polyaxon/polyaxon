006fd8978c64aa2cb38d6203b508ee302f6fc8d2
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-09 10:40:25 +0100

    Set searches choices

--- polyaxon/db/models/searches.py
+++ polyaxon/db/models/searches.py
@@ -8,12 +8,20 @@ from db.models.utils import DiffModel, NameableModel
 
 class Search(DiffModel, NameableModel):
     """A saved search query."""
+    search_content_types = (
+        (content_types.PROJECT, content_types.PROJECT),
+        (content_types.EXPERIMENT_GROUP, content_types.EXPERIMENT_GROUP),
+        (content_types.EXPERIMENT, content_types.EXPERIMENT),
+        (content_types.JOB, content_types.JOB),
+        (content_types.BUILD_JOB, content_types.BUILD_JOB),
+    )
+
     project = models.ForeignKey(
         'db.Project',
         on_delete=models.CASCADE,
         related_name='searches')
     content_type = models.CharField(
-        choices=content_types.CHOICES,
+        choices=search_content_types,
         max_length=24,
         blank=True,
         null=True)
