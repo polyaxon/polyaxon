04b6b4b7a2967feb7093ce1f041c095a41d28b39
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-09 19:34:09 +0100

    Add new authentication system

---
+++ polyaxon/scopes/authentication/base.py
@@ -0,0 +1,14 @@
+from hestia.auth import AuthenticationTypes, AuthenticationError
+from rest_framework.authentication import BasicAuthentication
+
+
+class PolyaxonAuthentication(BasicAuthentication):
+    keyword = None
+
+    def __init__(self):
+        if self.keyword not in AuthenticationTypes.VALUES:
+            raise AuthenticationError('Authentication bad configuration, '
+                                      'the keyword `{}` is not supported.'.format(self.keyword))
+
+    def authenticate_header(self, request):
+        return self.keyword
