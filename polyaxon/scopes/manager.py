48b8da2c442dd88b81da3a9216bf4354a364b6aa
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-11 20:41:32 +0100

    Add scope manager

---
+++ polyaxon/scopes/manager.py
@@ -0,0 +1,14 @@
+class ScopeMappingManager(object):
+    def __init__(self, config):
+        self._config = config
+        self._default = {
+            'HEAD': (),
+            'GET': (),
+            'POST': (),
+            'PUT': (),
+            'PATCH': (),
+            'DELETE': (),
+        }
+
+    def get(self, endpoint):
+        return self._config.get(endpoint, self._default)
