325da1b4511a07b4a273b7c9ef39cf174ae4f707
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-19 16:54:00 +0100

    Fix pylint

--- polyaxon/db/models/tokens.py
+++ polyaxon/db/models/tokens.py
@@ -42,7 +42,7 @@ class Token(DiffModel):
         return self.started_at + timedelta(days=settings.TOKEN_EXPIRATION) <= timezone.now()
 
     def has_scope(self, scope):
-        return scope in self.scopes  # pylint:disable=unsupported-membership-tes
+        return scope in self.scopes  # pylint:disable=unsupported-membership-test
 
     def refresh(self, started_at=None):
         self.update(
