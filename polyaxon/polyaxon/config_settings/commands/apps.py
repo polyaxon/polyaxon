8143942e13b8dcedd39214cda9b7e39b03844d4b
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-20 10:46:07 +0100

    Add ownership to commands

--- polyaxon/polyaxon/config_settings/commands/apps.py
+++ polyaxon/polyaxon/config_settings/commands/apps.py
@@ -2,6 +2,7 @@ from ..apps import *
 from ..auditor_apps import AUDITOR_APPS
 
 PROJECT_APPS = AUDITOR_APPS + (
+    'ownership.apps.OwnershipConfig',
     'commands.apps.CommandsConfig',
     'publisher.apps.PublisherConfig',
 )
