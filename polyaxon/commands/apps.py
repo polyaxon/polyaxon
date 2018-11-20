5f2cd4ccfc38b16c186ddfd997c25807067c1813
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-20 19:24:48 +0100

    Bump version to 0.3.2 [Add missing signal]

--- polyaxon/commands/apps.py
+++ polyaxon/commands/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CommandsConfig(AppConfig):
     name = 'commands'
     verbose_name = 'Commands'
+
+    def ready(self):
+        import signals.users  # noqa
