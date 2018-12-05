23d1d9d6a39a0f967aed3cacd6a53df2dabe1cb9
mouradmourafiq <mouradmourafiq@gmail.com>
2018-12-05 11:14:30 +0100

    Update owner with is_user check

--- polyaxon/db/models/owner.py
+++ polyaxon/db/models/owner.py
@@ -4,6 +4,7 @@ from django.core.validators import validate_unicode_slug
 from django.db import models
 from django.utils.functional import cached_property
 
+from constants import content_types
 from db.models.utils import DiffModel
 
 
@@ -31,3 +32,7 @@ class Owner(DiffModel):
     @cached_property
     def owner_type(self):
         return self.content_type.model
+
+    @cached_property
+    def is_user_owner(self):
+        return self.owner_type == content_types.USER
