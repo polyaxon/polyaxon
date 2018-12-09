032684c09c91007b6475d3011cff7aedcfed0ee8
mouradmourafiq <mouradmourafiq@gmail.com>
2018-12-09 15:23:39 +0100

    Update project object permission

--- polyaxon/access/object_permissions/project.py
+++ polyaxon/access/object_permissions/project.py
@@ -4,20 +4,11 @@ from scopes.access import DEFAULT_ACCESS, OWNER_ACCESS, SUPERUSER_ACCESS, UNAUTH
 
 
 def has_object_permission(permission, request, view, obj):
-    user = request.user
-
-    if not user or user.is_anonymous or not user.is_active:
-        request.access = UNAUTHENTICATED_ACCESS
-        return False
-
-    if user.is_superuser or user.is_staff:
-        request.access = SUPERUSER_ACCESS
+    # We check if the access type before continuing other checks
+    if request.access.is_superuser or request.access.is_owner:
         return True
 
-    if obj.owner == user:
-        request.access = OWNER_ACCESS
-        return True
-
+    # Other user
     if request.method in permissions.SAFE_METHODS and obj.is_public:
         request.access = DEFAULT_ACCESS
         return True
