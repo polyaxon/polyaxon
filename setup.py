ab2c144e427996b87aec364e3aec5bb3d0e067bf
mouradmourafiq <mouradmourafiq@gmail.com>
2018-12-11 12:30:47 +0100

    Update hestia

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(name='polyaxon-schemas',
           'kubernetes',
       ],
       install_requires=[
-          'hestia==0.1.4',
+          'hestia==0.1.5',
           'Jinja2==2.10',
           'marshmallow==2.16.3',
           'numpy>=1.15.2',
