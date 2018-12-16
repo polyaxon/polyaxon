2132f7d6a06778090e3535494e6a8351bfffa036
mouradmourafiq <mouradmourafiq@gmail.com>
2018-12-16 23:40:37 +0100

    Update requirements

--- client/src/constants/dev.env.ts
+++ client/src/constants/dev.env.ts
@@ -1,3 +1,3 @@
-export const DEV_BASE_URL = 'http://104.197.164.112';
-export const DEV_TOKEN = 'f639c5cfc2a3416bbfd450560333ada5bf3b23721b7a43fdbf0a794ae468a049';
-export const DEV_USER = 'root';
+export const DEV_BASE_URL = 'http://localhost:8000';
+export const DEV_TOKEN = '8ff04973157b2a5831329fbb1befd37f93e4de4f';
+export const DEV_USER = 'admin';
