ba49a9b3344f30f5bd3ea05144546e6a8a763ef0
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-03 21:29:20 +0100

    Add more tests for config list

--- tests/test_cli/test_config.py
+++ tests/test_cli/test_config.py
@@ -8,7 +8,22 @@ from polyaxon_cli.cli.config import config
 
 
 class TestConfigManager(BaseCommandTestCase):
-    @patch('polyaxon_cli.managers.config.GlobalConfigManager.get_config')
-    def test_config_list(self, get_user):
+    @patch('polyaxon_cli.managers.config.GlobalConfigManager.is_initialized')
+    def test_config_list_checks_initialized(self, is_initialized):
+        is_initialized.return_value = False
         self.runner.invoke(config, ['--list'])
-        assert get_user.call_count == 1
+        assert is_initialized.call_count == 1
+
+    @patch('polyaxon_cli.managers.config.GlobalConfigManager.is_initialized')
+    @patch('polyaxon_cli.managers.config.GlobalConfigManager.CONFIG')
+    def test_config_list_gets_default_config(self, default_config, is_initialized):
+        is_initialized.return_value = False
+        self.runner.invoke(config, ['--list'])
+        assert default_config.call_count == 1
+
+    @patch('polyaxon_cli.managers.config.GlobalConfigManager.is_initialized')
+    @patch('polyaxon_cli.managers.config.GlobalConfigManager.get_config')
+    def test_config_list_gets_file_config(self, get_config, is_initialized):
+        is_initialized.return_value = True
+        self.runner.invoke(config, ['--list'])
+        assert get_config.call_count == 1
