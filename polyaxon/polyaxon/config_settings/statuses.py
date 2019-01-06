e1bd91d965e1a3a09cda583376c9134fb632de62
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-06 16:27:15 +0100

    Update ocular

--- polyaxon/polyaxon/config_settings/statuses.py
+++ polyaxon/polyaxon/config_settings/statuses.py
@@ -2,4 +2,4 @@ from polyaxon.config_manager import config
 
 TTL_WATCH_STATUSES = config.get_int('POLYAXON_TTL_WATCH_STATUSES',
                                     is_optional=True,
-                                    default=60 * 60)
+                                    default=60 * 20)
