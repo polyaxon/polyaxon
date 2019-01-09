c2cde0c5078f8a3b7153647e3a2f5699a7542f34
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-09 12:53:55 +0100

    Init repo

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='polyaxon-deploy',
+      version='0.3.5',
+      description='Tools to validate and deploy Polyaxon to one of the supported platforms.',
+      maintainer='Mourad Mourafiq',
+      maintainer_email='mourad@polyaxon.com',
+      author='Mourad Mourafiq',
+      author_email='mourad@polyaxon.com',
+      url='https://github.com/polyaxon/polyaxon-deploy',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'aws',
+          'microsoft',
+          'azure',
+          'google cloud',
+          'tensorFlow',
+          'pytorch',
+          'deep-learning',
+          'machine-learning',
+          'data-science',
+          'neural-networks',
+          'artificial-intelligence',
+          'ai',
+          'reinforcement-learning',
+          'kubernetes',
+      ],
+      install_requires=[
+          "polyaxon-schemas==0.3.5",
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=[
+          "pytest",
+          "httpretty==0.8.14",
+          "fake-factory==0.7.2"
+      ],
+      cmdclass={'test': PyTest})
