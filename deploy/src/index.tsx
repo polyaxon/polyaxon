9461fc0ef91b588e8eb9d401ce199e33baeeeb8b
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-09 14:02:18 +0100

    Move to polyaxon deploy app

--- deploy/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import App from './components/app';
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root') as HTMLElement
-);
