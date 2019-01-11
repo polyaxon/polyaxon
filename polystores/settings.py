f5d4c844484f6f8043a7aa8c37e1e2f709928752
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-11 12:19:43 +0100

    Update polystores settings

--- polystores/settings.py
+++ polystores/settings.py
@@ -11,6 +11,6 @@ config = rhea.Rhea.read_configs([
 RUN_STORES_ACCESS_KEYS = config.get_dict('POLYSTORES_RUN_STORES_ACCESS_KEYS',
                                          is_optional=True,
                                          default={})
-TMP_AUTH_GCS_ACCESS_PATH = config.get_string('POLYSTORES_RUN_STORES_ACCESS_KEYS',
+TMP_AUTH_GCS_ACCESS_PATH = config.get_string('POLYSTORES_TMP_AUTH_GCS_ACCESS_PATH',
                                              is_optional=True,
                                              default='/tmp/.polyaxon/.gcsaccess.json')
