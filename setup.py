60e56598417f0a968ffe742069c9ca5f61c2b8b1
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-12 16:07:14 +0100

    Bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polystores',
-      version='0.0.7',
+      version='0.0.8',
       description='Polystores is an abstraction and a collection of clients '
                   'to interact with cloud storages.',
       maintainer='Mourad Mourafiq',
