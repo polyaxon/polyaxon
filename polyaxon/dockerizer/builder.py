bd9a329d0498ba4d3682bce52c4b8fcf9affbd9d
mouradmourafiq <mouradmourafiq@gmail.com>
2019-01-26 13:48:28 +0100

    Update dockerizer job

--- polyaxon/dockerizer/builder.py
+++ polyaxon/dockerizer/builder.py
@@ -7,7 +7,6 @@ import time
 
 from docker import APIClient
 from docker.errors import APIError, BuildError, DockerException
-from hestia.internal_services import InternalServices
 from hestia.list_utils import to_list
 from hestia.logging_utils import LogLevels
 
@@ -17,7 +16,7 @@ from constants.jobs import JobLifeCycle
 from db.redis.heartbeat import RedisHeartBeat
 from docker_images.image_info import get_image_name, get_tagged_image
 from dockerizer.dockerfile import POLYAXON_DOCKER_TEMPLATE
-from libs.http import download, untar_file
+from dockerizer.init.git_download import download_code
 from libs.paths.utils import delete_path
 from polyaxon.celery_api import celery_app
 from polyaxon.settings import K8SEventsCeleryTasks, SchedulerCeleryTasks
@@ -225,80 +224,6 @@ class DockerBuilder(object):
     def push(self):
         stream = self.docker.push(self.image_name, tag=self.image_tag, stream=True)
         return self._handle_log_stream(stream=stream)
-
-
-def download_code(build_job, build_path, filename):
-    if not os.path.exists(build_path):
-        os.makedirs(build_path)
-
-    filename = '{}/{}'.format(build_path, filename)
-
-    if build_job.code_reference.repo:
-        download_url = build_job.code_reference.repo.download_url
-        internal = True
-        headers = {
-            conf.get('HEADERS_INTERNAL').replace('_', '-'): InternalServices.DOCKERIZER
-        }
-        access_token = None
-    elif build_job.code_reference.git_url:
-        download_url = build_job.code_reference.git_url
-        internal = False
-        access_token = conf.get('REPOS_ACCESS_TOKEN')
-        # Gitlab requires heaer `private-token`
-        headers = {}
-    else:
-        raise ValueError('Code reference for this build job does not have any repo.')
-
-    if internal:
-        if build_job.code_reference.commit:
-            download_url = '{}?commit={}'.format(download_url, build_job.code_reference.commit)
-        tar_suffix = None
-    else:
-        tar_suffix = (
-            build_job.code_reference.commit
-            if build_job.code_reference.commit
-            else 'master'
-        )
-        archive_url = '/archive'
-        if 'bitbucket' in download_url.lower():
-            if access_token:
-                headers = {'Authorization': 'Bearer {}'.format(access_token)}
-            archive_url = '/get'
-        elif 'github' not in download_url.lower():
-            # We assume it's a gitlab (either saas or on-premis)
-            if access_token:
-                headers = {'PRIVATE-TOKEN': access_token}
-            download_url += '/-'  # Gitlab requires this underscore for valid urls
-        download_url += archive_url
-        download_url += '/{}'.format(tar_suffix)
-        download_url += '.tar.gz'
-
-    repo_file = download(
-        url=download_url,
-        filename=filename,
-        logger=_logger,
-        headers=headers,
-        access_token=access_token,
-        internal=internal)
-    if not repo_file:
-        send_status(build_job=build_job,
-                    status=JobLifeCycle.FAILED,
-                    message='Could not download code to build the image.')
-        return False
-    status = untar_file(
-        build_path=build_path,
-        filename=filename,
-        logger=_logger,
-        delete_tar=True,
-        internal=internal,
-        tar_suffix=tar_suffix)
-    if not status:
-        send_status(build_job=build_job,
-                    status=JobLifeCycle.FAILED,
-                    message='Could not handle downloaded code to build the image.')
-        return False
-
-    return True
 
 
 def build(build_job):
---
+++ polyaxon/dockerizer/init/git_download.py
@@ -0,0 +1,85 @@
+import logging
+import os
+
+from hestia.internal_services import InternalServices
+
+import conf
+
+from constants.jobs import JobLifeCycle
+from libs.http import download, untar_file
+
+_logger = logging.getLogger('polyaxon.dockerizer')
+
+
+def download_code(build_job, build_path, filename):
+    if not os.path.exists(build_path):
+        os.makedirs(build_path)
+
+    filename = '{}/{}'.format(build_path, filename)
+
+    if build_job.code_reference.repo:
+        download_url = build_job.code_reference.repo.download_url
+        internal = True
+        headers = {
+            conf.get('HEADERS_INTERNAL').replace('_', '-'): InternalServices.DOCKERIZER
+        }
+        access_token = None
+    elif build_job.code_reference.git_url:
+        download_url = build_job.code_reference.git_url
+        internal = False
+        access_token = conf.get('REPOS_ACCESS_TOKEN')
+        # Gitlab requires heaer `private-token`
+        headers = {}
+    else:
+        raise ValueError('Code reference for this build job does not have any repo.')
+
+    if internal:
+        if build_job.code_reference.commit:
+            download_url = '{}?commit={}'.format(download_url, build_job.code_reference.commit)
+        tar_suffix = None
+    else:
+        tar_suffix = (
+            build_job.code_reference.commit
+            if build_job.code_reference.commit
+            else 'master'
+        )
+        archive_url = '/archive'
+        if 'bitbucket' in download_url.lower():
+            if access_token:
+                headers = {'Authorization': 'Bearer {}'.format(access_token)}
+            archive_url = '/get'
+        elif 'github' not in download_url.lower():
+            # We assume it's a gitlab (either saas or on-premis)
+            if access_token:
+                headers = {'PRIVATE-TOKEN': access_token}
+            download_url += '/-'  # Gitlab requires this underscore for valid urls
+        download_url += archive_url
+        download_url += '/{}'.format(tar_suffix)
+        download_url += '.tar.gz'
+
+    repo_file = download(
+        url=download_url,
+        filename=filename,
+        logger=_logger,
+        headers=headers,
+        access_token=access_token,
+        internal=internal)
+    if not repo_file:
+        send_status(build_job=build_job,
+                    status=JobLifeCycle.FAILED,
+                    message='Could not download code to build the image.')
+        return False
+    status = untar_file(
+        build_path=build_path,
+        filename=filename,
+        logger=_logger,
+        delete_tar=True,
+        internal=internal,
+        tar_suffix=tar_suffix)
+    if not status:
+        send_status(build_job=build_job,
+                    status=JobLifeCycle.FAILED,
+                    message='Could not handle downloaded code to build the image.')
+        return False
+
+    return True
