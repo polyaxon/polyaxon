5206518446768c48bc03f716c3f2b0e520da008c
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-06 10:06:10 +0100

    Update config settings

--- polyaxon/administration/register/repos.py
+++ polyaxon/administration/register/repos.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from db.models.repos import CodeReference, Repo
+from db.models.repos import CodeReference, ExternalRepo, Repo
 
 
 class RepoAdmin(admin.ModelAdmin):
     readonly_fields = ('project', 'path',)
 
 
+class ExternalRepoAdmin(admin.ModelAdmin):
+    readonly_fields = ('project', 'name', 'git_url', 'path',)
+
+
 def register(admin_register):
     admin_register(Repo, RepoAdmin)
+    admin.site.register(ExternalRepo, ExternalRepoAdmin)
     admin_register(CodeReference)
--- polyaxon/polyaxon/config_settings/repos.py
+++ polyaxon/polyaxon/config_settings/repos.py
@@ -1,6 +1,6 @@
 from polyaxon.config_manager import config
 
 REPOS_ACCESS_TOKEN_KEY = 'POLYAXON_REPOS_ACCESS_TOKEN'
-REPOS_ACCESS_TOKEN = config.get_string(REPOS_ACCESS_TOKEN_KEY,
-                                       is_optional=True,
-                                       is_secret=True)
+REPOS_ACCESS_TOKEN = config.get_auth(REPOS_ACCESS_TOKEN_KEY,
+                                     is_optional=True,
+                                     is_secret=True)
