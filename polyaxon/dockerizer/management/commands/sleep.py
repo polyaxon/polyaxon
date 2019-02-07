77818eea46c7281aadc43d25ed50c66380f9f58f
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-07 17:04:53 +0100

    Update sleep cmd

--- polyaxon/dockerizer/management/commands/sleep.py
+++ polyaxon/dockerizer/management/commands/sleep.py
@@ -10,5 +10,9 @@ class Command(BaseCommand):
     """Management utility to start building a docker image."""
     help = 'Used to initialize a context to build a docker image.'
 
+    def add_arguments(self, parser):
+        parser.add_argument('build_job_uuid')
+        super().add_arguments(parser)
+
     def handle(self, *args, **options):
         time.sleep(60 * 60)
