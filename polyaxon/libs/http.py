e93a2b56055a0ed4d8f644b0f05cb780f7dda3ef
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-08 18:06:05 +0100

    Move download logic to dockerizer

--- polyaxon/libs/http.py
+++ polyaxon/libs/http.py
@@ -1,16 +1,7 @@
-import os
-import requests
-import tarfile
-
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 from urllib.parse import parse_qs, urlencode, urljoin, urlparse, urlunparse
 
-from hestia.auth import AuthenticationTypes
-from hestia.fs import move_recursively
-
 import conf
-
-from libs.api import get_http_api_url
 
 
 def absolute_uri(url: str) -> Optional[str]:
@@ -38,82 +29,3 @@ def add_notification_referrer_param(url: str,
     url_list = list(parsed_url)
     url_list[4] = urlencode(query, doseq=True)
     return urlunparse(url_list)
-
-
-def download(url: str,
-             filename: str,
-             logger,
-             authentication_type: str = None,
-             access_token: str = None,
-             headers: Dict = None,
-             internal: bool = True,
-             timeout: int = 60) -> Optional[str]:
-    """Get download url from the internal api."""
-    if internal:
-        authentication_type = authentication_type or AuthenticationTypes.INTERNAL_TOKEN
-    else:
-        authentication_type = AuthenticationTypes.TOKEN
-
-    if authentication_type == AuthenticationTypes.INTERNAL_TOKEN and not access_token:
-        access_token = conf.get('SECRET_INTERNAL_TOKEN')
-
-    # Auth headers if access_token is present
-    request_headers = {}
-    if access_token:
-        request_headers["Authorization"] = "{} {}".format(authentication_type, access_token)
-    # Add any additional headers
-    if headers:
-        request_headers.update(headers)
-
-    try:
-        if internal:
-            api_url = get_http_api_url()
-            url = '{}/{}'.format(api_url, url)
-        print("Downloading file from %s using %s" % (url, authentication_type))
-        response = requests.get(url,
-                                headers=request_headers,
-                                timeout=timeout,
-                                stream=True)
-
-        if response.status_code != 200:
-            logger.error("Failed to download file from %s: %s" % (url, response.status_code),
-                         extra={'stack': True})
-            return None
-
-        with open(filename, 'wb') as f:
-            logger.debug("Processing file %s" % filename)
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-        return filename
-
-    except requests.exceptions.RequestException:
-        logger.error("Download exception", exc_info=True)
-        return None
-
-
-def untar_file(build_path: str,
-               filename: str,
-               logger,
-               delete_tar: bool = False,
-               internal: bool = False,
-               tar_suffix: str = None) -> Optional[str]:
-    extract_path = build_path if internal else '/tmp'
-    if filename and os.path.exists(filename):
-        logger.debug("Untarring the contents of the file ...")
-        tar = tarfile.open(filename)
-        tar.extractall(extract_path)
-        tar.close()
-        if delete_tar:
-            logger.debug("Cleaning up the tar file ...")
-            os.remove(filename)
-
-        if not internal:
-            tarf = [f for f in os.listdir(extract_path) if tar_suffix in f]
-            if tarf:
-                src = os.path.join(extract_path, tarf[0])
-                move_recursively(src, build_path)
-        return filename
-    else:
-        print("File was not found, build_path: %s" % os.listdir(build_path))
-        return None
