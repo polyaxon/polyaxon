9886e67d66a2234e9efc920101fb84bdcaff8ab9
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-10 00:12:01 +0100

    Add build to tracking

--- polyaxon_client/tracking/__init__.py
+++ polyaxon_client/tracking/__init__.py
@@ -3,4 +3,5 @@
 from polyaxon_client.tracking.experiment import Experiment
 from polyaxon_client.tracking.group import Group
 from polyaxon_client.tracking.job import Job
+from polyaxon_client.tracking.build import BuildJob
 from polyaxon_client.tracking.paths import *
