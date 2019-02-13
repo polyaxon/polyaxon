3285058c31c97af1d6f55d64385f32e832879c5e
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-14 00:56:29 +0100

    Update init process and catch any exception that could break the cli

--- polyaxon_cli/cli/init.py
+++ polyaxon_cli/cli/init.py
@@ -5,8 +5,8 @@ import os
 import sys
 
 import click
+import clint
 
-from marshmallow import ValidationError
 
 from polyaxon_cli.cli.getters.project import get_project_or_local
 from polyaxon_cli.client import PolyaxonClient
@@ -14,7 +14,6 @@ from polyaxon_cli.client.exceptions import PolyaxonHTTPError, PolyaxonShouldExit
 from polyaxon_cli.logger import clean_outputs
 from polyaxon_cli.managers.ignore import IgnoreManager
 from polyaxon_cli.managers.project import ProjectManager
-from polyaxon_cli.schemas.exceptions import PolyaxonfileError
 from polyaxon_cli.schemas.polyaxonfile import PolyaxonFile
 from polyaxon_cli.utils import constants
 from polyaxon_cli.utils.files import create_init_file
@@ -22,14 +21,34 @@ from polyaxon_cli.utils.formatting import Printer
 from polyaxon_client.exceptions import PolyaxonClientException
 
 
+def create_polyaxonfile():
+    if os.path.isfile(constants.INIT_FILE):
+        try:
+            _ = PolyaxonFile(constants.INIT_FILE).specification
+            Printer.print_success(
+                "A valid polyaxonfile.yaml was found in the project.".format(constants.INIT_FILE))
+        except Exception as e:
+            Printer.print_error("A Polyaxonfile was found but it is not valid.", add_sign=True)
+            Printer.print_error('Error message `{}`.'.format(e))
+            sys.exit(1)
+    else:
+        create_init_file(constants.INIT_FILE_RUN)
+        # if we are here the file was not created
+        if not os.path.isfile(constants.INIT_FILE):
+            Printer.print_error(
+                "Something went wrong, init command did not create a file.\n"
+                "Possible reasons: you don't have enough rights to create the file.")
+            sys.exit(1)
+
+        Printer.print_success("{} was created successfully.".format(constants.INIT_FILE))
+
+
 @click.command()
 @click.argument('project', type=str)
-@click.option('--run', is_flag=True, default=True, show_default=True,
+@click.option('--polyaxonfile', is_flag=True, default=False, show_default=False,
               help='Init a polyaxon file with `exec` step template.')
-@click.option('--model', is_flag=True, default=False, show_default=True,
-              help='Init a polyaxon file with `model` step template.')
 @clean_outputs
-def init(project, run, model):
+def init(project, polyaxonfile):
     """Initialize a new polyaxonfile specification."""
     user, project_name = get_project_or_local(project)
     try:
@@ -43,48 +62,39 @@ def init(project, run, model):
         Printer.print_error('Error message `{}`.'.format(e))
         sys.exit(1)
 
-    if not any([model, run]) and not all([model, run]):
-        Printer.print_error("You must specify which init option, "
-                            "possible values: `--model` or `--run`.")
-        sys.exit(1)
+    init_project = False
+    if ProjectManager.is_initialized():
+        local_project = ProjectManager.get_config()
+        click.echo('Warning! This project is already initialized with the following project:')
+        with clint.textui.indent(4):
+            clint.textui.puts('User: {}'.format(local_project.user))
+            clint.textui.puts('Project: {}'.format(local_project.name))
+        if click.confirm('Would you like to override this current config?', default=False):
+            init_project = True
+    else:
+        init_project = True
 
-    result = False
-    if model:
-        result = create_init_file(constants.INIT_FILE_MODEL)
-
-    elif run:
-        result = create_init_file(constants.INIT_FILE_RUN)
-
-    if result:
+    if init_project:
         ProjectManager.purge()
         ProjectManager.set_config(project_config, init=True)
+        Printer.print_success('Project was initialized', add_sign=True)
+    else:
+        Printer.print_header('Project config was not changed.')
+
+    init_ignore = False
+    if IgnoreManager.is_initialized():
+        click.echo('Warning! Found a .polyaxonignore file.')
+        if click.confirm('Would you like to override it?', default=False):
+            init_ignore = True
+    else:
+        init_ignore = True
+
+    if init_ignore:
         IgnoreManager.init_config()
-        Printer.print_success(
-            "Project `{}` was initialized and Polyaxonfile was created successfully `{}`".format(
-                project, constants.INIT_FILE))
+        Printer.print_success('New .polyaxonignore file was created.', add_sign=True)
         sys.exit(1)
+    else:
+        Printer.print_header('.polyaxonignore file was not changed.')
 
-    # if we are here the file was not created
-    if not os.path.isfile(constants.INIT_FILE):
-        Printer.print_error(
-            "Something went wrong, init command did not create a file.\n"
-            "Possible reasons: you don't have the write to create the file.")
-        sys.exit(1)
-
-    # file was already there, let's check if the project passed correspond to this file
-    try:
-        PolyaxonFile(constants.INIT_FILE).specification
-    except (PolyaxonfileError, ValidationError) as e:
-        Printer.print_error(
-            "Something went wrong, init command did not create a file.\n"
-            "Another file already exist with.")
-        Printer.print_error('Error message: `{}`.'.format(e))
-        sys.exit(1)
-
-    # At this point we check if we need to re init configurations
-    ProjectManager.purge()
-    ProjectManager.set_config(project_config, init=True)
-    IgnoreManager.init_config()
-    Printer.print_success(
-        "Project `{}` was initialized and Polyaxonfile was created successfully `{}`".format(
-            project, constants.INIT_FILE))
+    if polyaxonfile:
+        create_polyaxonfile()
