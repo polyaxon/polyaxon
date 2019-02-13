351e8dadb5312c9a66b992a0a6bad2c687cd81a7
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-14 00:59:16 +0100

    Catch errors that could be raised during the upload of files:  * An OsError could be raised if there's not enough space

--- polyaxon_cli/cli/upload.py
+++ polyaxon_cli/cli/upload.py
@@ -21,19 +21,24 @@ def upload():  # pylint:disable=assign-to-new-keyword
     """Upload code of the current directory while respecting the .polyaxonignore file."""
     project = ProjectManager.get_config_or_raise()
     files = IgnoreManager.get_unignored_file_paths()
-    with create_tarfile(files, project.name) as file_path:
-        with get_files_in_current_directory('repo', [file_path]) as (files, files_size):
-            try:
-                PolyaxonClient().project.upload_repo(project.user,
-                                                     project.name,
-                                                     files,
-                                                     files_size)
-            except (PolyaxonHTTPError, PolyaxonShouldExitError, PolyaxonClientException) as e:
-                Printer.print_error('Could not upload code for project `{}`.'.format(project.name))
-                Printer.print_error('Error message `{}`.'.format(e))
-                Printer.print_error('Check if you have access rights for this project and '
-                                    'that you are not uploading large files.'
-                                    'If you are running a notebook, '
-                                    'you should stop it before uploading.')
-                sys.exit(1)
-            Printer.print_success('Files uploaded.')
+    try:
+        with create_tarfile(files, project.name) as file_path:
+            with get_files_in_current_directory('repo', [file_path]) as (files, files_size):
+                try:
+                    PolyaxonClient().project.upload_repo(project.user,
+                                                         project.name,
+                                                         files,
+                                                         files_size)
+                except (PolyaxonHTTPError, PolyaxonShouldExitError, PolyaxonClientException) as e:
+                    Printer.print_error('Could not upload code for project `{}`.'.format(project.name))
+                    Printer.print_error('Error message `{}`.'.format(e))
+                    Printer.print_error('Check if you have access rights for this project and '
+                                        'that you are not uploading large files.'
+                                        'If you are running a notebook, '
+                                        'you should stop it before uploading.')
+                    sys.exit(1)
+                Printer.print_success('Files uploaded.')
+    except Exception as e:
+        Printer.print_error("Could not upload the file.", add_sign=True)
+        Printer.print_error('Error message `{}`.'.format(e))
+        sys.exit(1)
