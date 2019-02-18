b963fcbcc4e7351500bc68fce58c2d281b7515d6
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-18 01:57:30 +0100

    Bump version to 0.1.1  * Catch container status failed issue

--- ocular/processor.py
+++ ocular/processor.py
@@ -56,7 +56,7 @@ def get_failed_status(job_container_status):
     job_container_status_terminated = job_container_status['state'][
         container_statuses.TERMINATED]
 
-    if not job_container_status:
+    if not job_container_status_terminated:
         return pod_statuses.FAILED, None
 
     if job_container_status_terminated['reason'] == 'Error':
