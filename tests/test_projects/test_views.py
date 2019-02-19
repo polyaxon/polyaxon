af6d0e50d38a2d772026480204933dc81785d6e6
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-19 15:57:43 +0100

    Fix tests

--- tests/test_projects/test_views.py
+++ tests/test_projects/test_views.py
@@ -231,7 +231,7 @@ class TestProjectDetailViewV1(BaseViewTest):
     @patch('scheduler.tasks.build_jobs.build_jobs_stop.apply_async')
     @patch('scheduler.tasks.jobs.jobs_stop.apply_async')
     @patch('scheduler.tasks.experiments.experiments_stop.apply_async')
-    @patch('scheduler.tasks.experiment_groups.experiments_group.apply_async')
+    @patch('scheduler.tasks.experiment_groups.experiments_group_stop.apply_async')
     def test_delete_archives_and_schedules_deletion(self,
                                                     xp_group_scheduler_mock,
                                                     xp_scheduler_mock,
