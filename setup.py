3d649468a4e2d359bab61d43a1fdb245f0a8746a
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-20 17:17:57 +0100

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polystores',
-      version='0.1.5',
+      version='0.1.6',
       description='Polystores is an abstraction and a collection of clients '
                   'to interact with cloud storages.',
       long_description=read_readme(),
