1d4862a139657024e834f0a0045c007613be6c9b
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-21 17:57:50 +0100

    Fix prospector

--- polyaxon_cli/schemas/polyaxonfile.py
+++ polyaxon_cli/schemas/polyaxonfile.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-from polyaxon_schemas.polyaxonfile.default import get_default_polyaxonfile
+from polyaxon_schemas.polyaxonfile.default import get_default_polyaxonfile  # noqa
 from polyaxon_schemas.polyaxonfile.polyaxonfile import PolyaxonFile  # noqa
