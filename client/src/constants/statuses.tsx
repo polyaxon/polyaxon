84ab00f136ebf35b89aa4a77ca6a7f855f90aea4
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-21 15:28:13 +0100

    Fix typo

--- client/src/constants/statuses.tsx
+++ client/src/constants/statuses.tsx
@@ -1,7 +1,7 @@
 export const CREATED = 'created';
 export const RUNNING = 'running';
 export const STOPPED = 'stopped';
-export const STOPPING = 'stopped';
+export const STOPPING = 'stopping';
 export const FAILED = 'failed';
 export const SUCCEEDED = 'succeeded';
 export const DONE = 'done';
