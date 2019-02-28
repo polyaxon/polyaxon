c9f2d6c06788fd40d33b7a5d60af47c74eb4cd5a
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-28 17:13:26 +0100

    Add role and id info

--- client/src/components/experimentJobs/experimentJobOverview.tsx
+++ client/src/components/experimentJobs/experimentJobOverview.tsx
@@ -5,6 +5,7 @@ import * as actions from '../../actions/job';
 import { ExperimentJobModel } from '../../models/experimentJob';
 import { EmptyList } from '../empty/emptyList';
 import DatesMetaInfo from '../metaInfo/datesMetaInfo';
+import IdMetaInfo from '../metaInfo/idMetaInfo';
 import NodeMetaInfo from '../metaInfo/nodeMetaInfo';
 import PodIdMetaInfo from '../metaInfo/podIdMetaInfo';
 import ResourcesMetaInfo from '../metaInfo/resourcesMetaInfo';
@@ -42,11 +43,19 @@ export default class ExperimentJobOverview extends React.Component<Props, {}> {
             <div className="row">
               <div className="col-md-11">
                 <Name
-                  name="Group Name"
+                  name="Job Name"
                   value={job.name || job.unique_name}
                   icon="fa-tasks"
                 />
               </div>
+            </div>
+            <div className="meta">
+               <span className="meta-info">
+                 <i className="fa fa-certificate icon" aria-hidden="true"/>
+                 <span className="title">Role:</span>
+                 {job.role}
+               </span>
+              <IdMetaInfo value={job.id} inline={true}/>
             </div>
             <div className="meta">
               <UserMetaInfo user={'user'} inline={true}/>
