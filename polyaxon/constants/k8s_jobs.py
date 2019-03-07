0e8e42ccc024355d0c7d71f89b500c0386c70b52
mouradmourafiq <mouradmourafiq@gmail.com>
2019-03-07 13:11:21 +0100

    Update experiment job format

--- polyaxon/constants/k8s_jobs.py
+++ polyaxon/constants/k8s_jobs.py
@@ -1,4 +1,4 @@
-EXPERIMENT_JOB_NAME_FORMAT = 'plx-{task_type}{task_idx}-{experiment_uuid}'
+EXPERIMENT_JOB_NAME_FORMAT = 'plx-{experiment_uuid}-{task_type}-{task_idx}'
 EXPERIMENT_KF_TASK_NAME_FORMAT = 'plx-{experiment_uuid}-{task_type}'
 EXPERIMENT_KF_JOB_NAME_FORMAT = 'plx-{experiment_uuid}'
 JOB_NAME_FORMAT = 'plx-{name}-{job_uuid}'
