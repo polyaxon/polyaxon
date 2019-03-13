ca1e54d54e98419d3323885f94d95ee6a8d06e90
mouradmourafiq <mouradmourafiq@gmail.com>
2019-03-13 18:36:38 +0100

    Bump version to 0.4.2rc0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-deploy',
-      version='0.4.1',
+      version='0.4.2rc0',
       description='Tools to validate and deploy Polyaxon to one of the supported platforms.',
       long_description=read_readme(),
       maintainer='Mourad Mourafiq',
@@ -53,7 +53,7 @@ setup(name='polyaxon-deploy',
           'kubernetes',
       ],
       install_requires=[
-          "polyaxon-schemas==0.4.1",
+          "polyaxon-schemas==0.4.2rc0",
       ],
       classifiers=[
           'Programming Language :: Python',
