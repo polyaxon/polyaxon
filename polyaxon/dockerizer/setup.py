0645c6660049bfcf21a1bb6058e163bee4b803c8
mouradmourafiq <mouradmourafiq@gmail.com>
2019-03-13 18:45:13 +0100

    Bump version to 0.4.2rc0

--- polyaxon/dockerizer/setup.py
+++ polyaxon/dockerizer/setup.py
@@ -23,7 +23,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-dockerizer',
-      version='0.4.1',
+      version='0.4.2rc0',
       description='Polyaxon dockerizer.',
       maintainer='Mourad Mourafiq',
       maintainer_email='mourad@polyaxon.com',
@@ -41,7 +41,7 @@ setup(name='polyaxon-dockerizer',
           'instrumentation'
       ],
       install_requires=[
-          'polyaxon-client==0.4.1',
+          'polyaxon-client==0.4.2rc0',
           'Unipath==1.1'
       ],
       classifiers=[
--- polyaxon/sidecar/setup.py
+++ polyaxon/sidecar/setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-sidecar',
-      version='0.4.1',
+      version='0.4.2rc0',
       description='Polyaxon sidecar.',
       maintainer='Mourad Mourafiq',
       maintainer_email='mourad@polyaxon.com',
@@ -40,7 +40,7 @@ setup(name='polyaxon-sidecar',
       ],
       install_requires=[
           'polyaxon-k8s==0.4.0',
-          'polyaxon-client==0.4.1',
+          'polyaxon-client==0.4.2rc0',
           'ocular==0.1.5',
           'Unipath==1.1'
       ],
