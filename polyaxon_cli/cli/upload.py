45f36c8eb5d5aa7fb6df6dccda5f6c0ac5666354
mouradmourafiq <mouradmourafiq@gmail.com>
2019-03-13 22:14:02 +0100

    Update list of possible errors when uploading code

--- polyaxon_cli/cli/upload.py
+++ polyaxon_cli/cli/upload.py
@@ -37,10 +37,12 @@ def upload(sync=True):  # pylint:disable=assign-to-new-keyword
                     Printer.print_error(
                         'Could not upload code for project `{}`.'.format(project.name))
                     Printer.print_error('Error message `{}`.'.format(e))
-                    Printer.print_error('Check if you have access rights for this project and '
-                                        'that you are not uploading large files.'
-                                        'If you are running a notebook, '
-                                        'you should stop it before uploading.')
+                    Printer.print_error(
+                        'Check the project exists, '
+                        'and that you have access rights, '
+                        'this could happen as well when uploading large files.'
+                        'If you are running a notebook and mounting the code to the notebook, '
+                        'you should stop it before uploading.')
                     sys.exit(1)
                 Printer.print_success('Files uploaded.')
     except Exception as e:
