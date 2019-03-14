a8a80d45288037b442077afc4460fa9696dbc147
mouradmourafiq <mouradmourafiq@gmail.com>
2019-03-14 01:59:16 +0100

    Bump version to 0.4.2rc1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-schemas',
-      version='0.4.2rc0',
+      version='0.4.2rc1',
       description='Schema definitions and validation for Polyaxon.',
       long_description=read_readme(),
       maintainer='Mourad Mourafiq',
