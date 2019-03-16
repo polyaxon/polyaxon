216e775e0698aad2ddb2a67438e1b264239d00d1
mouradmourafiq <mouradmourafiq@gmail.com>
2019-03-16 19:05:21 +0100

    Update raven

--- client/src/configureRaven.ts
+++ client/src/configureRaven.ts
@@ -1,10 +1,10 @@
-import * as Raven from 'raven-js';
+import * as Raven from '@sentry/browser';
 import { b64DecodeUnicode } from './constants/utils';
 
 const configureRaven = () => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'staging') {
     const dsn = 'aHR0cHM6Ly85NTZmODkwNDdhYTk0ZGQ1ODU4Mjg0N2E5YjVjMzliZEBzZW50cnkuaW8vMTE5NzU5OA==';
-    Raven.config(b64DecodeUnicode(dsn)).install();
+    Raven.init({ dsn: b64DecodeUnicode(dsn) });
   }
 };
 
