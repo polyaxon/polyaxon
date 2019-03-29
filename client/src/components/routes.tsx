5f784d824cf5baf4c59b7c55ce632fe0fc815832
mouradmourafiq <mouradmourafiq@gmail.com>
2019-03-29 18:43:10 +0100

    Extend entity creation with global shortcuts

--- client/src/components/routes.tsx
+++ client/src/components/routes.tsx
@@ -31,6 +31,11 @@ function Routes() {
   const clusterActivityLogsRoute = '/app/activitylogs/';
   const historyLogsRoute = '/app/historylogs/';
   const newProjectRoute = '/app/projects/new/';
+  const newExperimentRoute = '/app/experiments/new/';
+  const newGroupRoute = '/app/groups/new/';
+  const newJobRoute = '/app/jobs/new/';
+  const newNotebookRoute = '/app/notebooks/new/';
+  const newTensorboardRoute = '/app/tensorboards/new/';
   const userRoute = '/app/:user/';
   const bookmarksRoute = '/app/bookmarks/:user/';
   const archivesRoute = '/app/archives/:user/';
@@ -38,15 +43,15 @@ function Routes() {
   const buildDetailRoute = '/app/:user/:projectName/builds/:buildId/';
   const newBuildRoute = '/app/:user/:projectName/builds/new/';
   const notebookDetailRoute = '/app/:user/:projectName/notebooks/:notebookId/';
-  const newNotebookRoute = '/app/:user/:projectName/notebooks/new/';
+  const newProjectNotebookRoute = '/app/:user/:projectName/notebooks/new/';
   const tensorboardDetailRoute = '/app/:user/:projectName/tensorboards/:tensorboardId/';
   const newProjectTensorboardRoute = '/app/:user/:projectName/tensorboards/new/';
   const jobDetailRoute = '/app/:user/:projectName/jobs/:jobId/';
-  const newJobRoute = '/app/:user/:projectName/jobs/new/';
+  const newProjectJobRoute = '/app/:user/:projectName/jobs/new/';
   const newExperimentTensorboardRoute = '/app/:user/:projectName/experiments/:experimentId/tensorboards/new/';
-  const newExperimentRoute = '/app/:user/:projectName/experiments/new/';
+  const newProjectExperimentRoute = '/app/:user/:projectName/experiments/new/';
   const experimentDetailRoute = '/app/:user/:projectName/experiments/:experimentId/';
-  const newGroupRoute = '/app/:user/:projectName/groups/new/';
+  const newProjectGroupRoute = '/app/:user/:projectName/groups/new/';
   const groupDetailRoute = '/app/:user/:projectName/groups/:groupId/';
   const newGroupTensorboardRoute = '/app/:user/:projectName/groups/:groupId/tensorboards/new/';
   const experimentJobDetailRoute = '/app/:user/:projectName/experiments/:experimentId/jobs/:jobId/';
@@ -72,19 +77,24 @@ function Routes() {
       <Route path={historyLogsRoute} component={HistoryLogs}/>
       <Route path={clusterActivityLogsRoute} component={ClusterActivityLogs}/>
       <Route path={newProjectRoute} component={ProjectCreate}/>
+      <Route path={newExperimentRoute} component={ExperimentCreate}/>
+      <Route path={newGroupRoute} component={GroupCreate}/>
+      <Route path={newJobRoute} component={JobCreate}/>
+      <Route path={newNotebookRoute} component={NotebookCreate}/>
+      <Route path={newTensorboardRoute} component={TensorboardCreate}/>
       <Route path={experimentJobDetailRoute} component={ExperimentJobDetail}/>
       <Route path={newBuildRoute} component={BuildCreate}/>
       <Route path={buildDetailRoute} component={BuildDetail}/>
-      <Route path={newNotebookRoute} component={NotebookCreate}/>
+      <Route path={newProjectNotebookRoute} component={NotebookCreate}/>
       <Route path={notebookDetailRoute} component={NotebookDetail}/>
       <Route path={newProjectTensorboardRoute} component={TensorboardCreate}/>
       <Route path={tensorboardDetailRoute} component={TensorboardDetail}/>
-      <Route path={newJobRoute} component={JobCreate}/>
+      <Route path={newProjectJobRoute} component={JobCreate}/>
       <Route path={jobDetailRoute} component={JobDetail}/>
-      <Route path={newGroupRoute} component={GroupCreate}/>
+      <Route path={newProjectGroupRoute} component={GroupCreate}/>
       <Route path={newGroupTensorboardRoute} component={TensorboardCreate}/>
       <Route path={groupDetailRoute} component={GroupDetail}/>
-      <Route path={newExperimentRoute} component={ExperimentCreate}/>
+      <Route path={newProjectExperimentRoute} component={ExperimentCreate}/>
       <Route path={newExperimentTensorboardRoute} component={TensorboardCreate}/>
       <Route path={experimentDetailRoute} component={ExperimentDetail}/>
       <Route path={bookmarksRoute} component={Bookmarks}/>
