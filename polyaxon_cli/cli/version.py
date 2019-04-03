52ce0324530639249df74ec96b503009613b797d
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-03 12:33:35 +0200

    Add warning about cli ahead of server version

--- polyaxon_cli/cli/version.py
+++ polyaxon_cli/cli/version.py
@@ -103,6 +103,11 @@ def check_cli_version():
         ))
         with clint.textui.indent(4):
             clint.textui.puts("pip install -U polyaxon-cli")
+    elif LooseVersion(current_version) > LooseVersion(server_version.latest_version):
+        clint.textui.puts("You version of CLI ({}) is ahead of the latest version "
+                          "supported by Polyaxon Platform ({}) on your cluster, "
+                          "and might be incompatible.".format(current_version,
+                                                              server_version.latest_version))
 
 
 @click.command()
