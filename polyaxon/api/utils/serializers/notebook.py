33277cfbc71d7faffbbd1a9fb5b60b53bee41970
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-03 12:41:44 +0200

    Fix typo in serializer

--- polyaxon/api/utils/serializers/notebook.py
+++ polyaxon/api/utils/serializers/notebook.py
@@ -1,4 +1,4 @@
 class NotebookSerializerMixin(object):
 
     def get_notebook(self, obj):
-        return obj.notebook.id if obj.tensorboard else None
+        return obj.notebook.id if obj.notebook else None
