ba7797cabeb492f206fe630258cf0658385aca27
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-05 18:55:15 +0200

    Update logs user

--- manage/start.sh
+++ manage/start.sh
@@ -1,2 +1,3 @@
+chown -R polyaxon:polyaxon /polyaxon/logs/
 cd /polyaxon/polyaxon
 gosu polyaxon python3 manage.py $*
--- web/start.sh
+++ web/start.sh
@@ -2,6 +2,7 @@ python3 -c "from polyaxon_nginx.generate import generate_nginx_conf; generate_ng
 rm -f /etc/nginx/sites-enabled/default
 rm -f /etc/nginx/sites-available/default
 mkdir /etc/nginx/polyaxon
+chown -R polyaxon:polyaxon /polyaxon/logs/
 cp web/uwsgi_params /etc/nginx/uwsgi_params
 cp web/nginx.conf /etc/nginx/sites-available/polyaxon.config
 mv web/polyaxon.base.conf /etc/nginx/polyaxon/polyaxon.base.conf
