b3033b45375d839bed06c39e18830a02c4708309
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-08 20:30:54 +0200

    Fix setup file

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(TestCommand):
         sys.exit(errcode)
 
 
-setup(name='polyaxon-dockerfile-generator',
+setup(name='polyaxon-dockgen',
       version='0.4.3',
       description='Python client to interact with Polyaxon API.',
       long_description=read_readme(),
@@ -31,7 +31,7 @@ setup(name='polyaxon-dockerfile-generator',
       maintainer_email='mourad@polyaxon.com',
       author='Mourad Mourafiq',
       author_email='mourad@polyaxon.com',
-      url='https://github.com/polyaxon/polyaxon-dockerfile-generator',
+      url='https://github.com/polyaxon/polyaxon-dockgen',
       license='MIT',
       platforms='any',
       packages=find_packages(),
@@ -54,19 +54,8 @@ setup(name='polyaxon-dockerfile-generator',
           'kubernetes',
       ],
       install_requires=[
-          "clint==0.5.1",
           "polyaxon-schemas==0.4.3",
-          "polystores>=0.1.7",
-          "psutil==5.4.7",
-          "requests>=2.20.0",
-          "requests-toolbelt==0.8.0",
-          "websocket-client>=0.53.0",
       ],
-      extras_require={
-          'gcs': ['google-cloud-storage'],
-          's3': ['boto3', 'botocore'],
-          'azure': ['azure-storage'],
-      },
       classifiers=[
           'Programming Language :: Python',
           'Operating System :: OS Independent',
@@ -76,7 +65,5 @@ setup(name='polyaxon-dockerfile-generator',
       ],
       tests_require=[
           "pytest",
-          "httpretty==0.8.14",
-          "fake-factory==0.7.2"
       ],
       cmdclass={'test': PyTest})
