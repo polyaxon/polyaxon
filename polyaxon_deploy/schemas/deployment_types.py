fb1760a5d5c986a304b8b5208d1b49defe906888
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-09 17:22:13 +0200

    Add deployment types

--- polyaxon_deploy/schemas/deployment_types.py
+++ polyaxon_deploy/schemas/deployment_types.py
@@ -1,7 +1,9 @@
 class DeploymentTypes(object):
     KUBERNETES = 'kubernetes'
+    MINIKUBE = 'minikube'
+    MICRO_K8S = 'microk8s'
     DOCKER_COMPOSE = 'docker-compose'
     DOCKER = 'docker'
     HEROKU = 'heroku'
 
-    VALUES = [KUBERNETES, DOCKER_COMPOSE, DOCKER, HEROKU]
+    VALUES = [KUBERNETES, MINIKUBE, MICRO_K8S, DOCKER_COMPOSE, DOCKER, HEROKU]
