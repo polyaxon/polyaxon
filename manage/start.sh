5d7d16f6a1687de366acd6149680a414931c6385
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-09 12:32:06 +0200

    Update manage commands

--- manage/start.sh
+++ manage/start.sh
@@ -1,8 +1,7 @@
-cd /polyaxon/polyaxon
 if [[ -z "${POLYAXON_SECURITY_CONTEXT_USER}" ]] || [[ -z "${POLYAXON_SECURITY_CONTEXT_GROUP}" ]]; then
-    python3 manage.py $*
+    python3 polyaxon/manage.py $*
 else
     ./create_user.sh
     chown -R polyaxon:polyaxon /polyaxon/logs/
-    gosu polyaxon python3 manage.py $*
+    gosu polyaxon python3 polyaxon/manage.py $*
 fi
