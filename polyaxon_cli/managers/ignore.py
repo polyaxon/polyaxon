af1e7c1e32e46586dda424cb3a7d38234587c93b
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-12 00:51:14 +0200

    Upadate .polyaxonignore to honor similar syntax as .gitignore

--- polyaxon_cli/managers/ignore.py
+++ polyaxon_cli/managers/ignore.py
@@ -2,7 +2,8 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-
+import re
+from collections import namedtuple
 from pathlib import PurePath
 
 from polyaxon_cli.logger import logger
@@ -11,81 +12,161 @@ from polyaxon_cli.utils import constants
 from polyaxon_cli.utils.files import unix_style_path
 
 
+class Pattern(namedtuple("Pattern", "pattern is_exclude re")):
+    @staticmethod
+    def create(pattern):
+        if pattern[0:1] == '!':
+            is_exclude = False
+            pattern = pattern[1:]
+        else:
+            if pattern[0:1] == '\\':
+                pattern = pattern[1:]
+            is_exclude = True
+        return Pattern(pattern=pattern,
+                       is_exclude=is_exclude,
+                       re=re.compile(translate(pattern), re.IGNORECASE))
+
+    def match(self, path):
+        return bool(self.re.match(path))
+
+
+def translate(pat):
+    def _translate_segment():
+        if segment == '*':
+            return '[^/]+'
+        res = ''
+        i, n = 0, len(segment)
+        while i < n:
+            c = segment[i:i + 1]
+            i = i + 1
+            if c == '*':
+                res += '[^/]*'
+            elif c == '?':
+                res += '[^/]'
+            elif c == '[':
+                j = i
+                if j < n and segment[j:j + 1] == '!':
+                    j = j + 1
+                if j < n and segment[j:j + 1] == ']':
+                    j = j + 1
+                while j < n and segment[j:j + 1] != ']':
+                    j = j + 1
+                if j >= n:
+                    res += '\\['
+                else:
+                    stuff = segment[i:j].replace('\\', '\\\\')
+                    i = j + 1
+                    if stuff.startswith('!'):
+                        stuff = '^' + stuff[1:]
+                    elif stuff.startswith('^'):
+                        stuff = '\\' + stuff
+                    res += '[' + stuff + ']'
+            else:
+                res += re.escape(c)
+        return res
+
+    res = '(?ms)'
+
+    if '/' not in pat[:-1]:
+        # If there's no slash, this is a filename-based match
+        res += '(.*/)?'
+
+    if pat.startswith('**/'):
+        # Leading **/
+        pat = pat[2:]
+        res += '(.*/)?'
+
+    if pat.startswith('/'):
+        pat = pat[1:]
+
+    for i, segment in enumerate(pat.split('/')):
+        if segment == '**':
+            res += '(/.*)?'
+            continue
+        else:
+            res += ((re.escape('/') if i > 0 else '') + _translate_segment())
+
+    if not pat.endswith('/'):
+        res += '/?'
+
+    return res + '\\Z'
+
+
 class IgnoreManager(BaseConfigManager):
     """Manages .plxignore file in the current directory"""
     IS_GLOBAL = False
     CONFIG_FILE_NAME = '.polyaxonignore'
 
-    @classmethod
-    def init_config(cls):
-        cls.set_config(constants.DEFAULT_IGNORE_LIST, init=True)
-
     @staticmethod
     def _is_empty_or_comment(line):
         return not line or line.startswith('#')
 
-    @classmethod
-    def _get_whitelisted(cls, line):
-        return cls._remove_slash_prefix(line[1:]) if line.startswith('!') else None
-
-    @classmethod
-    def _unescape(cls, line):
-        """allow escaping file names that start with !, #, or '\' """
-        line = line[1:] if line.startswith('\\') else line
-        return cls._remove_slash_prefix(line)
-
     @staticmethod
-    def _remove_slash_prefix(line):
-        """For details on the glob matcher,
-        see: https://docs.python.org/3/library/pathlib.html#pathlib.PurePath.match
-        """
-        return line[1:] if line.startswith('/') else line
+    def _remove_trailing_spaces(line):
+        """Remove trailing spaces unless they are quoted with a backslash."""
+        while line.endswith(' ') and not line.endswith('\\ '):
+            line = line[:-1]
+        return line.replace('\\ ', ' ')
+
+    @classmethod
+    def init_config(cls):
+        cls.set_config(constants.DEFAULT_IGNORE_LIST, init=True)
+
+    @classmethod
+    def find_matching(cls, path, patterns):
+        """Yield all matching patterns for path."""
+        for pattern in patterns:
+            if pattern.match(path):
+                yield pattern
+
+    @classmethod
+    def is_ignored(cls, path, patterns):
+        """Check whether a path is ignored. For directories, include a trailing slash."""
+        status = None
+        for pattern in cls.find_matching(path, patterns):
+            status = pattern.is_exclude
+        return status
+
+    @classmethod
+    def read_file(cls, ignore_file):
+        for line in ignore_file:
+            line = line.rstrip('\r\n')
+
+            if cls._is_empty_or_comment(line):
+                continue
+
+            yield cls._remove_trailing_spaces(line)
+
+    @classmethod
+    def get_patterns(cls, ignore_file):
+        return [Pattern.create(line) for line in cls.read_file(ignore_file)]
 
     @classmethod
     def get_config(cls):
-        """This is a different config manager since it does not rely on a json object.
-
-        This config return 2 lists representing the ignore_list and white_list of files.
-        """
         config_file_path = cls.get_config_file_path()
 
         if not os.path.isfile(config_file_path):
-            return [], []
+            return []
 
-        ignore_list, white_list = [], []
         with open(config_file_path, "r") as ignore_file:
-            for line in ignore_file:
-                line = line.strip()
-                if cls._is_empty_or_comment(line):
-                    continue
-
-                white_list_line = cls._get_whitelisted(line)
-                if white_list_line:
-                    white_list.append(white_list_line)
-                    continue
-
-                ignore_list.append(cls._unescape(line))
-
-        return ignore_list, white_list
+            return cls.get_patterns(ignore_file)
 
     @classmethod
-    def get_unignored_file_paths(cls, ignore_list=None, white_list=None):
+    def get_unignored_file_paths(cls):
         config = cls.get_config()
-        ignore_list = ignore_list or config[0]
-        white_list = white_list or config[1]
         unignored_files = []
 
         for root, dirs, files in os.walk("."):
             logger.debug("Root:%s, Dirs:%s", root, dirs)
 
-            if cls._ignore_path(unix_style_path(root), ignore_list, white_list):
+            if cls.is_ignored(unix_style_path(root), config):
                 dirs[:] = []
                 logger.debug("Ignoring directory : %s", root)
                 continue
 
             for file_name in files:
                 file_path = unix_style_path(os.path.join(root, file_name))
-                if cls._ignore_path(file_path, ignore_list, white_list):
+                if cls.is_ignored(file_path, config):
                     logger.debug("Ignoring file : %s", file_name)
                     continue
 
--- tests/test_managers/test_ignore.py
+++ tests/test_managers/test_ignore.py
@@ -10,11 +10,69 @@ from polyaxon_cli.managers.ignore import IgnoreManager
 
 class TestIgnoreManager(TestCase):
     """Mock the config ignore file."""
+
     def test_default_props(self):
         assert IgnoreManager.IS_GLOBAL is False
         assert IgnoreManager.IS_POLYAXON_DIR is False
         assert IgnoreManager.CONFIG_FILE_NAME == '.polyaxonignore'
         assert IgnoreManager.CONFIG is None
+
+    @staticmethod
+    def get_ignored(patterns):
+        return [r.pattern for r in patterns if r.is_exclude]
+
+    @staticmethod
+    def get_whitelisted(patterns):
+        return [r.pattern for r in patterns if not r.is_exclude]
+
+    @patch('polyaxon_cli.managers.ignore.os.path.isfile', return_value=True)
+    @patch('polyaxon_cli.managers.ignore.open', new_callable=mock_open)
+    def test_ignored_lines(self, mock_open, _):
+        configs = [
+            ('foo.c', 'foo.[dh]'),
+            ('foo/foo.c', '/foo.c'),
+            ('foo/foo.c', '/*.c'),
+            ('foo/bar/', '/bar/'),
+            ('foo/bar/', 'foo/bar/*'),
+            ('foo/bar', 'foo?bar')
+        ]
+        for (path, pattern) in configs:
+            mock_open.return_value.__iter__.return_value = [pattern]
+            patterns = IgnoreManager.get_config()
+            self.assertEqual(
+                (self.get_ignored(patterns), self.get_whitelisted(patterns)),
+                ([pattern], []))
+            assert list(IgnoreManager.find_matching(path, patterns)) == []
+
+    @patch('polyaxon_cli.managers.ignore.os.path.isfile', return_value=True)
+    @patch('polyaxon_cli.managers.ignore.open', new_callable=mock_open)
+    def test_whitelisted_lines(self, mock_open, _):
+        configs = [
+            ('foo.c', '*.c'),
+            ('.c', '*.c'),
+            ('foo/foo.c', '*.c'),
+            ('foo/foo.c', 'foo.c'),
+            ('foo.c', '/*.c'),
+            ('foo.c', '/foo.c'),
+            ('foo.c', 'foo.c'),
+            ('foo.c', 'foo.[ch]'),
+            ('foo/bar/bla.c', 'foo/**'),
+            ('foo/bar/bla/blie.c', 'foo/**/blie.c'),
+            ('foo/bar/bla.c', '**/bla.c'),
+            ('bla.c', '**/bla.c'),
+            ('foo/bar', 'foo/**/bar'),
+            ('foo/bla/bar', 'foo/**/bar'),
+            ('foo/bar/', 'bar/'),
+            ('foo/bar/', 'bar'),
+            ('foo/bar/something', 'foo/bar/*'),
+        ]
+        for (path, pattern) in configs:
+            mock_open.return_value.__iter__.return_value = [pattern]
+            patterns = IgnoreManager.get_config()
+            self.assertEqual(
+                (self.get_ignored(patterns), self.get_whitelisted(patterns)),
+                ([pattern], []))
+            assert len(list(IgnoreManager.find_matching(path, patterns))) == 1
 
     @patch('polyaxon_cli.managers.ignore.os.path.isfile', return_value=True)
     @patch('polyaxon_cli.managers.ignore.open', new_callable=mock_open)
@@ -22,8 +80,10 @@ class TestIgnoreManager(TestCase):
         file_data = ['', '# comment', '', '*.py']
         mock_open.return_value.__iter__.return_value = file_data
 
-        result = IgnoreManager.get_config()
-        self.assertEqual(result, (['*.py'], []))
+        patterns = IgnoreManager.get_config()
+        self.assertEqual(
+            (self.get_ignored(patterns), self.get_whitelisted(patterns)),
+            (['*.py'], []))
 
     @patch('polyaxon_cli.managers.ignore.os.path.isfile', return_value=True)
     @patch('polyaxon_cli.managers.ignore.open', new_callable=mock_open)
@@ -31,8 +91,10 @@ class TestIgnoreManager(TestCase):
         file_data = ['/test', '!/ignore']
         mock_open.return_value.__iter__.return_value = file_data
 
-        result = IgnoreManager.get_config()
-        self.assertEqual(result, (['test'], ['ignore']))
+        patterns = IgnoreManager.get_config()
+        self.assertEqual(
+            (self.get_ignored(patterns), self.get_whitelisted(patterns)),
+            (['/test'], ['/ignore']))
 
     @patch('polyaxon_cli.managers.ignore.os.path.isfile', return_value=True)
     @patch('polyaxon_cli.managers.ignore.open', new_callable=mock_open)
@@ -40,13 +102,15 @@ class TestIgnoreManager(TestCase):
         file_data = ['', '# comment', '*.py', '!file1.py']
         mock_open.return_value.__iter__.return_value = file_data
 
-        result = IgnoreManager.get_config()
-        self.assertEqual(result, (['*.py'], ['file1.py']))
+        patterns = IgnoreManager.get_config()
+        self.assertEqual(
+            (self.get_ignored(patterns), self.get_whitelisted(patterns)),
+            (['*.py'], ['file1.py']))
 
     @patch('polyaxon_cli.managers.ignore.os.path.isfile', return_value=False)
     def test_returns_two_empty_lists_if_file_is_not_present(self, _):
-        result = IgnoreManager.get_config()
-        self.assertEqual(result, ([], []))
+        patterns = IgnoreManager.get_config()
+        self.assertEqual(patterns, [])
 
     @patch('polyaxon_cli.managers.ignore.os.path.isfile', return_value=True)
     @patch('polyaxon_cli.managers.ignore.open', new_callable=mock_open)
@@ -54,5 +118,7 @@ class TestIgnoreManager(TestCase):
         file_data = ['', '# comment', '\#file1', '\!file2']
         mock_open.return_value.__iter__.return_value = file_data
 
-        result = IgnoreManager.get_config()
-        self.assertEqual(result, (['#file1', '!file2'], []))
+        patterns = IgnoreManager.get_config()
+        self.assertEqual(
+            (self.get_ignored(patterns), self.get_whitelisted(patterns)),
+            (['#file1', '!file2'], []))
