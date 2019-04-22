3fb8b441778608074829e0ea85d82cbecb27a890
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-22 13:10:22 +0200

    Update commands

--- polyaxon/polyaxon/config_settings/commands/__init__.py
+++ polyaxon/polyaxon/config_settings/commands/__init__.py
@@ -1,6 +1,8 @@
+from polyaxon.config_settings.cors import *
 from polyaxon.config_settings.dirs import *
 from polyaxon.config_settings.k8s import *
 from polyaxon.config_settings.labels import *
+from polyaxon.config_settings.middlewares import *
 from polyaxon.config_settings.spawner import *
 from polyaxon.config_settings.stores import *
 
--- polyaxon/polyaxon/config_settings/commands/apps.py
+++ polyaxon/polyaxon/config_settings/commands/apps.py
@@ -2,6 +2,8 @@ from ..apps import *
 from ..auditor_apps import AUDITOR_APPS
 
 PROJECT_APPS = AUDITOR_APPS + (
+    'django.contrib.admin',
+    'django.contrib.admindocs',
     'ownership.apps.OwnershipConfig',
     'commands.apps.CommandsConfig',
     'publisher.apps.PublisherConfig',
