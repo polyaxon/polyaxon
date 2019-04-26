2721d8fa516d9d283ce7fa3061b4f13aaeda2698
mouradmourafiq <mouradmourafiq@gmail.com>
2019-04-26 02:24:11 +0200

    Fix typo

--- client/src/components/tensorboards/creationModes/template.tsx
+++ client/src/components/tensorboards/creationModes/template.tsx
@@ -58,7 +58,7 @@ export default class TensorboardCreateTemplate extends React.Component<Props, {}
           <div className="row initialize-instructions">
             <div className="col-md-12">
               <div className="jumbotron jumbotron-action text-center empty-jumbotron">
-                <h3>No templates was found or you don't have access to this feature yet.</h3>
+                <h3>No template was found or you don't have access to this feature yet.</h3>
               </div>
             </div>
           </div>
