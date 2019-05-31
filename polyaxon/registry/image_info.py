a7f08304e59ad0b80c51120c8db32b59f3ab974b
mouradmourafiq <mouradmourafiq@gmail.com>
2019-05-31 14:17:45 +0200

    Allow empty host to be able to push to default registry

--- polyaxon/registry/image_info.py
+++ polyaxon/registry/image_info.py
@@ -1,14 +1,16 @@
 import logging
 
-from typing import Tuple
+from typing import Tuple, Optional
 
 _logger = logging.getLogger('polyaxon.dockerizer.images')
 
 
-def get_image_name(build_job: 'BuildJob', registry_host: str) -> str:
-    return '{}/{}_{}'.format(registry_host,
-                             build_job.project.name.lower(),
-                             build_job.project.id)
+def get_image_name(build_job: 'BuildJob', registry_host: Optional[str]) -> str:
+    if registry_host:
+        return '{}/{}_{}'.format(registry_host,
+                                 build_job.project.name.lower(),
+                                 build_job.project.id)
+    return '{}_{}'.format(build_job.project.name.lower(), build_job.project.id)
 
 
 def get_image_info(build_job: 'BuildJob', registry_host: str) -> Tuple[str, str]:
