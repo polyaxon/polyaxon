24eb222b9af8c627fe29a084a3fcb9e7573a7b79
mouradmourafiq <mouradmourafiq@gmail.com>
2019-05-31 08:46:33 +0200

    Add crypto to requirements  * Update monitor requirements

--- polyaxon/polyaxon/config_settings/monitor_statuses/__init__.py
+++ polyaxon/polyaxon/config_settings/monitor_statuses/__init__.py
@@ -1,6 +1,5 @@
 from polyaxon.config_settings.k8s import *
 from polyaxon.config_settings.labels import *
 from polyaxon.config_settings.spawner import *
-from polyaxon.config_settings.statuses import *
 
 from .apps import *
