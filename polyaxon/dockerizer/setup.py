6d62ede1ea45c8cbd06f2c42783a33aba785f8fe
mouradmourafiq <mouradmourafiq@gmail.com>
2019-06-03 20:40:35 +0200

    Update dockerizer

--- polyaxon/dockerizer/setup.py
+++ polyaxon/dockerizer/setup.py
@@ -42,7 +42,7 @@ setup(name='polyaxon-dockerizer',
       ],
       install_requires=[
           'polyaxon-client==0.4.4',
-          'polyaxon-dockerizer==0.0.4',
+          'polyaxon-dockerizer==0.0.5',
       ],
       classifiers=[
           'Programming Language :: Python',
