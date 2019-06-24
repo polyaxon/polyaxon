0b65349ae26134f1195f08b4ab881b366e298351
mouradmourafiq <mouradmourafiq@gmail.com>
2019-06-24 23:10:13 +0200

    update polystores version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(name='polyaxon-client',
       ],
       install_requires=[
           "polyaxon-schemas==0.4.4",
-          "polystores>=0.1.8",
+          "polystores>=0.1.9",
           "psutil==5.4.7",
           "requests>=2.20.0",
           "requests-toolbelt==0.8.0",
