458fd524a4c9cf20e987ee430e1b9d59c313ff2b
mouradmourafiq <mouradmourafiq@gmail.com>
2019-07-01 17:17:00 +0200

    Bump version to 0.5.0rc2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 
 setup(name='polyaxon-client',
-      version='0.5.0rc1',
+      version='0.5.0rc2',
       description='Python client to interact with Polyaxon API.',
       long_description=read_readme(),
       long_description_content_type="text/markdown",
@@ -55,7 +55,7 @@ setup(name='polyaxon-client',
           'kubernetes',
       ],
       install_requires=[
-          "polyaxon-schemas==0.5.0rc1",
+          "polyaxon-schemas==0.5.0rc2",
           "polystores>=0.1.9",
           "psutil==5.4.7",
           "requests>=2.20.0",
