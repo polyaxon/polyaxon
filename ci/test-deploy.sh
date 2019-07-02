8baf9d45c5161f3a3e00cc5b5b86501dbda423d6
Mourad <mouradmourafiq@users.noreply.github.com>
2019-07-02 11:57:30 +0200

    Update ci (#44)

--- ci/test-deploy.sh
+++ ci/test-deploy.sh
@@ -2,7 +2,24 @@
 
 set -eux
 
-IP=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+#!/bin/sh
+
+set -eux
+
+# Is there a standard interface name?
+for iface in eth0 ens4 enp0s3; do
+    IP=$(ifconfig $iface | grep 'inet addr' | cut -d: -f2 | awk '{print $1}');
+    if [ -n "$IP" ]; then
+        echo "IP: $IP"
+        break
+    fi
+done
+if [ -z "$IP" ]; then
+    echo "Failed to get IP, current interfaces:"
+    ifconfig -a
+    exit 2
+fi
+
 TEST_NAMESPACE=polyaxon
 TEST_URL=http://$IP:31811
 
@@ -20,6 +37,24 @@ done
 
 echo "getting polyaxon version"
 curl -s $TEST_URL/api/v1/versions/cli/ | grep version
+
+
+echo "running tests"
+
+display_logs() {
+  echo "***** minikube *****"
+  minikube logs
+  echo "***** node *****"
+  kubectl describe node
+  echo "***** pods *****"
+  kubectl --namespace $TEST_NAMESPACE get pods
+  echo "***** events *****"
+  kubectl --namespace $TEST_NAMESPACE get events
+  echo "***** hub *****"
+  kubectl --namespace $TEST_NAMESPACE logs deploy/hub
+  echo "***** proxy *****"
+  kubectl --namespace $TEST_NAMESPACE logs deploy/proxy
+}
 
 echo "configure cli"
 polyaxon config set --host=$IP --port=31811 | grep updated
