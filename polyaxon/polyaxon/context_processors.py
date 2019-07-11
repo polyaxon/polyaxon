43e32e4993c0ca8ee25144aac837639fa35674a7
mouradmourafiq <mouradmourafiq@gmail.com>
2019-07-11 22:18:06 +0200

    Remove magic strings and use context processors

--- polyaxon/polyaxon/context_processors.py
+++ polyaxon/polyaxon/context_processors.py
@@ -9,7 +9,8 @@ def cluster(request):
 def versions(request):
     return {
         'cli_version': CliVersion.load(),
-        'platfornm_version': PlatformVersion.load(),
+        'platform_version': PlatformVersion.load(),
         'lib_version': LibVersion.load(),
         'chart_version': ChartVersion.load(),
+        'assets_version': '0.5.1'
     }
