f8d8ea9d6ce5cfe493caeff197cb0f3c97d06ed7
mouradmourafiq <mouradmourafiq@gmail.com>
2019-08-01 17:15:30 +0200

    Update django security release

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(name='polyaxon-core',
       ],
       install_requires=[
           "celery==4.3.0",
-          "Django==2.2.3",
+          "Django==2.2.4",
           "django-cors-headers==2.4.0",
           "django-picklefield==2.0",
           "djangorestframework==3.10.0",
