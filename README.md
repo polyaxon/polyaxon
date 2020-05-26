2cf33deb126ca4a9e7dbf41b66c27d46dbd78f25
mouradmourafiq <mouradmourafiq@gmail.com>
2020-05-26 19:54:09 +0200

    Update quick start

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE)
 [![Build Status](https://travis-ci.org/polyaxon/polyaxon-quick-start.svg?branch=master)](https://travis-ci.org/polyaxon/polyaxon-quick-start)
-[![Slack](https://img.shields.io/badge/chat-on%20slack-aadada.svg?logo=slack&longCache=true)](https://join.slack.com/t/polyaxon/shared_invite/enQtMzQ0ODc2MDg1ODc0LWY2ZTdkMTNmZjBlZmRmNjQxYmYwMTBiMDZiMWJhODI2ZTk0MDU4Mjg5YzA5M2NhYzc5ZjhiMjczMDllYmQ2MDg)
+[![Slack](https://img.shields.io/badge/chat-on%20slack-aadada.svg?logo=slack&longCache=true)](https://polyaxon.com/slack/)
 
 # polyaxon-quick-start
 
+> N.B. This is a read-only repo, you can create issues or PRs in the [main repo](https://github.com/polyaxon/polyaxon/issues).
+
 A quick start project for polyaxon.
 
-This example is used for the quick start [section in the documentation](https://docs.polyaxon.com/concepts/quick_start/)
-
-> If you are looking for the quick-start example used for v0.4, please go to the [v0.4 branch](https://github.com/polyaxon/polyaxon-quick-start/tree/v0.4) 
+This example is used for the quick start [section in the documentation](https://polyaxon.com/docs/quick_start/)
 
 This example also includes different `polyaxonfiles`:
 
--- model.py
+++ model.py
@@ -1,171 +1,178 @@
-from __future__ import absolute_import, division, print_function
+#!/usr/bin/python
+#
+# Copyright 2018-2020 Polyaxon, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
+
+import keras
 import tensorflow as tf
-from polyaxon_client.tracking import Experiment, get_log_level, get_data_paths, get_outputs_path
-from tensorflow.examples.tutorials.mnist import input_data
+
+from keras.datasets import fashion_mnist
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dense, Dropout, Activation, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import optimizers
+
+from polyaxon import tracking
+from polyaxon.tracking.contrib.keras import PolyaxonKerasCallback, PolyaxonKerasModelCheckpoint
 
 
-def set_logging(log_level=None):
-    if log_level == 'INFO':
-        log_level = tf.logging.INFO
-    elif log_level == 'DEBUG':
-        log_level = tf.logging.DEBUG
-    elif log_level == 'WARN':
-        log_level = tf.logging.WARN
-    else:
-        log_level = 'INFO'
-
-    tf.logging.set_verbosity(log_level)
+OPTIMIZERS = {
+    'adam': optimizers.Adam,
+    'rmsprop': optimizers.RMSprop,
+    'sgd': optimizers.SGD,
+}
 
 
-set_logging(get_log_level())
+def create_model(
+    conv1_size,
+    conv2_size,
+    dropout,
+    hidden1_size,
+    conv_activation,
+    dense_activation,
+    optimizer,
+    learning_rate,
+    loss,
+    num_classes,
+):
+    model = Sequential()
+    model.add(Conv2D(conv1_size, (5, 5), activation=conv_activation,
+                     input_shape=(img_width, img_height, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(conv2_size, (5, 5), activation=conv_activation))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout))
+    model.add(Flatten())
+    model.add(Dense(hidden1_size, activation=dense_activation))
+    model.add(Dense(num_classes, activation='softmax'))
 
-data_paths = list(get_data_paths().values())[0]
-data_paths = "{}/mnist".format(data_paths)
-mnist = input_data.read_data_sets(data_paths, one_hot=False)
+    model.compile(
+        optimizer=OPTIMIZERS[optimizer](lr=learning_rate),
+        loss=loss,
+        metrics=['accuracy'],
+    )
 
-
-def get_model_fn(learning_rate, dropout, activation):
-    """Create a `model_fn` compatible with tensorflow estimator based on hyperparams."""
-
-    def get_network(x_dict, is_training):
-        with tf.variable_scope('network'):
-            x = x_dict['images']
-            x = tf.reshape(x, shape=[-1, 28, 28, 1])
-            conv1 = tf.layers.conv2d(x, 32, 5, activation=activation)
-            conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
-            conv2 = tf.layers.conv2d(conv1, 64, 3, activation=activation)
-            conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
-            fc1 = tf.contrib.layers.flatten(conv2)
-            fc1 = tf.layers.dense(fc1, 1024)
-            fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
-            out = tf.layers.dense(fc1, 10)
-        return out
-
-    def model_fn(features, labels, mode):
-        is_training = mode == tf.estimator.ModeKeys.TRAIN
-
-        results = get_network(features, is_training=is_training)
-
-        predictions = tf.argmax(results, axis=1)
-
-        # Return prediction
-        if mode == tf.estimator.ModeKeys.PREDICT:
-            return tf.estimator.EstimatorSpec(mode, predictions=predictions)
-
-        # Define loss
-        loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits=results, labels=tf.cast(labels, dtype=tf.int32)))
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        train_op = optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
-
-        # Evaluation metrics
-        accuracy = tf.metrics.accuracy(labels=labels, predictions=predictions)
-        precision = tf.metrics.precision(labels=labels, predictions=predictions)
-
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            predictions=predictions,
-            loss=loss_op,
-            train_op=train_op,
-            eval_metric_ops={'accuracy': accuracy, 'precision': precision})
-
-    return model_fn
+    return model
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--batch_size',
-        default=128,
-        type=int
-    )
+        '--conv1_size',
+        type=int,
+        default=32)
     parser.add_argument(
-        '--num_steps',
-        default=800,
-        type=int
-    )
-    parser.add_argument(
-        '--num_iterations',
-        default=1,
-        type=int
-    )
-    parser.add_argument(
-        '--learning_rate',
-        default=0.001,
-        type=float
+        '--conv2_size',
+        type=int,
+        default=64
     )
     parser.add_argument(
         '--dropout',
-        default=0.25,
-        type=float
+        type=float,
+        default=0.2
     )
     parser.add_argument(
-        '--num_epochs',
-        default=1,
-        type=int
+        '--hidden1_size',
+        type=int,
+        default=500
     )
     parser.add_argument(
-        '--activation',
-        default='relu',
-        type=str
+        '--conv_activation',
+        type=str,
+        default="relu"
     )
     parser.add_argument(
-        '--distributed',
-        default=False,
-        type=bool
+        '--dense_activation',
+        type=str,
+        default="relu"
+    )
+    parser.add_argument(
+        '--optimizer',
+        type=str,
+        default='adam'
+    )
+    parser.add_argument(
+        '--learning_rate',
+        type=float,
+        default=0.001
+    )
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        '--loss',
+        type=str,
+        default="categorical_crossentropy"
     )
 
     args = parser.parse_args()
-    arguments = args.__dict__
 
-    batch_size = arguments.pop('batch_size')
-    num_steps = arguments.pop('num_steps')
-    learning_rate = arguments.pop('learning_rate')
-    dropout = arguments.pop('dropout')
-    num_epochs = arguments.pop('num_epochs')
-    activation = arguments.pop('activation')
-    distributed = arguments.pop('distributed')
-    num_iterations = arguments.pop('num_iterations')
-    if activation == 'relu':
-        activation = tf.nn.relu
-    elif activation == 'sigmoid':
-        activation = tf.nn.sigmoid
-    elif activation == 'linear':
-        activation = None
+    img_width, img_height = 28, 28
 
-    experiment = Experiment()
-    if distributed:
-        # Check if we need to export TF_CLUSTER
-        experiment.get_tf_config()
+    # Data
+    (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+    labels = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+              "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-    estimator = tf.estimator.Estimator(
-        get_model_fn(learning_rate=learning_rate, dropout=dropout, activation=activation),
-        model_dir=get_outputs_path())
+    X_train = X_train.astype('float32')
+    X_train /= 255.
+    X_test = X_test.astype('float32')
+    X_test /= 255.
 
-    # Train the Model
-    input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={'images': mnist.train.images},
-        y=mnist.train.labels,
-        batch_size=batch_size,
-        num_epochs=num_epochs,
-        shuffle=True)
+    # reshape input data
+    X_train = X_train.reshape(X_train.shape[0], img_width, img_height, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_width, img_height, 1)
 
-    for i in range(num_iterations):
-        estimator.train(input_fn, steps=num_steps)
+    # one hot encode outputs
+    y_train = keras.utils.to_categorical(y_train)
+    y_test = keras.utils.to_categorical(y_test)
+    num_classes = y_test.shape[1]
 
-        # Evaluate the Model
-        input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={'images': mnist.test.images},
-            y=mnist.test.labels,
-            batch_size=batch_size,
-            shuffle=False)
+    # Polyaxon
+    tracking.init()
+    plx_callback = PolyaxonKerasCallback()
+    plx_model_callback = PolyaxonKerasModelCheckpoint()
+    log_dir = tracking.get_tensorboard_path()
 
-        metrics = estimator.evaluate(input_fn)
+    print("log_dir", log_dir)
+    print("model_dir", plx_model_callback.filepath)
+    # TF Model
+    model = create_model(
+        conv1_size=args.conv1_size,
+        conv2_size=args.conv2_size,
+        dropout=args.dropout,
+        hidden1_size=args.hidden1_size,
+        conv_activation=args.conv_activation,
+        dense_activation=args.dense_activation,
+        optimizer=args.optimizer,
+        learning_rate=args.learning_rate,
+        loss=args.loss,
+        num_classes=y_test.shape[1]
+    )
 
-        print("Testing metrics: {}", metrics)
-        experiment.log_metrics(loss=metrics['loss'],
-                               accuracy=metrics['accuracy'],
-                               precision=metrics['precision'])
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir,
+        histogram_freq=1,
+        update_freq=100
+    )
+
+    model.fit(x=X_train,
+              y=y_train,
+              epochs=args.epochs,
+              validation_data=(X_test, y_test),
+              callbacks=[tensorboard_callback, plx_callback, plx_model_callback])
