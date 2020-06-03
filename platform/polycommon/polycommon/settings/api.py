6516234f0240b3ddc5429efb4da66420f23b4708
mouradmourafiq <mouradmourafiq@gmail.com>
2020-06-03 19:45:10 +0200

    Fix url conf

--- platform/polycommon/polycommon/settings/api.py
+++ platform/polycommon/polycommon/settings/api.py
@@ -13,13 +13,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List, Optional
+from typing import List
 
 from polycommon.config_manager import ConfigManager
 
 
 def set_api(context, config: ConfigManager, processors: List[str] = None):
-    context["ROOT_URLCONF"] = ""
+    context["ROOT_URLCONF"] = "polyconf.urls"
 
     def get_allowed_hosts():
         allowed_hosts = config.get_string(
