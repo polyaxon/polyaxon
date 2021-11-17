03447e7f2f069f2789e6ba20ef04802ed4653daf
polyaxon-ci <ci@polyaxon.com>
2021-11-17 14:48:11 +0100

    Split platform

--- ci/test-helm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-python3 ci/validate/helm.py --values=values1.yml --output-dir=tmp_template1
-python3 ci/validate/helm.py --values=values2.yml --output-dir=tmp_template2
-python3 ci/validate/helm.py --values=values3.yml --output-dir=tmp_template3
