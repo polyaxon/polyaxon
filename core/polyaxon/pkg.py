d23b95136804a7abbd6b1b979cedeac4a9e2f375
polyaxon-ci <ci@polyaxon.com>
2022-03-27 22:00:58 +0200

    Bump version to v1.17.1

--- core/polyaxon/pkg.py
+++ core/polyaxon/pkg.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 NAME = "polyaxon"
-VERSION = "1.17.0"
+VERSION = "1.17.1"
 SCHEMA_VERSION = 1.1
 DESC = "Command Line Interface (CLI) and client to interact with Polyaxon API."
 URL = "https://github.com/polyaxon/polyaxon"
