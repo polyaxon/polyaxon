0563882d0d1bfdf4e64a65bcd91e8d6d4ab6ed8f
polyaxon-ci <ci@polyaxon.com>
2022-08-31 16:57:59 +0200

    Fix artifacts name sanitization for root folders

--- core/polyaxon/polypod/compiler/lineage/artifacts_collector.py
+++ core/polyaxon/polypod/compiler/lineage/artifacts_collector.py
@@ -24,7 +24,7 @@ from traceml.artifacts import V1ArtifactKind, V1RunArtifact
 def collect_lineage_artifacts_path(artifact_path: str) -> Optional[V1RunArtifact]:
     name = os.path.basename(artifact_path.rstrip("/"))  # Trim handles cases like `foo/` -> ''
     return V1RunArtifact(
-        name=to_fqn_name(name),
+        name=to_fqn_name(name) if name else "_",
         kind=V1ArtifactKind.DIR,
         path=artifact_path,
         summary={"path": artifact_path},
